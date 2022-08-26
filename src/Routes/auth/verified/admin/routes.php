8953d64cca9280bc9eb7086b7883cc85c2a96c77
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-26 18:01:58 +0300

    routes

--- src/Routes/auth/verified/admin/routes.php
+++ src/Routes/auth/verified/admin/routes.php
@@ -2,7 +2,9 @@
 
 Route::middleware(['isadmin'])->prefix('admin')->group(function () {
     
+    require __DIR__ . '/clienti/routes.php';
+
     require __DIR__ . '/localitati/routes.php';
 
-    
+
 });
