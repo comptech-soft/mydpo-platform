7d9658c8474d312c4bd76d8ec71ab8f47d89ad54
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-26 17:57:42 +0300

    routes

--- src/Routes/auth/verified/admin/routes.php
+++ src/Routes/auth/verified/admin/routes.php
@@ -1,33 +1,8 @@
 <?php
 
-use MyDpo\Http\Controllers\Admin\CountriesController;
-use MyDpo\Http\Controllers\Admin\RegionsController;
-use MyDpo\Http\Controllers\Admin\LocalitatiController;
-
-
 Route::middleware(['isadmin'])->prefix('admin')->group(function () {
-
-    Route::prefix('localitati')->group( function() {
-        
-        Route::get('/', [LocalitatiController::class, 'index']);
-        
-        Route::post('items', [LocalitatiController::class, 'getItems']);
-
-    });
+    
+    require __DIR__ . '/localitati/routes.php';
 
     
-
-    Route::prefix('countries')->group( function() {
-        
-        Route::post('items', [CountriesController::class, 'getItems']);
-
-    });
-
-    Route::prefix('regions')->group( function() {
-        
-        Route::post('items', [RegionsController::class, 'getItems']);
-
-    });
-    
-
 });
