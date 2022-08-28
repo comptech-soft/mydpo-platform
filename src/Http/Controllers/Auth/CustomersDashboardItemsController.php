fb35650dae2e40f593ce9c30a48126f453350ba2
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-28 15:21:10 +0300

    Dashboard Items

--- src/Http/Controllers/Auth/CustomersDashboardItemsController.php
+++ src/Http/Controllers/Auth/CustomersDashboardItemsController.php
@@ -5,13 +5,13 @@ namespace MyDpo\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Helpers\Response;
-use MyDpo\Models\CustomerStatus;
+use MyDpo\Models\CustomerDashboardItem;
 
 class CustomersDashboardItemsController extends Controller
 {
     
     public function getItems(Request $r) {
-        return CustomerStatus::getItems($r->all());
+        return CustCustomerDashboardItemomerStatus::getItems($r->all());
     }
 
 }
