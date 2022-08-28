8af6ea5977122d5a78c0f3e8263cadcb96995ac9
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-28 22:41:38 +0300

    Dashboard Items

--- src/Routes/auth/verified/routes.php
+++ src/Routes/auth/verified/routes.php
@@ -12,6 +12,7 @@ Route::middleware('verified')->group(function () {
 
     require __DIR__ . '/customers-statuses/routes.php';
     require __DIR__ . '/customers-dashboard-items/routes.php';
+    require __DIR__ . '/customers-documents/routes.php';
 
     require __DIR__ . '/admin/routes.php';
 
