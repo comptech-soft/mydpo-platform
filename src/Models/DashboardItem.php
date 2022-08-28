2f0dba6fc0029b6225a07825a748014fae1025ba
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-28 14:52:48 +0300

    Customers

---
+++ src/Models/DashboardItem.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MyDpo\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use MyDpo\Helpers\Performers\Datatable\GetItems;
+use Kalnoy\Nestedset\NodeTrait;
+
+class DashboardItem extends Model {
+
+    use NodeTrait;
+
+    protected $table = 'dashboard-items';
+
+    protected $casts = [
+        'props' => 'json',
+    ];
+    
+    protected $fillable = [
+        'id',
+        'name',
+        'slug',
+        'icon',
+        'title',
+        'slot',
+        'visible_on_admin',
+        'visible_on_b2b',
+        'props',
+    ];
+
+    protected $with = ['ancestors'];
+
+    
+    public static function getItems($input) {
+        return (new GetItems($input, self::query(), __CLASS__))->Perform();
+    }
+}
