87645f16bbc7382ba06195856b2bfdc66c72d40e
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-30 01:23:57 +0300

    Cursuri

--- src/Models/Knolyx.php
+++ src/Models/Knolyx.php
@@ -6,6 +6,32 @@ class Knolyx {
 
     
     public static function CreateUser($user) {
-        dd($user);
+        if( ! $user['k_id'] )
+        {
+            $kUserProvision =  \Http::withHeaders([
+                'X-Project-Id' => config('knolyx.project_id'),
+                'X-Api-Key' => config('knolyx.app_key')
+            ])
+            ->put(
+                config('knolyx.endpoint') . 'user/provision',
+                [
+                    'firstName' => $user->first_name, 
+                    'lastName' => $user->last_name,
+                    'email' => $user->email,
+                    'disableWelcomeEmail' => TRUE,
+                ]
+            )
+            ->json();
+
+            if( array_key_exists('id', $kUserProvision))
+            {
+                $user->k_id = $kUserProvision['id'];
+                $user->save();    
+            }
+
+            $user->refresh();
+
+            dd($user);
+        }
     }
 }
