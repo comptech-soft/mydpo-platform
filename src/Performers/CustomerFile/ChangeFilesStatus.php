dce1af5e14ae1e92e957c4dc1a3b2441ce21e337
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-29 13:20:49 +0300

    Dashboard Items

--- src/Performers/CustomerFile/ChangeFilesStatus.php
+++ src/Performers/CustomerFile/ChangeFilesStatus.php
@@ -3,23 +3,76 @@
 namespace MyDpo\Performers\CustomerFile;
 
 use MyDpo\Helpers\Perform;
-// use MyDpo\Models\UserSetting;
+use MyDpo\Models\CustomerFile;
 
 class ChangeFilesStatus extends Perform {
 
+    public function SendNotify() {
+        if( ($this->input['status'] == 'public') && ($record->status == 'protected'))
+        {
+            $customer = Customer::where('id', $record->customer_id)->with(['persons'])->first();
+
+            $notificare = Notification::getByEntityAndAction('document', 'trimitere');
+
+            foreach($customer->persons as $person)
+            {
+                $notificare->Notify([
+                    'sender_id' => \Sentinel::check()->id,
+                    'entity_id' => $record->id,
+                    'customer_id' => $customer->id,
+                    'receiver_id' => $person->user_id,
+                    'date_from' => NULL,
+                    'date_to' => NULL,
+                    'readed_at' => NULL,
+                    'status' => 'created',
+                    'props' => [
+                        'file' => $record,
+                        'url' => '???',
+                        'sender' => \Sentinel::check(),
+                    ],
+                    'created_by' => \Sentinel::check()->id,
+                ]);
+            }
+
+        }
+    }
     public function Action() {
 
-        dd($this->input);
-        // $record = UserSetting::getByUserAndCode($this->input['user_id'], $this->input['code']);
-        
-        // if(! $record)
-        // {
-        //     $record = UserSetting::create($this->input);
-        // }
-        // else
-        // {
-        //     $record->update(['value' => $this->input['value']]);
-        // }
+        foreach($this->input['files'] as $i => $file_id)
+        {
+            $record = CustomerFile::find($file_id);
+
+            $record->status = $this->input['status'];
+
+            if(false)
+            {
+                $this->SendNotify();
+            }
+
+            $record->save();
+
+            activity()
+                ->by(\Auth::user())
+                ->on($record)
+                ->withProperties(
+                    [
+                        'input' => request()->all(),
+                        'ip' => request()->ip(),
+                    ]
+                )
+                ->event(__CLASS__)
+                ->createdAt($now = now())
+                ->log(
+                    __(
+                        ':name a schimbat statusul fișierelor', 
+                        [
+                            'name' => \Auth::user()->full_name 
+                        ]
+                    ), 
+                );
+
+            
+        }
         
     }
 
