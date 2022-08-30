eb5ea8be19d36c99bd2f8bab59c0cce5ac8bdded
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-30 11:52:59 +0300

    Translations

--- src/Models/MaterialStatus.php
+++ src/Models/MaterialStatus.php
@@ -32,6 +32,14 @@ class MaterialStatus extends Model {
         return false;
     }
 
+    public function getAtCursuriAttribute() {
+        if( array_key_exists('cursuri', $this->applied_to) )
+        {
+            return $this->applied_to['cursuri'] == 1;
+        }
+        return false;
+    }
+
     public static function getItems($input) {
         return (new GetItems($input, self::query(), __CLASS__))->Perform();
     }
