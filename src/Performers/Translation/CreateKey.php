e869fe9fd9b5e98eadd8a2177966f3d1978764a9
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-30 09:17:29 +0300

    Translations

--- src/Performers/Translation/CreateKey.php
+++ src/Performers/Translation/CreateKey.php
@@ -3,11 +3,18 @@
 namespace MyDpo\Performers\Translation;
 
 use MyDpo\Helpers\Perform;
-
+use MyDpo\Models\Translation;
 class CreateKey extends Perform {
 
     public function Action() {
-        dd($this->input);
+        $record = Translation::where('ro', $this->input['key'])->first();
+
+        if( ! $record )
+        {
+            Translation::create([
+                'ro' => $this->input['key'],
+            ])
+        }
     }
 
 }
