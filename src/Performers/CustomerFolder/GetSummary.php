eb10ff503caf6789010dfe497784649d1cd4a4f4
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-31 20:04:56 +0300

    Files

--- src/Performers/CustomerFolder/GetSummary.php
+++ src/Performers/CustomerFolder/GetSummary.php
@@ -9,8 +9,30 @@ class GetSummary extends Perform {
 
     public function Action() {
 
-        
-        $this->payload = __METHOD__;
+        $files = \DB::select("
+            SELECT
+                `materiale-statuses`.`name`,
+                COALESCE(`files-summary`.`count_files`, 0) AS `count_files`
+            FROM `materiale-statuses`
+            LEFT JOIN
+                (
+                    SELECT
+                        COUNT(`customers-files`.id) AS count_files,
+                        `customers-files`.`status`
+                    FROM `customers-files`
+                    WHERE 
+                        (`customers-files`.customer_id = " . $this->input['customer_id'] . ") AND 
+                        ( (`customers-files`.`deleted` IS NULL) OR (`customers-files`.`deleted` = 0) )
+                    GROUP BY `customers-files`.`status`
+                ) 
+                AS `files-summary`
+            ON `materiale-statuses`.`slug` = `files-summary`.`status`
+        ");
+
+        $this->payload = [
+
+            'files' => $files,
+        ];
     
     }
 }
