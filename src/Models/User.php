1383eab587b46370e4ba9cfe15a8b713c0f1a1f4
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-01 12:09:27 +0300

    Accounts

--- src/Models/User.php
+++ src/Models/User.php
@@ -140,7 +140,7 @@ class User extends Authenticatable implements CanResetPassword, MustVerifyEmail
             'role_users', 
             'user_id', 
             'role_id'
-        )->withTimestamps();
+        )->with(['customer_id'])->withTimestamps();
     }
 
     function settings() {
