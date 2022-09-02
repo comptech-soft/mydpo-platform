ef9abdddb9f8089a7286883e4b521dc301592099
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-02 23:07:58 +0300

    User login

--- src/Http/Controllers/Admin/TeamController.php
+++ src/Http/Controllers/Admin/TeamController.php
@@ -6,13 +6,15 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Helpers\Response;
 
-class TeamController extends Controller
-{
+class TeamController extends Controller {
     
     public function index(Request $r) {
+
+        $asset = (config('app.platform') == 'admin') ? 'team' : 'my-team';
+
         return Response::View(
             '~templates.index', 
-            asset('apps/team/index.js')
+            asset('apps/' + $asset + '/index.js')
         );
     }
 
