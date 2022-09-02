233eb1ec3334bc40fec0048cf98e122d4e17d5e1
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-02 23:03:21 +0300

    User login

--- src/Routes/auth/verified/admin/echipa/routes.php
+++ src/Routes/auth/verified/admin/echipa/routes.php
@@ -2,7 +2,9 @@
 
 use MyDpo\Http\Controllers\Admin\TeamController;
 
-Route::prefix('team')->group( function() {
+$prefix = (config('app.platform') == 'admin') ? 'team' : 'my-team';
+
+Route::prefix($prefix)->group( function() {
         
     Route::get('/', [TeamController::class, 'index']);        
 
