d5bf4599e3e918979484d4b753e1b4d6a52543f4
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-02 09:36:33 +0300

    User login

--- src/Performers/Usersession/UserLogin.php
+++ src/Performers/Usersession/UserLogin.php
@@ -4,6 +4,7 @@ namespace MyDpo\Performers\Usersession;
 
 use MyDpo\Helpers\Perform;
 use Illuminate\Auth\Events\Login;
+use MyDpo\Helpers\UserSession;
 
 class UserLogin extends Perform {
     
@@ -19,6 +20,29 @@ class UserLogin extends Perform {
             throw new \Exception('Datele de autentificare nu se potrivesc cu înregistrările noastre.');
         } 
 
+        /**
+         * Vad ce roluri poate avea operatorul ==> $slugs
+         */
+        $user = \Auth::user();
+
+        if(config('app.platform') == 'b2b')
+        {
+            $slugs = ['master', 'customer'];
+        }
+        else
+        {
+            $slugs = ['sa', 'admin', 'operator'];
+        }
+
+        /**
+         * Daca nu are ==> nu face login
+         */
+        if( ! $user->inRoles($slugs) ) 
+        {
+            UserSession::logout();
+            throw new \Exception('Datele de autentificare nu se potrivesc cu înregistrările noastre.');
+        }
+
         request()->session()->regenerate();
 
         $this->payload = $attempt;
@@ -31,5 +55,4 @@ class UserLogin extends Perform {
 
     }
 
-   
 } 
