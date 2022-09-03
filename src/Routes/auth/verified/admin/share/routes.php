acd9da3c24c2711c6f0667f69be89c09720b8f50
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-03 10:56:54 +0300

    Share

--- src/Routes/auth/verified/admin/share/routes.php
+++ src/Routes/auth/verified/admin/share/routes.php
@@ -5,6 +5,5 @@ use MyDpo\Http\Controllers\Admin\ShareController;
 Route::prefix('share')->group( function() {
         
     Route::get('/{entity}', 'ShareController@index'); 
-    // Route::post('items', [LocalitatiController::class, 'getItems']);
 
 });
