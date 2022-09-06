08b9f0e4872d517b911165bbca24289520f6181c
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-06 20:51:43 +0300

    Uploads

---
+++ src/Http/Controllers/System/UploadsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MyDpo\Http\Controllers\System;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Comptech\Helpers\Response;
+// use Comptech\Models\System\Upload;
+
+class UploadsController extends Controller {
+
+    public function getFileProperties(Request $r) {
+
+        return Upload::getFileProperties($r->only(['file', 'is_image', 'path', 'user_id', 'rules']));
+
+    }
+
+}
