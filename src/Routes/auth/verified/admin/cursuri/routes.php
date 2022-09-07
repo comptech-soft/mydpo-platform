148aef58701bad1799fed78aefad7230558cfe50
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-07 11:33:10 +0300

    Cursuri

--- src/Routes/auth/verified/admin/cursuri/routes.php
+++ src/Routes/auth/verified/admin/cursuri/routes.php
@@ -6,10 +6,6 @@ Route::prefix('cursuri')->group( function() {
         
     Route::get('/', [CursuriController::class, 'index']);        
     Route::post('items', [CursuriController::class, 'getItems']);
-    Route::post('action/{action}', [CursuriController::class, 'doAction']);
-
-    Route::post('open-knolyx-course', [CursuriController::class, 'openKnolyxCourse']);
-
-    
+    Route::post('action/{action}', [CursuriController::class, 'doAction']);    
 
 });
--- src/Routes/auth/verified/customers-cursuri/routes.php
+++ src/Routes/auth/verified/customers-cursuri/routes.php
@@ -1,9 +1,21 @@
 <?php
 
 use MyDpo\Http\Controllers\Auth\CustomersCursuriController;
+use MyDpo\Http\Controllers\Admin\CursuriController;
 
 Route::prefix('customers-cursuri')->group( function() {
         
     Route::post('items', [CustomersCursuriController::class, 'getItems']);
     
+});
+
+
+
+Route::prefix('cursuri')->group( function() {
+        
+
+    Route::post('open-knolyx-course', [CursuriController::class, 'openKnolyxCourse']);
+
+    
+
 });
