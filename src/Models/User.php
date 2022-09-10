905105dd0f0a25c36ded74c1818909f833d02339
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-10 14:10:50 +0300

    Users

--- src/Models/User.php
+++ src/Models/User.php
@@ -243,7 +243,7 @@ class User extends Authenticatable implements CanResetPassword, MustVerifyEmail
             //         new UpdatedPassword($input['password'], $input['password_confirmation']),
             //     ];
             // }
-            $result['email']['unique:users,email'] .= (',' . $input['id']);
+            $result['email'][4] .= (',' . $input['id']);
         }
 
         return $result;
