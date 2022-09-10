bb51b026c7084edfade7c5e69c5de2f412672367
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-11 00:27:57 +0300

    Users

--- src/Routes/guest/routes.php
+++ src/Routes/guest/routes.php
@@ -5,11 +5,15 @@ use MyDpo\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::middleware('guest')->group(function () {
 
-    
-    
     Route::get('login',  [LoginController::class, 'index']);
     Route::post('system/login',  [LoginController::class, 'login']);
 
+    Route::get('forgot-password',  [ForgotPasswordController::class, 'index']);
+    Route::post('system/forgot-password',  [ForgotPasswordController::class, 'sendResetPasswordLink']);
+    
+    /**
+     * Atentie la asta! Trebuie pentru SSO
+     */
     Route::get('connect', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
