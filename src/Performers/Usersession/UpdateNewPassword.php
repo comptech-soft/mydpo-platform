eb209110f5b5b936b19d577da4ddb5f9dbaa85a6
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-11 02:50:17 +0300

    Users

--- src/Performers/Usersession/UpdateNewPassword.php
+++ src/Performers/Usersession/UpdateNewPassword.php
@@ -16,29 +16,29 @@ class UpdateNewPassword extends Perform {
          * will update the password on an actual user model and persist it to the
          * database. Otherwise we will parse the error and return the response.
          */
-        $input = $this->input;
+        // $input = $this->input;
 
-        $status = Password::reset(
-            $input,
-            function ($user) use ($input) {
-                $user->forceFill([
-                    'password' => \Hash::make($input['password']),
-                    'remember_token' => \Str::random(60),
-                ])->save();
+        // $status = Password::reset(
+        //     $input,
+        //     function ($user) use ($input) {
+        //         $user->forceFill([
+        //             'password' => \Hash::make($input['password']),
+        //             'remember_token' => \Str::random(60),
+        //         ])->save();
 
-                event(new PasswordReset($user));
-            }
-        );
+        //         event(new PasswordReset($user));
+        //     }
+        // );
 
         /**
          * If the password was successfully reset, we will redirect the user back to
          * the application's home authenticated view. If there is an error we can
          * redirect them back to where they came from with their error message.
          */
-        if($status != Password::PASSWORD_RESET)
-        {
-            throw new \Exception( __($status) );
-        }
+        // if($status != Password::PASSWORD_RESET)
+        // {
+        //     throw new \Exception( __($status) );
+        // }
 
     }
 
