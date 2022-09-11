03eeb9b3c60bb4e2e8cd7f68312b0a017116e81f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-11 14:29:06 +0300

    Dashboard

--- src/Performers/CustomerDashboardItem/SaveReorderedItems.php
+++ src/Performers/CustomerDashboardItem/SaveReorderedItems.php
@@ -7,21 +7,16 @@ use MyDpo\Models\UserSetting;
 
 class SaveReorderedItems extends Perform {
 
-    public static function MakeSettingCode($platform, $customer_id, $user_id) {
-        return  $platform . '-customer-dashboard-' . $customer_id . '-' . $user_id;
-    }
+
 
     public function Action() {
 
-        $code = self::MakeSettingCode(
-            $this->input['platform'],
-            $this->input['customer_id'],
-            $this->input['user_id']
-        );
-        
+       
         UserSetting::saveSetting([
             'user_id' => $this->input['user_id'], 
-            'code' => $code ,
+            'platform' => $this->input['platform'],
+            'customer_id' => $this->input['customer_id'], 
+            'code' => 'customer-dashboard',
             'value' => $this->input['items'],
         ]);  
     }
