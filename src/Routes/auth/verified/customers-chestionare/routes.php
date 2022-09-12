b92e1fd5d73eca2f4606d04c9b19c3d31a604629
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-12 03:55:32 +0300

    Dashboard

--- src/Routes/auth/verified/customers-chestionare/routes.php
+++ src/Routes/auth/verified/customers-chestionare/routes.php
@@ -2,7 +2,7 @@
 
 use MyDpo\Http\Controllers\Auth\CustomersChestionareController;
 
-Route::prefix('customers-centralizatoare')->group( function() {
+Route::prefix('customers-chestonare')->group( function() {
         
     Route::post('items', [CustomersChestionareController::class, 'getItems']);
     Route::post('get-summary', [CustomersChestionareController::class, 'getSummary']);
