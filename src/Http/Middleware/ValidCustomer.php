840c5aa5ef9ae907d41d2974db53a0dea0f95b24
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-13 14:04:43 +0300

    Dashboard

---
+++ src/Http/Middleware/ValidCustomer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MyDpo\Http\Middleware;
+
+use Closure;
+
+class ValidCustomer {
+
+    public function handle($request, Closure $next) {
+
+        dd($request->all());
+        // $user = $request->user();
+
+        // if( ! $user )
+        // {
+        //     return redirect(config('app.url'));
+        // }
+
+        // if( config('app.platform') != 'admin')
+        // {
+        //     return redirect(config('app.url'));
+        // }
+
+        // if( ! $user->inRoles(['sa', 'admin', 'operator']) )
+        // {
+        //     return redirect(config('app.url'));
+        // }
+        
+        return $next($request);
+    }
+}
