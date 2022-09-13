b13d6fac6563e541ef9e2db91465894ee86f5883
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-13 19:05:08 +0300

    Dashboard

---
+++ src/Http/Controllers/Auth/CustomersEmailsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Helpers\Response;
+
+class CustomersEmailsController extends Controller {
+    
+    public function index($customer_id,Request $r) {
+
+        return Response::View(
+            '~templates.index', 
+            asset('apps/customer-emails/index.js'),
+            [], 
+            [
+                'customer_id' => $customer_id,
+            ]
+        );
+    }
+
+  
+}
