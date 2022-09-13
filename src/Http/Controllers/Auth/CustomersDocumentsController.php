7d1b60c32c7472b560530b18271fe0012ddc6bc1
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-13 12:43:05 +0300

    Dashboard

---
+++ src/Http/Controllers/Auth/CustomersDocumentsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Helpers\Response;
+use MyDpo\Models\Customer;
+
+class CustomersDashboardController extends Controller {
+    
+    public function index($customer_id,Request $r) {
+
+        $customer = Customer::find($customer_id);
+
+        if( ! $customer )
+        {
+            if(config('app.platform') == 'admin')
+            {
+                return redirect(config('app.url') . '/admin/clienti');
+            }
+            return redirect(config('app.url')); 
+        }
+
+        return Response::View(
+            '~templates.index', 
+            asset('apps/customer-documents/index.js'),
+            [], 
+            [
+                'customer_id' => $customer_id,
+            ]
+        );
+    }
+    
+}
