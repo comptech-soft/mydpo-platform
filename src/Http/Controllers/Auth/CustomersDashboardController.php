1b65220e379675d8f437e4754f61926ac248942c
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-13 14:08:36 +0300

    Dashboard

--- src/Http/Controllers/Auth/CustomersDashboardController.php
+++ src/Http/Controllers/Auth/CustomersDashboardController.php
@@ -5,22 +5,10 @@ namespace MyDpo\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Helpers\Response;
-use MyDpo\Models\Customer;
 
 class CustomersDashboardController extends Controller {
     
     public function index($customer_id,Request $r) {
-
-        $customer = Customer::find($customer_id);
-
-        if( ! $customer )
-        {
-            if(config('app.platform') == 'admin')
-            {
-                return redirect(config('app.url') . '/admin/clienti');
-            }
-            return redirect(config('app.url')); 
-        }
 
         return Response::View(
             '~templates.index', 
@@ -30,6 +18,7 @@ class CustomersDashboardController extends Controller {
                 'customer_id' => $customer_id,
             ]
         );
+        
     }
 
 
--- src/Http/Middleware/ValidCustomer.php
+++ src/Http/Middleware/ValidCustomer.php
@@ -3,29 +3,24 @@
 namespace MyDpo\Http\Middleware;
 
 use Closure;
+use MyDpo\Models\Customer;
+
 
 class ValidCustomer {
 
     public function handle($request, Closure $next) {
 
-        dd($request->customer_id);
-        // $user = $request->user();
+        $customer = Customer::find($request->customer_id);
 
-        // if( ! $user )
-        // {
-        //     return redirect(config('app.url'));
-        // }
+        if( ! $customer )
+        {
+            if(config('app.platform') == 'admin')
+            {
+                return redirect(config('app.url') . '/admin/clienti');
+            }
+            return redirect(config('app.url')); 
+        }
 
-        // if( config('app.platform') != 'admin')
-        // {
-        //     return redirect(config('app.url'));
-        // }
-
-        // if( ! $user->inRoles(['sa', 'admin', 'operator']) )
-        // {
-        //     return redirect(config('app.url'));
-        // }
-        
         return $next($request);
     }
 }
