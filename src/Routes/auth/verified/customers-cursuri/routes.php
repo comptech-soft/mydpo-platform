094904a9a008a0b2695def5cbf458b03498deb02
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-13 14:21:07 +0300

    Dashboard

--- src/Routes/auth/verified/customers-cursuri/routes.php
+++ src/Routes/auth/verified/customers-cursuri/routes.php
@@ -3,6 +3,13 @@
 use MyDpo\Http\Controllers\Auth\CustomersCursuriController;
 use MyDpo\Http\Controllers\Admin\CursuriController;
 
+
+Route::middleware('valid-customer')->prefix('customer-cursuri')->group( function() {
+
+    Route::get('/{customer_id}', [CustomersCursuriController::class, 'index']);
+
+});
+
 Route::prefix('customers-cursuri')->group( function() {
         
     Route::post('items', [CustomersCursuriController::class, 'getItems']);
