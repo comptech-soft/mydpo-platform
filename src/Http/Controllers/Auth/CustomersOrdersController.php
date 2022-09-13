d91576be33cec59fdc9b51172fe66b8add15342c
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-13 16:12:39 +0300

    Dashboard

--- src/Http/Controllers/Auth/CustomersOrdersController.php
+++ src/Http/Controllers/Auth/CustomersOrdersController.php
@@ -4,11 +4,23 @@ namespace MyDpo\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use MyDpo\Helpers\Response;
 use MyDpo\Models\CustomerOrder;
 
-class CustomersOrdersController extends Controller
-{
+class CustomersOrdersController extends Controller {
     
+    public function index($customer_id,Request $r) {
+
+        return Response::View(
+            '~templates.index', 
+            asset('apps/customer-comenzi/index.js'),
+            [], 
+            [
+                'customer_id' => $customer_id,
+            ]
+        );
+    }
+
     public function getItems(Request $r) {
         return CustomerOrder::getItems($r->all());
     }
--- src/Routes/auth/verified/customers-orders/routes.php
+++ src/Routes/auth/verified/customers-orders/routes.php
@@ -2,6 +2,12 @@
 
 use MyDpo\Http\Controllers\Auth\CustomersOrdersController;
 
+Route::middleware('valid-customer')->prefix('customer-comenzi')->group( function() {
+
+    Route::get('/{customer_id}', [CustomersOrdersController::class, 'index']);
+
+});
+
 Route::prefix('customers-orders')->group( function() {
         
     Route::post('items', [CustomersOrdersController::class, 'getItems']);
