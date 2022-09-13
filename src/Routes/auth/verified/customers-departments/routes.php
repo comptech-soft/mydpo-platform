38965954ba0a3b61ab2336235b1f9332ff318445
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-13 15:53:29 +0300

    Dashboard

--- src/Routes/auth/verified/customers-departments/routes.php
+++ src/Routes/auth/verified/customers-departments/routes.php
@@ -2,6 +2,13 @@
 
 use MyDpo\Http\Controllers\Auth\CustomersDepartmentsController;
 
+Route::middleware('valid-customer')->prefix('customer-departments')->group( function() {
+
+    Route::get('/{customer_id}', [CustomersDepartmentsController::class, 'index']);
+
+});
+
+
 Route::prefix('customers-departments')->group( function() {
         
     Route::post('items', [CustomersDepartmentsController::class, 'getItems']);
