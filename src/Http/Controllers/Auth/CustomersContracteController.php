c1b3b9bb6b913e474f84e9f9c52a77cb6cd06cbc
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-13 16:08:21 +0300

    Dashboard

--- src/Http/Controllers/Auth/CustomersContracteController.php
+++ src/Http/Controllers/Auth/CustomersContracteController.php
@@ -4,11 +4,23 @@ namespace MyDpo\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use MyDpo\Helpers\Response;
 use MyDpo\Models\CustomerContract;
 
-class CustomersContracteController extends Controller
-{
+class CustomersContracteController extends Controller {
     
+    public function index($customer_id,Request $r) {
+
+        return Response::View(
+            '~templates.index', 
+            asset('apps/customer-departments/index.js'),
+            [], 
+            [
+                'customer_id' => $customer_id,
+            ]
+        );
+    }
+
     public function getItems(Request $r) {
         return CustomerContract::getItems($r->all());
     }
--- src/Routes/auth/verified/customers-contracte/routes.php
+++ src/Routes/auth/verified/customers-contracte/routes.php
@@ -2,6 +2,12 @@
 
 use MyDpo\Http\Controllers\Auth\CustomersContracteController;
 
+Route::middleware('valid-customer')->prefix('customer-contracte')->group( function() {
+
+    Route::get('/{customer_id}', [CustomersContracteController::class, 'index']);
+
+});
+
 Route::prefix('customers-contracte')->group( function() {
         
     Route::post('items', [CustomersContracteController::class, 'getItems']);
