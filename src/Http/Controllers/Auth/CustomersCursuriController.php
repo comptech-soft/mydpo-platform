d70b976ffd8efd470ab6be4d82688318961248ff
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-13 14:22:33 +0300

    Dashboard

--- src/Http/Controllers/Auth/CustomersCursuriController.php
+++ src/Http/Controllers/Auth/CustomersCursuriController.php
@@ -6,9 +6,20 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Models\CustomerCurs;
 
-class CustomersCursuriController extends Controller
-{
+class CustomersCursuriController extends Controller {
     
+    public function index($customer_id,Request $r) {
+
+        return Response::View(
+            '~templates.index', 
+            asset('apps/customer-cursuri/index.js'),
+            [], 
+            [
+                'customer_id' => $customer_id,
+            ]
+        );
+    }
+
     public function getItems(Request $r) {
         return CustomerCurs::getItems($r->all());
     }
