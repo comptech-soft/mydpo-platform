81eb476757e85e2460fc5d013f2c6e46bdb837f8
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-13 19:02:30 +0300

    Dashboard

--- src/Routes/auth/verified/routes.php
+++ src/Routes/auth/verified/routes.php
@@ -25,6 +25,7 @@ Route::middleware('verified')->group(function () {
     require __DIR__ . '/customers-materiale/routes.php';
     require __DIR__ . '/customers-team/routes.php';
     require __DIR__ . '/customers-rapoarte-lunare/routes.php';
+    require __DIR__ . '/customers-taskuri/routes.php';
 
     require __DIR__ . '/users/routes.php';
     
