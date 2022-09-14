f39f0b8c6b6b533531ef24b0ba9341c172390016
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-14 21:28:30 +0300

    Dashboard

--- src/Routes/auth/verified/customers-dashboard/routes.php
+++ src/Routes/auth/verified/customers-dashboard/routes.php
@@ -2,7 +2,7 @@
 
 use MyDpo\Http\Controllers\Auth\CustomersDashboardController;
 
-Route::middleware('valid-customer')->prefix('customer-dashboard')->group( function() {
+Route::middleware(['valid-customer', 'isadmin'])->prefix('customer-dashboard')->group( function() {
         
     Route::get('/{customer_id}', [CustomersDashboardController::class, 'index']);
 
