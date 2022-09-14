6d76a5abd11ed169ccf00585f083277e32d8c7e9
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-14 21:53:00 +0300

    Dashboard

--- src/Http/Middleware/ValidCustomer.php
+++ src/Http/Middleware/ValidCustomer.php
@@ -30,7 +30,7 @@ class ValidCustomer {
         /**
          * Numai cei cu conturi pot intra
          */
-        $customers = CustomerAccount::where('user_id', \Auth::user()->id)->with(['customer'])->get(['customers-persons.id', 'customer.name'])->select()->toArray();
+        $customers = CustomerAccount::where('user_id', \Auth::user()->id)->get(['customer_id']);
         dd($customer->id, \Auth::user()->id, $customers);
         
     }
