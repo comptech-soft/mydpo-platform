c4a5f4d515da08bbaf6365b821938d47cd7d7c41
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-22 09:59:57 +0300

    Permissions

---
+++ src/Routes/auth/verified/admin/users/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+use MyDpo\Http\Controllers\Admin\UsersController;
+
+Route::prefix('utilizatori')->group( function() {
+        
+    Route::get('/', [UsersController::class, 'index']);        
+    // Route::post('items', [CustomersController::class, 'getItems']);
+
+    /**
+     * Asta la ce e buna? 
+     */
+    // Route::post('persons/items', [CustomersController::class, 'getItemsWithPersons']);
+    
+    // Route::post('action/{action}', [CustomersController::class, 'doAction']);
+});
