e4c73d7473993267dcc35320bfc1c542e6d27309
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-22 08:58:25 +0300

    Permissions

---
+++ src/Models/Permission.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MyDpo\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
+
+// https://github.com/lazychaser/laravel-nestedset#installation
+
+class Permission extends Model {
+ 
+    protected $table = 'permissions';
+
+    protected $with = ['ancestors'];
+
+    protected $fillable = [
+        'id',
+        'name',
+        'slug',
+        'platform',
+        'description',
+        'order_no',
+        'props',
+        'deleted',
+        'created_by',
+        'updated_by',
+    ];
+
+}
