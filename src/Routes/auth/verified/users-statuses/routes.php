2735755312207a9662d9f4f142d9a4be96250956
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-22 12:01:58 +0300

    Permissions

--- src/Routes/auth/verified/users-statuses/routes.php
+++ src/Routes/auth/verified/users-statuses/routes.php
@@ -2,7 +2,7 @@
 
 use MyDpo\Http\Controllers\Auth\UsersStatusesController;
 
-Route::prefix('users')->group( function() {
+Route::prefix('users-statuses')->group( function() {
         
     Route::post('items', [UsersStatusesController::class, 'getItems']);
     
