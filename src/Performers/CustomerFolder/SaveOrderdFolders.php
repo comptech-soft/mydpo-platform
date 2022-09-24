7fb4ad0386a496085be76f901a909f31b7612b6f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-24 22:47:43 +0300

    Reorder folders

--- src/Performers/CustomerFolder/SaveOrderdFolders.php
+++ src/Performers/CustomerFolder/SaveOrderdFolders.php
@@ -3,12 +3,19 @@
 namespace MyDpo\Performers\CustomerFolder;
 
 use MyDpo\Helpers\Perform;
+use MyDpo\Models\UserSetting;
 
 class SaveOrderdFolders extends Perform {
 
     public function Action() {
 
-        dd($this->input);
+        UserSetting::saveSetting([
+            'user_id' => $this->input['user_id'], 
+            'platform' => $this->input['platform'],
+            'customer_id' => $this->input['customer_id'], 
+            'code' =>  $this->input['platform'] . '-' . $this->input['customer_id'] . '-customer-folders-order',
+            'value' => $this->input['items'],
+        ]);  
     
     }
 }
