87ddc72bf536a570655ed66b1d07b34b0f1f955c
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-26 12:02:51 +0300

    Folders access

---
+++ src/Models/CustomerFolderPermission.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MyDpo\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerFolderPermission extends Model {
+
+    protected $table = 'customers-folders-permissions';
+    
+    protected $casts = [
+        'id' => 'integer',
+        'created_by' => 'integer',
+        'updated_by' => 'integer',
+        'customer_id' => 'integer',
+        'folder_id' => 'integer',
+        'user_id' => 'integer',
+        'has_access' => 'integer',
+    ];
+
+    protected $fillable = [
+        'id',
+        'customer_id',
+        'folder_id',
+        'user_id',
+        'has_access',
+        'updated_by',
+        'deleted_by',
+    ];
+}
