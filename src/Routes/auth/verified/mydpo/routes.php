8fa3c89c7f6b076aca9ab58f528b6faab753f96b
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-09-30 23:53:07 +0300

    Users documents

--- src/Routes/auth/verified/mydpo/routes.php
+++ src/Routes/auth/verified/mydpo/routes.php
@@ -3,6 +3,8 @@
 Route::middleware(['isb2b'])->prefix('b2b')->group(function () {
     
     require __DIR__ . '/clienti/routes.php';
+    require __DIR__ . '/utilizatori/routes.php';
+
     // require __DIR__ . '/contracte/routes.php';
     // require __DIR__ . '/comenzi/routes.php';
 
---
+++ src/Routes/auth/verified/mydpo/utilizatori/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+use MyDpo\Http\Controllers\Admin\UsersController;
+
+Route::prefix('utilizatori')->group( function() {
+           
+    Route::post('items', [UsersController::class, 'getItems']);
+
+});
