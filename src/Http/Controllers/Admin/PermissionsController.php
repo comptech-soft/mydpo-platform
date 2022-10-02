1b0ef2fd79274f00387ead4dac5a02e14f8232dc
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-02 23:23:23 +0300

    Roles

--- src/Http/Controllers/Admin/PermissionsController.php
+++ src/Http/Controllers/Admin/PermissionsController.php
@@ -4,6 +4,7 @@ namespace MyDpo\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use MyDpo\Helpers\Response;
 use MyDpo\Models\Permission;
 
 class PermissionsController extends Controller {
