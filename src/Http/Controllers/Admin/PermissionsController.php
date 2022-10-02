ee34491737627e9811ba38f75afb1fea0256b16c
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-02 23:20:31 +0300

    Roles

--- src/Http/Controllers/Admin/PermissionsController.php
+++ src/Http/Controllers/Admin/PermissionsController.php
@@ -8,6 +8,13 @@ use MyDpo\Models\Permission;
 
 class PermissionsController extends Controller {
     
+    public function index(Request $r) {
+        return Response::View(
+            '~templates.index', 
+            asset('apps/permissions/index.js')
+        );
+    }
+
     public function getItems(Request $r) {
         return Permission::getItems($r->all());
     }
--- src/Routes/auth/verified/admin/permissions/routes.php
+++ src/Routes/auth/verified/admin/permissions/routes.php
@@ -3,7 +3,8 @@
 use MyDpo\Http\Controllers\Admin\PermissionsController;
 
 Route::prefix('permissions')->group( function() {
-            
+
+    Route::get('/', [PermissionsController::class, 'index']);       
     Route::post('items', [PermissionsController::class, 'getItems']);
 
 });
