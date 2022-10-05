82b01f037a6d2716f15de7e401feb917a002563f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-05 05:23:12 +0300

    User documanets

--- src/Performers/CustomerFolder/SaveFoldersAccess.php
+++ src/Performers/CustomerFolder/SaveFoldersAccess.php
@@ -9,7 +9,36 @@ class SaveFoldersAccess extends Perform {
 
     public function Action() {
 
-        dd($this->input);
-    
+        CustomerFolderPermission::where('customer_id', $this->input['customer_id'])
+            ->where('user_id',  $this->input['user_id'])
+            ->update([
+                'has_access' => 0,
+            ]);
+
+        foreach($this->input['selectedFolders'] as $i => $folder_id) 
+        {
+            $record = CustomerFolderPermission::where('customer_id', $this->input['customer_id'])
+                ->where('user_id',  $this->input['user_id'])
+                ->where('folder_id',  $folder_id)->first();
+
+            if(!! $record)
+            {
+                $record->update([
+                    'has_access' => 1,
+                    'updated_by' => \Auth::user()->id,
+                ]);
+            }
+            else
+            {
+                CustomerFolderPermission::create([
+                    'has_access' => 1,
+                    'customer_id' => $this->input['customer_id'],
+                    'user_id' => $this->input['user_id'],
+                    'folder_id' => $folder_id,
+                    'created_by' => \Auth::user()->id,
+                ]);
+            }
+        }
+
     }
 }
