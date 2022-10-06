b69ef9c37ebfa1e7b4aa3f243589243c1c207018
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-06 12:30:51 +0300

    Documents

--- src/Listeners/CustomerDocuments/FilesUpload.php
+++ src/Listeners/CustomerDocuments/FilesUpload.php
@@ -7,7 +7,5 @@ use MyDpo\Events\CustomerDocuments\FilesUpload as FilesUploadEvent;
 class FilesUpload {
 
     public function handle(FilesUploadEvent $event) {
-        
-        dd(__METHOD__);
     }
 }
