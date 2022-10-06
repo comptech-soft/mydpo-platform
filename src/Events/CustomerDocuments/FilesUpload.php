3d13185c2c95bdf59743889b66c6d32188be6469
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-06 16:20:40 +0300

    Documents

--- src/Events/CustomerDocuments/FilesUpload.php
+++ src/Events/CustomerDocuments/FilesUpload.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class FilesUpload {
+class FilesUpload implements ShouldBroadcast {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public function __construct() {
