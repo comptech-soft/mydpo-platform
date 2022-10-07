0b253910883abe6483a40b21cba560f3d40eb18d
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-07 11:03:36 +0300

    Documents

--- src/Performers/Customer/GetCustomersByIds.php
+++ src/Performers/Customer/GetCustomersByIds.php
@@ -3,14 +3,25 @@
 namespace MyDpo\Performers\Customer;
 
 use MyDpo\Helpers\Perform;
-// use MyDpo\Models\CustomerFile;
+use MyDpo\Models\Customer;
 
 class GetCustomersByIds extends Perform {
 
    
     public function Action() {
 
-        dd(__METHOD__, $this->input);
+        $ids = $this->input['ids'];
+
+        if( count($ids) == 0 )
+        {
+            $customers = [];
+        }
+        else
+        {
+            $customers = Customer::whereIn('id', $ids)->get();
+        }
+
+        $this->payload = $customers;
     }
 
 } 
