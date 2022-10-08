0fd68073872f06ce91243b1903c82b5ea315e82b
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-08 23:10:50 +0300

    Documents

--- src/Http/Controllers/Auth/CustomersDashboardController.php
+++ src/Http/Controllers/Auth/CustomersDashboardController.php
@@ -5,11 +5,24 @@ namespace MyDpo\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Helpers\Response;
+use MyDpo\Models\UserCustomer;
 
 class CustomersDashboardController extends Controller {
     
-    public function index($customer_id,Request $r) {
+    public function index($customer_id, Request $r) {
 
+        $user = \Auth::user();
+
+        if( $user->role && ($user->role->slug == 'operator') )
+        {
+            $exists = UserCustomer::where('user_id', $user->id)->where('customer_id', $customer_id)->first();
+            if( ! $exists )
+            {
+                return redirect(config('app.url'));
+            }
+        }
+
+        
         return Response::View(
             '~templates.index', 
             asset('apps/customer-dashboard/index.js'),
@@ -20,6 +33,4 @@ class CustomersDashboardController extends Controller {
         );
         
     }
-
-
 }
