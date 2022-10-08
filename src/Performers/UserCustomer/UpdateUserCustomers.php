dd4b30afd72662acf0138b4b78e8370e33e35d4e
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-08 08:31:59 +0300

    Documents

--- src/Performers/UserCustomer/UpdateUserCustomers.php
+++ src/Performers/UserCustomer/UpdateUserCustomers.php
@@ -9,7 +9,21 @@ class UpdateUserCustomers extends Perform {
 
     public function Action() {
 
-        dd(__METHOD__, $this->input);
+        UserCustomer::where('user_id', $this->input['user_id'])->delete();
+
+        if(! array_key_exists('customers_ids', $this->input))
+        {
+            $this->input['customers_ids'] = [];
+        }
+
+        foreach($this->input['customers_ids'] as $i => $customer_id)
+        {
+            UserCustomer::create([
+                'user_id' => $this->input['user_id'],
+                'customer_id' => $customer_id,
+                'created_by' => \Auth::user()->id,
+            ]);
+        }
         
     }
 
