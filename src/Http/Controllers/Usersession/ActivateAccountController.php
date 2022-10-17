639a7802ef2c1b5d45827bfa99fc8755f28cb197
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-17 12:42:30 +0300

    =Folders order

---
+++ src/Http/Controllers/Usersession/ActivateAccountController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Usersession;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Helpers\Response;
+use MyDpo\Helpers\UserSession;
+
+class ActivateAccountController extends Controller {
+
+    public function index(Request $r) {
+        return Response::View(
+            '~templates.index', 
+            asset('apps/activate-accont/index.js'),
+            [], 
+            [
+                'token' => $token,
+            ]
+        );
+    }
+
+    public function sendResetPasswordLink(Request $r) {
+        return UserSession::sendResetPasswordLink($r->only([
+            'email'
+        ]));
+    }
+
+}
