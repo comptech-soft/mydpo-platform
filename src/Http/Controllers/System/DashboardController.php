f2c2816f8a3f53fda412614cda9329e6f5808d9f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-18 13:37:32 +0300

    =Folders order

--- src/Http/Controllers/System/DashboardController.php
+++ src/Http/Controllers/System/DashboardController.php
@@ -7,36 +7,48 @@ use Illuminate\Http\Request;
 use MyDpo\Helpers\Response;
 
 class DashboardController extends Controller {
+    
     public function index(Request $r) {
-
-        $user = \Auth::user();
-
-        dd($user);
-
-        if(! $user->active )
-        {
-            return Response::View(
-                '~templates.index', 
-                asset('apps/email-verify-prompt/index.js'),
-                [],
-                $r->all()
-            );        
-        }
 
         /**
          * admin ==> dashboard
          * b2b   ==> my-dashboard
          */
 
-        dd(config('app.platform'));
+        $user = \Auth::user();
 
         $asset = (config('app.platform') == 'admin') ? 'dashboard' : 'my-dashboard';
 
-        return Response::View(
-            '~templates.index', 
-            asset('apps/' . $asset . '/index.js'),
-            [],
-            $r->all()
-        );
+        if(config('app.platform') == 'admin')
+        {
+            if(! $user->active )
+            {
+                return Response::View(
+                    '~templates.index', 
+                    asset('apps/email-verify-prompt/index.js'),
+                    [],
+                    $r->all()
+                );        
+            }
+
+            return Response::View(
+                '~templates.index', 
+                asset('apps/' . $asset . '/index.js'),
+                [],
+                $r->all()
+            );
+        }
+        else
+        {
+            dd('Bam bam >>>> ');
+        }
+
+        
+
+        
+
+        
+
+        
     }
 }
