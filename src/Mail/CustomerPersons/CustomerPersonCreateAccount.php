4e221c0f20df8e2ebf77335934eaf5aa7b954559
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-18 08:27:50 +0300

    =Folders order

--- src/Mail/CustomerPersons/CustomerPersonCreateAccount.php
+++ src/Mail/CustomerPersons/CustomerPersonCreateAccount.php
@@ -11,10 +11,13 @@ class CustomerPersonCreateAccount extends BaseEmail {
     public function __construct($entity, $action, $input) {
 
         parent::__construct($entity, $action, $input);
-
-        dd($this->input);
         
-        $activation = Activation::createActivation($this->input['account']->user_id);
+        $activation = Activation::createActivation(
+            $this->input['account']->user_id, 
+            $this->input['customer_id'],
+            $this->input['roleUser']->role_id, 
+        );
+
         $platform = Platform::where('slug', 'b2b')->first();
 
         $this->actionUrl = \Str::replace(
--- src/Models/Activation.php
+++ src/Models/Activation.php
@@ -23,12 +23,29 @@ class Activation extends Model {
         'token',
     ];
 
-    public static function createActivation($user_id) {
+    public static function createActivation($user_id, $customer_id, $role_id) {
 
-        return self::create([
-            'user_id' => $user_id,
-            'token' => \Str::random(64),
-        ]);
+        $record = self::where('user_id', $user_id)
+            ->where('customer_id', $customer_id)
+            ->first();
+
+        if(!! $record)
+        {
+            $record->update([
+                'activated' => 0,
+                'activated_at' => NULL,
+                'role_id' => $role_id,
+            ]);
+        }
+        else
+        {
+            $record = self::create([
+                'user_id' => $user_id,
+                'customer_id' => $customer_id,
+                'role_id' => $role_id,
+                'token' => \Str::random(64),
+            ]);   
+        }
     }
 
 
