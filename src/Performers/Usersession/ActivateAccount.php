a669eb09305927d99b03b97c2b1631647f833e41
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-18 09:02:10 +0300

    =Folders order

--- src/Performers/Usersession/ActivateAccount.php
+++ src/Performers/Usersession/ActivateAccount.php
@@ -27,43 +27,46 @@ class ActivateAccount extends Perform {
 
         $user = User::byEmail($this->input['email']);
 
-        if(! $user )
+        if(! $user || ($user->id != $activation->user_id))
         {
-            throw new \Exception('Nu există utilizator corespunzător acestei cereri de activare a contului.');
+            throw new \Exception('Nu există utilizatorul corespunzător acestei cereri de activare a contului.');
         }
 
         $customer = Customer::find($activation->customer_id);
 
-        dd($customer);
+        if(! $customer )
+        {
+            throw new \Exception('Nu există clientul corespunzător acestei cereri de activare a contului.');
+        }
 
-        // /**
-        //  * Here we will attempt to reset the user's password. If it is successful we
-        //  * will update the password on an actual user model and persist it to the
-        //  * database. Otherwise we will parse the error and return the response.
-        //  */
-        // $input = $this->input;
+        /**
+         * Here we will attempt to reset the user's password. If it is successful we
+         * will update the password on an actual user model and persist it to the
+         * database. Otherwise we will parse the error and return the response.
+         */
+        $input = $this->input;
 
-        // $status = Password::reset(
-        //     $input,
-        //     function ($user) use ($input) {
-        //         $user->forceFill([
-        //             'password' => \Hash::make($input['password']),
-        //             'remember_token' => \Str::random(60),
-        //         ])->save();
+        $status = Password::reset(
+            $input,
+            function ($user) use ($input) {
+                $user->forceFill([
+                    'password' => \Hash::make($input['password']),
+                    'remember_token' => \Str::random(60),
+                    'email_verified_at' => \Carbon\Carbon::now(),
+                ])->save();
+                // event(new PasswordReset($user));
+            }
+        );
 
-        //         event(new PasswordReset($user));
-        //     }
-        // );
-
-        // /**
-        //  * If the password was successfully reset, we will redirect the user back to
-        //  * the application's home authenticated view. If there is an error we can
-        //  * redirect them back to where they came from with their error message.
-        //  */
-        // if($status != Password::PASSWORD_RESET)
-        // {
-        //     throw new \Exception( __($status) );
-        // }
+        /**
+         * If the password was successfully reset, we will redirect the user back to
+         * the application's home authenticated view. If there is an error we can
+         * redirect them back to where they came from with their error message.
+         */
+        if($status != Password::PASSWORD_RESET)
+        {
+            throw new \Exception( __($status) );
+        }
 
     }
 
