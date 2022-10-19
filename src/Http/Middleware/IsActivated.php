771769c177f92ae4f11007e85101cfa20b8bf731
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-19 08:25:56 +0300

    =Folders order

--- src/Http/Middleware/IsActivated.php
+++ src/Http/Middleware/IsActivated.php
@@ -19,7 +19,7 @@ class IsActivated {
             return redirect( route('activate.account', ['token' => $activation->token]) );
         }
         
-        
+        dd('????');
 
         // if( ! $user )
         // {
