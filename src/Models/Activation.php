0ec459d791191eee762585cafb4ceb11d8e71c2c
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-19 11:47:13 +0300

    =Folders order

--- src/Models/Activation.php
+++ src/Models/Activation.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 // use MyDpo\Helpers\Performers\Datatable\GetItems;
 // use MyDpo\Helpers\Performers\Datatable\DoAction;
 use MyDpo\Models\Customer;
-// use MyDpo\Rules\Category\UniqueName;
+use MyDpo\Models\User;
 
 class Activation extends Model {
    
@@ -32,6 +32,10 @@ class Activation extends Model {
 
     public function customer() {
         return $this->belongsTo(Customer::class, 'customer_id');
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public static function byToken($token) {
--- src/Performers/Usersession/GetInfosByToken.php
+++ src/Performers/Usersession/GetInfosByToken.php
@@ -3,10 +3,7 @@
 namespace MyDpo\Performers\Usersession;
 
 use MyDpo\Helpers\Perform;
-// use Illuminate\Support\Facades\Password;
 use MyDpo\Models\Activation;
-// use MyDpo\Models\User;
-// use MyDpo\Models\Customer;
 
 class GetInfosByToken extends Perform {
 
@@ -19,6 +16,7 @@ class GetInfosByToken extends Perform {
         $this->payload = [
             'activation' => $activation,
             'customer' => $activation->customer,
+            'user' => $activation->user,
         ];
 
     }
