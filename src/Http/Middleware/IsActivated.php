d628b7ec1d46341e3b4b71e895a3d1c4986729c4
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-19 07:57:22 +0300

    =Folders order

--- src/Http/Middleware/IsActivated.php
+++ src/Http/Middleware/IsActivated.php
@@ -13,8 +13,6 @@ class IsActivated {
 
         $activation = Activation::byUserAndCustomer($user->id, $request->customer_id);
 
-        dd($activation);
-        
         if(! $activation || ($activation->activated == 0))
         {
             dd('Nashpa');
