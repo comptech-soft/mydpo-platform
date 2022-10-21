3d9ae787d4095163fd65c38d9f673109f16baf1e
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-21 23:26:43 +0300

    =Accounts

--- src/Routes/auth/verified/customers-team/routes.php
+++ src/Routes/auth/verified/customers-team/routes.php
@@ -7,3 +7,10 @@ Route::middleware('valid-customer')->prefix('customer-team')->group( function() 
     Route::get('/{customer_id}', [CustomersTeamController::class, 'index']);
 
 });
+
+Route::prefix('customers-team')->group( function() {
+        
+    Route::post('items', [CustomersTeamController::class, 'getItems']);
+    Route::post('action/{action}', [CustomersTeamController::class, 'doAction']);
+
+});
