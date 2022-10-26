addb9df6d15ccc804167740b37debe9f448fe8ac
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-26 20:38:10 +0300

    Dashboard routes

--- src/Http/Controllers/Auth/CustomersChestionareController.php
+++ src/Http/Controllers/Auth/CustomersChestionareController.php
@@ -6,9 +6,20 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Models\CustomerChestionar;
 
-class CustomersChestionareController extends Controller
-{
+class CustomersChestionareController extends Controller {
     
+    public function index($customer_id, Request $r) {
+
+        return Response::View(
+            '~templates.index', 
+            asset('apps/customer-chestionare/index.js'),
+            [], 
+            [
+                'customer_id' => $customer_id,
+            ]
+        );
+    }
+
     public function getItems(Request $r) {
         return CustomerChestionar::getItems($r->all());
     }
--- src/Routes/auth/verified/customers-chestionare/routes.php
+++ src/Routes/auth/verified/customers-chestionare/routes.php
@@ -2,6 +2,13 @@
 
 use MyDpo\Http\Controllers\Auth\CustomersChestionareController;
 
+
+Route::middleware('valid-customer')->prefix('/customer-chestionare')->group( function() {
+
+    Route::middleware('is-activated')->get('/{customer_id}', [CustomersChestionareController::class, 'index']);
+
+});
+
 Route::prefix('customers-chestionare')->group( function() {
         
     Route::post('items', [CustomersChestionareController::class, 'getItems']);
