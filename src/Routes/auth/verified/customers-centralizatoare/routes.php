da48f64e03eb7686295ef6e0843837004c564350
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-26 20:34:50 +0300

    Dashboard routes

--- src/Routes/auth/verified/customers-centralizatoare/routes.php
+++ src/Routes/auth/verified/customers-centralizatoare/routes.php
@@ -4,7 +4,7 @@ use MyDpo\Http\Controllers\Auth\CustomersCentralizatoareController;
 
 Route::middleware('valid-customer')->prefix('/customer-centralizatoare')->group( function() {
 
-    Route::middleware('is-activated')->get('/{customer_id}', [CustomersAnalizaGapController::class, 'index']);
+    Route::middleware('is-activated')->get('/{customer_id}', [CustomersCentralizatoareController::class, 'index']);
 
 });
 
