9496c721ec43f98d6187efb655661a0f7ac927a5
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-30 00:42:45 +0300

    Customer Accounts

--- src/Http/Controllers/Admin/UserDashboardController.php
+++ src/Http/Controllers/Admin/UserDashboardController.php
@@ -8,7 +8,7 @@ use MyDpo\Helpers\Response;
 
 class UserDashboardController extends Controller {
     
-    public function index($user_id, Request $r) {
+    public function index($source, $user_id, Request $r) {
 
 
         return Response::View(
@@ -17,6 +17,7 @@ class UserDashboardController extends Controller {
             [], 
             [
                 'user_id' => $user_id,
+                'source' => $source,
             ]
         );
     }
