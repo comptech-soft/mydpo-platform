3f789cfda8facaa43f3537d84d832e863236a738
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-10-30 22:30:58 +0200

    Customer Accounts

--- src/Http/Controllers/Admin/ServiciiComandateController.php
+++ src/Http/Controllers/Admin/ServiciiComandateController.php
@@ -3,12 +3,15 @@
 namespace MyDpo\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use MyDpo\Helpers\Response;
+use MyDpo\CustomerService;
 
 class ServiciiComandateController extends Controller {
-    
-    public function index(Request $r) {
+
+    public function index() {
+
+        CustomerService::syncWithOrders();
+
         return Response::View(
             '~templates.index', 
             asset('apps/servicii-comandate/index.js')
--- src/Models/CustomerService.php
+++ src/Models/CustomerService.php
@@ -85,4 +85,8 @@ class CustomerService extends Model {
         return $result;
     }
 
+    public static function syncWithOrders() {
+        dd(__METHOD__);
+    }
+
 }
