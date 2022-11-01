1b21803a77ad914bc3138ab1d7a8a1038a985ba2
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-11-02 00:18:29 +0200

    B2b Dashboard

---
+++ src/Routes/auth/verified/mydpo/permissions/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+use MyDpo\Http\Controllers\Admin\PermissionsController;
+
+Route::prefix('permissions')->group( function() {    
+    Route::post('items', [PermissionsController::class, 'getItems']);
+});
--- src/Routes/auth/verified/mydpo/routes.php
+++ src/Routes/auth/verified/mydpo/routes.php
@@ -4,34 +4,7 @@ Route::middleware(['isb2b'])->prefix('b2b')->group(function () {
     
     require __DIR__ . '/clienti/routes.php';
     require __DIR__ . '/utilizatori/routes.php';
-
-    // require __DIR__ . '/contracte/routes.php';
-    // require __DIR__ . '/comenzi/routes.php';
-
-    // require __DIR__ . '/persons/routes.php';
-
-    // require __DIR__ . '/planificare/routes.php';
-    // require __DIR__ . '/timesheet/routes.php';
-
-    // require __DIR__ . '/centralizatoare/routes.php';
-    // require __DIR__ . '/chestionare/routes.php';
-    // require __DIR__ . '/cursuri/routes.php';
-
-
-    // require __DIR__ . '/../admin/echipa/routes.php';
-
-    // require __DIR__ . '/rapoarte/routes.php';
-
-    // require __DIR__ . '/servicii/routes.php';
-    // require __DIR__ . '/taskuri/routes.php';
-    // require __DIR__ . '/registre/routes.php';
-
-    // require __DIR__ . '/sabloane-email/routes.php';
-    // require __DIR__ . '/sabloane-notificari/routes.php';
-
-    // require __DIR__ . '/localitati/routes.php';
-
-    // require __DIR__ . '/config/routes.php';
-
+    require __DIR__ . '/permissions/routes.php';
+    
 
 });
