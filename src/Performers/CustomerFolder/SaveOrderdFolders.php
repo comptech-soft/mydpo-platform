9890fa2f1bbb82a7acb8dc406689ff8b9d776248
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-11-06 21:40:30 +0200

    Uploaders

--- src/Performers/CustomerFolder/SaveOrderdFolders.php
+++ src/Performers/CustomerFolder/SaveOrderdFolders.php
@@ -9,13 +9,20 @@ class SaveOrderdFolders extends Perform {
 
     public function Action() {
 
-        dd($this->input);
+        if($this->input['type'] == 'documente')
+        {
+            $code = $this->input['platform'] . '-' . $this->input['customer_id'] . '-customer-folders-order';
+        }
+        else
+        {
+            $code = $this->input['platform'] . '-' . $this->input['customer_id'] . '-customer-' . $this->input['type'] . '-order';
+        }
 
         UserSetting::saveSetting([
             'user_id' => $this->input['user_id'], 
             'platform' => $this->input['platform'],
             'customer_id' => $this->input['customer_id'], 
-            'code' =>  $this->input['platform'] . '-' . $this->input['customer_id'] . '-customer-folders-order',
+            'code' =>  $code,
             'value' => $this->input['items'],
         ]);  
     
