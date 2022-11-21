a89b922d0048d402581ba0502b87d167d67ff4f5
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-11-21 12:21:20 +0200

    Users

--- src/Performers/Database/UpdateField.php
+++ src/Performers/Database/UpdateField.php
@@ -8,7 +8,13 @@ class UpdateField extends Perform {
 
     public function Action() {
         
-        dd($this->input);
+        $record = \DB::table($this->input('table'))
+            ->where('id', $this->input['id'])
+            ->update([
+                $this->input['field'] => $this->input['value'],
+            ]);
+
+        $this->payload = $record->refresh();
     }
 
 } 
