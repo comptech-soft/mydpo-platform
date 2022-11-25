d5cba288315918b4f26e453ff1597cdc3d306d32
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-11-25 13:31:33 +0200

    Accounts

--- src/Performers/CustomerAccount/UpdateRole.php
+++ src/Performers/CustomerAccount/UpdateRole.php
@@ -11,31 +11,24 @@ class UpdateRole extends Perform {
 
     public function Action() {
 
+
+        $account = CustomerAccount::find($this->input['id']);
+
         $roleuser = RoleUser::CreateAccountRole(
             $this->input['customer_id'], 
             $this->input['user_id'], 
             $this->input['role_id']
         );
 
-
         if($this->input['checked'] == 1)
         {
-            if($this->input['activated'] == 1)
-            {
+            $activation = Activation::createActivation($this->input['user_id'], $this->input['customer_id'], $this->input['role_id']);
 
-            }
-            else
-            {
-                $account = CustomerAccount::find($this->input['id']);
+            $activation->activated = $account->activated = ($this->input['activated'] == 1 ? 0 : 1);
+            $activation->activated_at = $account->activated_at = ($activation->activated == 0 ? NULL : \Carbon\Carbon::now());
 
-                $activation = Activation::createActivation($this->input['user_id'], $this->input['customer_id'], $this->input['role_id']);
-
-                $activation->activated = $account->activated = 1;
-                $activation->activated_at = $account->activated_at = \Carbon\Carbon::now();
-
-                $activation->save();
-                $account->save();
-            }
+            $activation->save();
+            $account->save();
         }
     
     }
