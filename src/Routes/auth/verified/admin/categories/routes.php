438f921100e5a897472e7b6028e5acadbc00ff36
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-11-29 20:29:15 +0200

    Elearning

--- src/Routes/auth/verified/admin/categories/routes.php
+++ src/Routes/auth/verified/admin/categories/routes.php
@@ -1,11 +1,20 @@
 <?php
 
 use MyDpo\Http\Controllers\Admin\CategoriesController;
+use MyDpo\Http\Controllers\Admin\CursuriadresareController;
 
 Route::prefix('categories')->group( function() {
            
     Route::post('items/{type?}', [CategoriesController::class, 'getItems']);
     Route::post('action/{action}', [CategoriesController::class, 'doAction']);
     Route::post('valid-name', [CategoriesController::class, 'isValidName']);
+    
+});
+
+Route::prefix('cursuri-adresare')->group( function() {
+           
+    Route::post('items/{type?}', [CursuriadresareController::class, 'getItems']);
+    Route::post('action/{action}', [CursuriadresareController::class, 'doAction']);
+    Route::post('valid-name', [CursuriadresareController::class, 'isValidName']);
     
 });
