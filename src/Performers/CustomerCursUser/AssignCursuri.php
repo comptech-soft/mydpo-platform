6ac40e43337fcfb03c89291f7d6d381c12a04e05
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-12-19 18:00:55 +0200

    Assign cursuri

--- src/Performers/CustomerCursUser/AssignCursuri.php
+++ src/Performers/CustomerCursUser/AssignCursuri.php
@@ -4,12 +4,31 @@ namespace MyDpo\Performers\CustomerCursUser;
 
 use MyDpo\Helpers\Perform;
 use MyDpo\Models\CustomerCursUser;
+use MyDpo\Models\Sharematerial;
 
 class AssignCursuri extends Perform {
 
     public function Action() {
 
-        dd($this->input);
-    
+        $number = Sharematerial::getNextNumber([
+            'type' => 'curs',
+        ])['payload'];
+
+        $shareinput = [
+            'number' => $number,
+            'date' => \Carbon\Carbon::now()->format('Y-m-d'),
+            'status' => 'b2b',
+            'type' => 'curs',
+            'date_from' => $this->input['date_from'],
+            'date_to' => $this->input['date_to'],
+            'materiale_trimise' => $this->input['materiale_trimise'],
+            'customers' => $this->input['customers'],
+            'created_by' => \Auth::user()->id,
+        ];
+
+        $trimitere = Sharematerial::create($shareinput);
+        
+
+        dd($trimitere);
     }
 }
