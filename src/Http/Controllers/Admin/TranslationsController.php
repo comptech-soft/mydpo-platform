7e84d014e4931cbda4c39395deda768d25697b8f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-01-24 00:07:37 +0200

    Translations

---
+++ src/Http/Controllers/Admin/TranslationsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Helpers\Response;
+use MyDpo\Models\Translation;
+
+class TranslationsController extends Controller {
+    
+    public function index($customer_id,Request $r) {
+
+        return Response::View(
+            '~templates.index', 
+            asset('apps/translations/index.js'),
+            [], 
+            []
+        );
+    }
+
+    public function getItems(Request $r) {
+        return Translation::getItems($r->all());
+    }
+    
+    public function doAction($action, Request $r) {
+        return Translation::doAction($action, $r->all());
+    }
+
+}
