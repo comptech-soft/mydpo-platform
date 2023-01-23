c33f95123a2e5624e23ff8db6b3b048aa6747768
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-01-24 00:01:40 +0200

    Translations

---
+++ src/Routes/auth/verified/admin/translations/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use MyDpo\Http\Controllers\Admin\TranslationsController;
+
+
+Route::prefix('translations')->group( function() {
+        
+    Route::get('/', [TranslationsController::class, 'index']);        
+    Route::post('items', [TranslationsController::class, 'getItems']);
+    Route::post('action/{action}', [TranslationsController::class, 'doAction']);
+   
+});
