9da5f2ab076b3d2f6c7eeb4f1ee4f6d92a1e8694
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-01-24 02:03:21 +0200

    Translations

--- src/Performers/Translation/CreateFile.php
+++ src/Performers/Translation/CreateFile.php
@@ -8,7 +8,20 @@ use MyDpo\Models\Translation;
 class CreateFile extends Perform {
 
     public function Action() {
-        dd(__METHOD__);
+        
+        $file = str_replace('\\', '/', resource_path()) . '/comptech-soft/translations/en.js';
+
+        $str = "module.exports = {" . chr(13) . chr(10);
+        foreach(Translation::all() as $i => $record) {
+
+            if(!! $record->en)
+            {
+                $str .= ('"' . $record->ro . '": "' . $record->en . '"' . chr(13) . chr(10));
+            }
+        }
+        $str .= chr(13) . chr(10). "}";
+
+        \File::put(($file), $str);
     }
 
 }
