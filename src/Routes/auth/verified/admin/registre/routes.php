a7c809a761af12952d380e906f5626cce3290306
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-02-19 18:48:35 +0200

    Registre

--- src/Routes/auth/verified/admin/registre/routes.php
+++ src/Routes/auth/verified/admin/registre/routes.php
@@ -6,5 +6,6 @@ Route::prefix('registre')->group( function() {
         
     Route::get('/', [RegistreController::class, 'index']);        
     Route::post('items', [RegistreController::class, 'getItems']);
+    Route::post('action/{action}', [RegistreController::class, 'doAction']);
 
 });
