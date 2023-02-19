a242a3d1c3fe024048bf17a22514247be549900f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-02-19 18:44:26 +0200

    Registre

--- src/Routes/auth/verified/admin/roles/routes.php
+++ src/Routes/auth/verified/admin/roles/routes.php
@@ -6,7 +6,6 @@ Route::prefix('roles')->group( function() {
         
     Route::get('/', [RolesController::class, 'index']);        
     Route::post('items', [RolesController::class, 'getItems']);
-    // Route::post('action/{action}', [UsersController::class, 'doAction']);
     Route::post('save-role-permissions', [RolesController::class, 'saveRolePermissions']);
 
 });
