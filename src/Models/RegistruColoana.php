b528fb73a32eb751b37386bcccb8576c0dd6a4b9
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-02-20 09:27:48 +0200

    Registre

--- src/Models/RegistruColoana.php
+++ src/Models/RegistruColoana.php
@@ -59,7 +59,7 @@ class RegistruColoana extends Model {
             $input = [
                 ...$input,
                 'is_group' => NULL,
-                'order_no' => self::getNextOrderNo($input['register_id']),
+                'order_no' => !! $input['group_id']  ? self::getNextGroupOrderNo($input['group_id']) : self::getNextOrderNo($input['register_id']),
             ];
         }
 
@@ -69,13 +69,7 @@ class RegistruColoana extends Model {
     }
 
     public static function doUpdate($input, $record) {
-        // $input = [
-        //     ...$input,
-        //     'type' => $input['column_type'],
-        // ];
-
         $record->update($input);
-
         return $record;
     }
 
@@ -89,6 +83,14 @@ class RegistruColoana extends Model {
                 MAX(CAST(`order_no` AS UNSIGNED)) as max_order_no 
             FROM `registers-columns` 
             WHERE (register_id=" . $register_id . ') AND ( (is_group = 1) OR (group_id IS NULL))'
+        );
+        return number_format(1 + $records[0]->max_order_no, 0, '', '');
+    }
+
+    public static function getNextGroupOrderNo($group_id) {
+        $records = \DB::select("
+            SELECT MAX(CAST(`order_no` AS UNSIGNED)) as max_order_no 
+            FROM `registers-columns` WHERE (group_id=" . $group_id . ')'
         );
         return number_format(1 + $records[0]->max_order_no, 0, '', '');
     }
