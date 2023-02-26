b45dbe382f1d05020cc416f54c9fb89422de94b9
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-02-26 13:56:54 +0200

    Customer registre

--- src/Performers/CustomerRegistruAsociat/SaveAsociere.php
+++ src/Performers/CustomerRegistruAsociat/SaveAsociere.php
@@ -3,12 +3,23 @@
 namespace MyDpo\Performers\CustomerRegistruAsociat;
 
 use MyDpo\Helpers\Perform;
+use MyDpo\Models\CustomerRegistruAsociat;
 
 class SaveAsociere extends Perform {
 
     public function Action() {
 
         dd($this->input);
+
+        CustomerRegistruAsociat::where('customer_id', $this->input['customer_id'])->delete();
+
+        foreach($this->input['registre'] as $i => $register_id)
+        {
+            CustomerRegistruAsociat::create([
+                'register_id' => $register_id,
+                'customer_id' => $this->input['customer_id'],
+            ]);
+        }
     
     }
 }
