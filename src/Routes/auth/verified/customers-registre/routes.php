b9aa3d52fb461862ca1ea76ee75eeb5e99f08dc9
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-02-26 12:31:53 +0200

    Customer registre

--- src/Routes/auth/verified/customers-registre/routes.php
+++ src/Routes/auth/verified/customers-registre/routes.php
@@ -1,10 +1,16 @@
 <?php
 
 use MyDpo\Http\Controllers\Auth\CustomersRegistreController;
-
+use MyDpo\Http\Controllers\Auth\CustomersRegistreAsiciateController;
 
 Route::middleware('valid-customer')->prefix('/customer-registre')->group( function() {
 
     Route::middleware('is-activated')->get('/{customer_id}', [CustomersRegistreController::class, 'index']);
+
+});
+
+Route::middleware('valid-customer')->prefix('/customers-registers-asociate')->group( function() {
+
+    Route::post('items', [CustomersRegistreAsiciateController::class, 'getItems']);
 
 });
