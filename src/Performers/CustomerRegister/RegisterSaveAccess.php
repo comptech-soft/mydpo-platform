987933c491cef006649b2a9689e82049118ca58c
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-03-14 10:08:42 +0200

    Register

--- src/Performers/CustomerRegister/RegisterSaveAccess.php
+++ src/Performers/CustomerRegister/RegisterSaveAccess.php
@@ -9,8 +9,21 @@ class RegisterSaveAccess extends Perform {
 
     public function Action() {
 
-      
-        //$record = CustomerRegistruUser::find($this->input['']);
+        CustomerRegistruUser::where('customer_registru_id', $this->input['customer_registru_id'])->delete();
+
+        if(array_key_exists('selected', $this->input))
+        {
+            foreach($this->input['selected'] as $user_id => $departamente)
+            {
+                CustomerRegistruUser::create([
+                    'customer_registru_id' => $this->input['customer_registru_id'],
+                    'customer_id' => $this->input['customer_id'],
+                    'register_id' => $this->input['register_id'],
+                    'user_id' => $user_id,
+                    'departamente' => $departamente,                    
+                ]);
+            }
+        }
     }
     
 }
