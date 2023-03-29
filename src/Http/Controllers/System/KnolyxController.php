7348c8af5afeb1e9ce79c266ddf089fbd893b1e0
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-03-29 20:03:02 +0300

    createWebhook

--- src/Http/Controllers/System/KnolyxController.php
+++ src/Http/Controllers/System/KnolyxController.php
@@ -13,30 +13,35 @@ class KnolyxController extends Controller {
     public function webhookProcess(Request $r) {
 
 
-        $q = $r->all();
+        $q = [...$r->all()];
 		
 		$user = User::where('k_id', $q['data']['user']['id'])->first();
 		
 		$curs = Curs::where('k_id', $q['data']['course']['id'])->first();
 			
-		$cursuser = CustomerCursUser::where('user_id', $user->id)->where('curs_id', $curs->id)->update([
-			'status' => 'done',
-			'done_at' => \Carbon\Carbon::now()->format('Y-m-d'),
-		]);
+		$records = CustomerCursUser::where('user_id', $user->id)->where('curs_id', $curs->id)->get();
 
-        \Log::info($user->id . '#' . $curs->id . '#' . $cursuser);
 
-        return $cursuser;
+        foreach($record as $i => $record) 
+        {
+            $record->update([
+                'status' => 'done',
+                'done_at' => \Carbon\Carbon::now()->format('Y-m-d'),
+            ]);
 
-        // $params = [
-        //     "id" => "af46ec07-de79-40b5-916c-0058b54b2d2b", // unique id, messages with the same id should be ignored
-        //     "type" => "PING",
-        //     "data" => [
-        //         "message" => "PING"
-        //     ]
-        // ];
+            \Log::info($user->id . '#' . $curs->id . '#' . $record->id . '#' . $record->status . '#' . $record->done_at);
+        }
 
-        // return $params;
+        $params = [
+            "id" => "af46ec07-de79-40b5-916c-0058b54b2d2b", // unique id, messages with the same id should be ignored
+            "type" => "PING",
+            "data" => [
+                "message" => "PING"
+            ]
+        ];
+
+        return $params;
     }
 
 }
+
