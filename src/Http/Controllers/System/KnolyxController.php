47b68fc2199a6ac00a456aed793ae37f438af86b
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-03-29 19:27:13 +0300

    createWebhook

--- src/Http/Controllers/System/KnolyxController.php
+++ src/Http/Controllers/System/KnolyxController.php
@@ -9,15 +9,37 @@ use Illuminate\Http\Request;
 class KnolyxController extends Controller {
 
     public function webhookProcess(Request $r) {
-        $params = [
-            "id" => "af46ec07-de79-40b5-916c-0058b54b2d2b", // unique id, messages with the same id should be ignored
-            "type" => "PING",
-            "data" => [
-                "message" => "PING"
-            ]
+
+
+        $q = [
+            'id' => '8afa72f7-b20f-463a-9257-68a92d43db1e',
+            'type' => 'COURSE_COMPLETED',
+            'data' => [
+                'user' => [
+                    'id' => 38229,
+                    'name' => '[unknown] [unknown]',
+                    'email' => 'ovidiua.andrus@comptech.ro',
+                    'organizationId' => 173,
+                ],
+                'course' => [
+                    'id' => 2455,
+                    'name' => 'Intro to marketing',
+                ],
+            ],
         ];
 
-        return $params;
+
+        return $q;
+
+        // $params = [
+        //     "id" => "af46ec07-de79-40b5-916c-0058b54b2d2b", // unique id, messages with the same id should be ignored
+        //     "type" => "PING",
+        //     "data" => [
+        //         "message" => "PING"
+        //     ]
+        // ];
+
+        // return $params;
     }
 
 }
