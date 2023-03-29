307b6ea09abf095c27603b18ec54fe548b4a1f79
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-03-29 19:44:02 +0300

    createWebhook

--- src/Http/Controllers/System/KnolyxController.php
+++ src/Http/Controllers/System/KnolyxController.php
@@ -4,32 +4,27 @@ namespace MyDpo\Http\Controllers\System;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-// use MyDpo\Helpers\Response;
+use MyDpo\Models\User;
+use MyDpo\Models\Curs;
+use MyDpo\Models\CustomerCursUser;
 
 class KnolyxController extends Controller {
 
     public function webhookProcess(Request $r) {
 
 
-        $q = [
-            'id' => '8afa72f7-b20f-463a-9257-68a92d43db1e',
-            'type' => 'COURSE_COMPLETED',
-            'data' => [
-                'user' => [
-                    'id' => 38229,
-                    'name' => '[unknown] [unknown]',
-                    'email' => 'ovidiua.andrus@comptech.ro',
-                    'organizationId' => 173,
-                ],
-                'course' => [
-                    'id' => 2455,
-                    'name' => 'Intro to marketing',
-                ],
-            ],
-        ];
+        $q = $r->all();
+		
+		$user = User::where('k_id', $q['data']['user']['id'])->first();
+		
+		$curs = Curs::where('k_id', $q['data']['course']['id'])->first();
+			
+		$cursuser = CustomerCursUser::where('user_id', $user->id)->where('curs_id', $curs->id)->update([
+			'status' => 'done',
+			'done_at' => \Carbon\Carbon::now()->format('Y-m-d'),
+		]);
 
-
-        return $q;
+        return $cursuser;
 
         // $params = [
         //     "id" => "af46ec07-de79-40b5-916c-0058b54b2d2b", // unique id, messages with the same id should be ignored
