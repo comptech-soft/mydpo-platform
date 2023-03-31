afb2fc8abcf83440d789c1127a5167064383fec5
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-03-31 12:47:56 +0300

    Register

--- src/Performers/CustomerRegistruRow/ChangeStatus.php
+++ src/Performers/CustomerRegistruRow/ChangeStatus.php
@@ -10,13 +10,21 @@ class ChangeStatus extends Perform {
 
     public function Action() {
 
-        $row = CustomerRegistruRow::find($this->input['row_id']);
-        $row->status = $this->input['status'];
-        $row->save();
+        if(! is_array($this->input['row_id']) )
+        {
+            $this->input['row_id'] = [$this->input['row_id']];
+        }
 
-        $value = CustomerRegistruRowValue::where('row_id', $this->input['row_id'])->where('type', 'STATUS')->first();
-        $value->value = $this->input['status'];
-        $value->save();
+        foreach($this->input['row_id'] as $i => $row_id)
+        {
+            $row = CustomerRegistruRow::find($row_id);
+            $row->status = $this->input['status'];
+            $row->save();
 
+            $value = CustomerRegistruRowValue::where('row_id', $row_id)->where('type', 'STATUS')->first();
+            $value->value = $this->input['status'];
+            $value->save();
+        }
+        
     }
 }
