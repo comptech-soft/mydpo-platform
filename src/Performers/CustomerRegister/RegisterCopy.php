587a3c4bab4937787877c697c022985c3337faed
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-04-11 13:13:30 +0300

    Registre

--- src/Performers/CustomerRegister/RegisterCopy.php
+++ src/Performers/CustomerRegister/RegisterCopy.php
@@ -3,29 +3,67 @@
 namespace MyDpo\Performers\CustomerRegister;
 
 use MyDpo\Helpers\Perform;
-// use MyDpo\Models\CustomerRegistruUser;
+use MyDpo\Models\CustomerRegister;
+use MyDpo\Models\CustomerRegistruRow;
+use MyDpo\Models\CustomerRegistruRowValue;
 
 class RegisterCopy extends Perform {
 
     public function Action() {
 
-        dd($this->input);
+        $targetRegister = CustomerRegister::find($this->input['register_id']);
 
-        // CustomerRegistruUser::where('customer_registru_id', $this->input['customer_registru_id'])->delete();
+        $input = [
+            'responsabil_nume' => $this->input['responsabil_nume'],
+            'responsabil_functie' => $this->input['responsabil_functie'],
+            'customer_id' => $this->input['customer_id'],
+            'register_id' => $targetRegister->register_id,
+            'departament_id' => $this->input['departament_id'],
+            'number' => $this->input['number'],
+            'date' => $this->input['date'],
+            'status' => $this->input['status'],
+            'props' => $targetRegister->props,
+            'columns' => $targetRegister->columns,
+        ];
 
-        // if(array_key_exists('selected', $this->input))
-        // {
-        //     foreach($this->input['selected'] as $user_id => $departamente)
-        //     {
-        //         CustomerRegistruUser::create([
-        //             'customer_registru_id' => $this->input['customer_registru_id'],
-        //             'customer_id' => $this->input['customer_id'],
-        //             'register_id' => $this->input['register_id'],
-        //             'user_id' => $user_id,
-        //             'departamente' => $departamente,                    
-        //         ]);
-        //     }
-        // }
+        $createdRegister = CustomerRegister::create($input);
+
+        $targetRows = CustomerRegistruRow::where('customer_register_id', $targetRegister->id)->get();
+
+        foreach($targetRows as $i => $targetRow)
+        {
+
+            $values = $targetRow->values()->get();
+
+            $toCopy = false;
+            foreach($values as $j => $value)
+            {
+                if($value['type'] == 'DEPARTAMENT')
+                {
+                    
+                    if( in_array($value->value, $this->input['selectedDepartamente']) )
+                    {
+                        $toCopy = true;
+                    }
+                }
+            }
+
+            if($toCopy)
+            {
+
+                $createdRow = $targetRow->replicate();
+                $createdRow->customer_register_id = $createdRegister->id;
+                $createdRow->save();
+                foreach($values as $j => $value)
+                {
+                    $createValue = $value->replicate();
+                    $createValue->row_id = $createdRow->id;
+                    $createValue->save();
+                }
+            }
+
+        }
+
     }
     
 }
