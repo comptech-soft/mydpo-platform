90ca1a2e087b4ff2f834a1d32119aa2f32fe16b7
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-04-11 09:10:40 +0300

    Registre

--- src/Performers/CustomerRegistruRow/DeleteRows.php
+++ src/Performers/CustomerRegistruRow/DeleteRows.php
@@ -11,23 +11,18 @@ class DeleteRows extends Perform {
     public function Action() {
 
 
-        dd($this->input);
+       
 
-        // if(! is_array($this->input['row_id']) )
-        // {
-        //     $this->input['row_id'] = [$this->input['row_id']];
-        // }
+        if(! is_array($this->input['row_id']) )
+        {
+            $this->input['row_id'] = [$this->input['row_id']];
+        }
 
-        // foreach($this->input['row_id'] as $i => $row_id)
-        // {
-        //     $row = CustomerRegistruRow::find($row_id);
-        //     $row->status = $this->input['status'];
-        //     $row->save();
-
-        //     $value = CustomerRegistruRowValue::where('row_id', $row_id)->where('type', 'STATUS')->first();
-        //     $value->value = $this->input['status'];
-        //     $value->save();
-        // }
+        foreach($this->input['row_id'] as $i => $row_id)
+        {
+            $row = CustomerRegistruRow::find($row_id);
+            $row->deleteRowWithValues();
+        }
         
     }
 }
