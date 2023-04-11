d0a4a9d326cb61903cfa20f1b9090b5deb9333d5
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-04-11 09:12:16 +0300

    Registre

--- src/Performers/CustomerRegistruRow/DeleteRows.php
+++ src/Performers/CustomerRegistruRow/DeleteRows.php
@@ -3,10 +3,12 @@
 namespace MyDpo\Performers\CustomerRegistruRow;
 
 use MyDpo\Helpers\Perform;
+use MyDpo\Models\CustomerRegistruRow;
 
 class DeleteRows extends Perform {
 
     public function Action() {
+        
         if(! is_array($this->input['row_id']) )
         {
             $this->input['row_id'] = [$this->input['row_id']];
