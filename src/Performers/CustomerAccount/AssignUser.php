f470f8a9e2df2b95e197240be2f23bce65c7330b
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-04-15 16:34:41 +0300

    Utilizatori platforma

--- src/Performers/CustomerAccount/AssignUser.php
+++ src/Performers/CustomerAccount/AssignUser.php
@@ -6,11 +6,36 @@ use MyDpo\Helpers\Perform;
 use MyDpo\Models\RoleUser;
 use MyDpo\Models\Activation;
 use MyDpo\Models\CustomerAccount;
+use MyDpo\Events\CustomerPersons\CustomerPersonCreateAccount;
 
 class AssignUser extends Perform {
 
     public function Action() {
 
-        dd($this->input);
+        $accountInput = [
+            'customer_id' => $this->input['customer_id'],
+            'user_id' => $this->input['user_id'],
+            'department_id' => $this->input['department_id'],
+            'newsletter' => $this->input['newsletter'],
+            'locale' => $this->input['locale'],
+            'role_id' => $this->input['role_id'],
+        ];
+
+        $account = CustomerAccount::create($accountInput);
+
+        $roleUser = RoleUser::CreateAccountRole(
+            $this->input['customer_id'], 
+            $accountInput['user_id'], 
+            $account->role_id,
+        );
+
+        event(new CustomerPersonCreateAccount([
+            ...$accountInput,
+            'account' => $account,
+            'roleUser' => $roleUser,
+        ]));
+
+        $this->payload = $account;
+
     }
 }
