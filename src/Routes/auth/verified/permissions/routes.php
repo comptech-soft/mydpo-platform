99180068957b161e4f7b6bd49effb11d3fefc342
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-04-16 19:16:10 +0300

    Updates

---
+++ src/Routes/auth/verified/permissions/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use MyDpo\Http\Controllers\Admin\PermissionsController;
+
+Route::prefix('permissions')->group( function() {
+
+    Route::get('/', [PermissionsController::class, 'index']);       
+    Route::post('items', [PermissionsController::class, 'getItems']);
+    Route::post('action/{action}', [PermissionsController::class, 'doAction']);
+
+});
