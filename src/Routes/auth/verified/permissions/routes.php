175e7652d80b6b4c6a72cab70babde928cc7d7df
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-04-25 20:11:48 +0300

    Centralizatoare

--- src/Routes/auth/verified/permissions/routes.php
+++ src/Routes/auth/verified/permissions/routes.php
@@ -7,5 +7,6 @@ Route::prefix('permissions')->group( function() {
     Route::get('/', [PermissionsController::class, 'index']);       
     Route::post('items', [PermissionsController::class, 'getItems']);
     Route::post('action/{action}', [PermissionsController::class, 'doAction']);
+    Route::post('reorder', [PermissionsController::class, 'reorder']);
 
 });
