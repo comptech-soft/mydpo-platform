c6392d7565ee25cd974c777cb7ed3c79f6f44e56
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-05-07 18:43:10 +0300

    Centralizatoare

--- src/Models/Centralizator.php
+++ src/Models/Centralizator.php
@@ -69,6 +69,53 @@ class Centralizator extends Model {
         ;
     }
 
+    public static function doInsert($input, $record) {
+
+        $record = self::create($input);
+
+        foreach($input['body'] as $key => $value)
+        {
+            if($value == 1)
+            {
+                $record->{'AddColumn' . ucfirst($key)}();
+            }
+        }
+    }
+
+    public function AddColumnVizibilitate() {
+
+        $this->AddColumn('Vizibilitate', 'VISIBILITY', -100, 100);
+        
+        
+    }
+
+    public function AddColumn($caption, $type, $order_no, $width) {
+        
+        $input = [
+            'centralizator_id' => $this->id,
+            'caption' => $caption,
+            'slug' => md5($caption . time()),
+            'is_group' => 0,
+            'group_id' => NULL,
+            'type' =>  $type,
+            'order_no' => $order_no,
+            'width' => $width,
+        ];
+
+        $record = CentralizatorColoana::where('centralizator_id', $this->id)->where('type', 'VISIBILITY')->first();
+
+        if(!! $record)
+        {
+            $record->update($input);
+        }
+        else
+        {
+            $record = CentralizatorColoana::create($input);
+        }
+
+    }
+
+
     public static function doDelete($input, $record) {
 
         CentralizatorColoana::where('centralizator_id', $record->id)->delete();
