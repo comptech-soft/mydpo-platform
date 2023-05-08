6eef0943fcaf74d932408ca4db9c3263bf267875
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-05-09 02:11:07 +0300

    Centralizatoare

--- src/Routes/auth/verified/customers-centralizatoare/routes.php
+++ src/Routes/auth/verified/customers-centralizatoare/routes.php
@@ -2,15 +2,15 @@
 
 use MyDpo\Http\Controllers\Auth\CustomersCentralizatoareController;
 
-Route::middleware('valid-customer')->prefix('/customer-centralizatoare')->group( function() {
+Route::middleware('valid-customer')->prefix('/customer-centralizatoare-dashboard')->group( function() {
 
     Route::middleware('is-activated')->get('/{customer_id}', [CustomersCentralizatoareController::class, 'index']);
 
 });
 
-Route::prefix('customers-centralizatoare')->group( function() {
+// Route::prefix('customers-centralizatoare')->group( function() {
 
-    Route::post('items', [CustomersCentralizatoareController::class, 'getItems']);
-    Route::post('get-summary', [CustomersCentralizatoareController::class, 'getSummary']);
+//     Route::post('items', [CustomersCentralizatoareController::class, 'getItems']);
+//     Route::post('get-summary', [CustomersCentralizatoareController::class, 'getSummary']);
 
-});
+// });
