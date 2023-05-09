133446b60cff6575b1fca557b69cb162d26d8be5
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-05-09 09:19:16 +0300

    Centralizatoare

---
+++ src/Http/Controllers/Auth/CustomerCentralizatorController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Helpers\Response;
+// use MyDpo\Models\CustomerCentralizator;
+
+class CustomersCentralizatoareController extends Controller {
+    
+    public function index($customer_id, $centralizator_id, Request $r) {
+
+        return Response::View(
+            '~templates.index', 
+            asset('apps/customer-centralizator/index.js'),
+            [], 
+            [
+                'customer_id' => $customer_id,
+                'centralizator_id' => $centralizator_id,
+            ]
+        );
+    }
+
+    // public function getItems(Request $r) {
+    //     return CustomerCentralizator::getItems($r->all());
+    // }
+
+    // public function getSummary(Request $r) {
+    //     return CustomerCentralizator::getSummary($r->all());
+    // }
+    
+}
--- src/Routes/auth/verified/customers-centralizatoare/routes.php
+++ src/Routes/auth/verified/customers-centralizatoare/routes.php
@@ -1,10 +1,17 @@
 <?php
 
 use MyDpo\Http\Controllers\Auth\CustomersCentralizatoareController;
+use MyDpo\Http\Controllers\Auth\CustomerCentralizatorController;
 
-Route::middleware('valid-customer')->prefix('/customer-centralizatoare-dashboard')->group( function() {
+Route::middleware('valid-customer')->prefix('customer-centralizatoare-dashboard')->group( function() {
 
-    Route::middleware('is-activated')->get('/{customer_id}', [CustomersCentralizatoareController::class, 'index']);
+    Route::middleware('is-activated')->get('{customer_id}', [CustomersCentralizatoareController::class, 'index']);
+
+});
+
+Route::middleware('valid-customer')->prefix('customer-centralizator')->group( function() {
+
+    Route::middleware('is-activated')->get('{customer_id}/{centralizator_id}', [CustomerCentralizatorController::class, 'index']);
 
 });
 
