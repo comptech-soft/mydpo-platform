cb628178d310f7cfe260149f759f8ed727512828
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-05-18 15:33:59 +0300

    Centralizatoare

--- src/Performers/CustomerCentralizatorRow/SetRowsVisibility.php
+++ src/Performers/CustomerCentralizatorRow/SetRowsVisibility.php
@@ -4,7 +4,9 @@ namespace MyDpo\Performers\CustomerCentralizatorRow;
 
 use MyDpo\Helpers\Perform;
 use MyDpo\Models\CustomerCentralizator;
+use MyDpo\Models\CustomerCentralizatorRow;
 use MyDpo\Models\CustomerCentralizatorRowValue;
+use Carbon\Carbon;
 
 class SetRowsVisibility extends Perform {
 
@@ -14,6 +16,37 @@ class SetRowsVisibility extends Perform {
 
         if(!! count($this->selected_rows) )
         {
+
+            $rows = CustomerCentralizatorRow::whereIn('id', $this->selected_rows)->get();
+
+            foreach($rows as $i => $row)
+            {
+
+                $props = !! $row->props ? $row->props : []; 
+                $props = [
+                    ...$props,
+                    'action' => [
+                        'name' => 'visibility',
+                        'action_at' => Carbon::now()->format('Y-m-d'),
+                        'tooltip' => 'Setat ' . ($this->visibility == 1 ? 'public' : 'privat') . ' de :user_full_name la :action_at. (:customer_name)',
+                        'user' => [
+                            'id' => \Auth::user()->id,
+                            'full_name' => \Auth::user()->full_name,
+                            'role' => [
+                                'name' => \Auth::user()->role->name,
+                            ]
+                        ],
+                        'customer' => [
+                            'id' => $this->customer_id,
+                            'name' => $this->customer,
+                        ],
+                    ],
+                ];
+
+                $row->props = $props;
+                $row->save();
+            }
+
             $customer_centralizator = CustomerCentralizator::find($this->customer_centralizator_id);
 
             $records = CustomerCentralizatorRowValue::where('column_id', $customer_centralizator->visible_column_id)
