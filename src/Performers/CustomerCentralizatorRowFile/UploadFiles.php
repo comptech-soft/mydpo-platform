25c4bb52d6c375e227004333905d8f179184e76b
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-05-18 23:20:12 +0300

    Centralizatoare

--- src/Performers/CustomerCentralizatorRowFile/UploadFiles.php
+++ src/Performers/CustomerCentralizatorRowFile/UploadFiles.php
@@ -3,7 +3,7 @@
 namespace MyDpo\Performers\CustomerCentralizatorRowFile;
 
 use MyDpo\Helpers\Perform;
-// use MyDpo\Models\CustomerRegistruRowFile;
+use MyDpo\Models\CustomerCentralizatorRowFile;
 
 class UploadFiles extends Perform {
 
@@ -16,10 +16,46 @@ class UploadFiles extends Perform {
                 $this->attachFile($file);
             }
         }
+
+        $this->payload = [
+            'record' => NULL,
+        ];
     }
 
     protected function attachFile($file) {
 
-        dd($file);
+        $ext = strtolower($file->extension());
+
+        $filename = md5(time()) . '-' . \Str::slug(str_replace($file->extension(), '', $file->getClientOriginalName())) . '.' .  strtolower($file->extension());
+
+        $result = $file->storeAs('registre-rows/' .  \Auth::user()->id, $filename, 's3');
+
+        $input = [
+            'file_original_name' => $file->getClientOriginalName(),
+            'file_original_extension' => $file->extension(),
+            'file_full_name' => $filename,
+            'file_mime_type' => $file->getMimeType(),
+            'file_upload_ip' => request()->ip(),
+            'file_size' => $file->getSize(),
+            'url' => config('filesystems.disks.s3.url') . $result,
+            'created_by' => \Auth::user()->id,
+        ];
+
+        if(in_array($ext, ['jpg', 'jpeg', 'png']))
+        {
+            $image = \Image::make($file);
+            $input = [
+                ...$input,
+                'file_size' => $image->filesize(),
+                'file_width' => $image->width(),
+                'file_height' => $image->height(),
+            ];
+        }
+
+        CustomerCentralizatorRowFile::create([
+            'row_id' => $this->row_id,
+            'file' => $input,
+            'created_by' => \Auth::user()->id,
+        ]);
     }
 }
