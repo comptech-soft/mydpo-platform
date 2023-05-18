ad8cbdeb0e1d66788507d00da3cce3876164e5f3
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-05-18 09:46:42 +0300

    Centralizatoare

--- src/Performers/CustomerCentralizator/Import.php
+++ src/Performers/CustomerCentralizator/Import.php
@@ -4,43 +4,15 @@ namespace MyDpo\Performers\CustomerCentralizator;
 
 use MyDpo\Helpers\Perform;
 use MyDpo\Exports\CustomerCentralizator\Exporter;
-// use MyDpo\Models\CustomerCentralizator;
+use MyDpo\Imports\CustomerCentralizator\Importer;
 
 class Import extends Perform {
 
     public function Action() {
 
-        dd($this->input);
+        $importer = new Importer($this->input);
 
-        // // $departamente_ids = [];
-        // // if( array_key_exists('departamente_ids', $this->input) )
-        // // {
-        // //     $departamente_ids = $this->input['departamente_ids'];
-        // // }
-
-        // $exporter = new Exporter($this->department_ids, $this->id);
-
-        // // dd($this->file_name);
-
-        // // $this->createUserFolder();
-
-        // $file_name = 'public/exports/' . \Auth::user()->id. '/' . $this->file_name;
-
-        // $file = \Str::replace('public', 'storage', $file_name);
-
-
-
-        // // dd(asset($file_name));
-
-        // // $file = \Str::replace('public', 'storage', $filename);
-
-        // \Excel::store($exporter, $file_name, NULL, NULL, ['visibility' => 'public']);
-
-        // $this->payload = [
-        //     'record' => [
-        //         'url' => asset($file),
-        //     ],
-        // ];
+        \Excel::import($importer, $this->file);
     
     }
 }
