4c8bb04393ed099fcda5a7e2c8ce0b903ca40129
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-05-21 11:12:03 +0300

    Centralizatoare

--- src/Models/CustomerAccount.php
+++ src/Models/CustomerAccount.php
@@ -4,6 +4,7 @@ namespace MyDpo\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use MyDpo\Helpers\Performers\Datatable\GetItems;
+use MyDpo\Traits\Itemable;
 use MyDpo\Helpers\Performers\Datatable\DoAction;
 use MyDpo\Models\User;
 use MyDpo\Models\Customer;
