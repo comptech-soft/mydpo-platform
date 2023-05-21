c4c93a12ba3eecae7f7ffe4738221b3e6e3415bb
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-05-21 11:09:11 +0300

    Centralizatoare

--- src/Routes/api/routes.php
+++ src/Routes/api/routes.php
@@ -6,6 +6,7 @@ Route::middleware('verified')->group(function () {
 
     require __DIR__ . '/centralizatoare/routes.php';
 
+    require __DIR__ . '/customer-accounts/routes.php';
     require __DIR__ . '/customer-departamente/routes.php';
     require __DIR__ . '/customer-centralizatoare/routes.php';
 
