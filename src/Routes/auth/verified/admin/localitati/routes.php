1c0f2071fbce48d5bddffb061f10a2cdb0378ae0
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-05-26 08:56:04 +0300

    Centralizatoare

--- src/Routes/auth/verified/admin/localitati/routes.php
+++ src/Routes/auth/verified/admin/localitati/routes.php
@@ -9,6 +9,7 @@ Route::prefix('localitati')->group( function() {
     Route::get('/', [LocalitatiController::class, 'index']);        
     Route::post('items', [LocalitatiController::class, 'getItems']);
     Route::post('get-items', [LocalitatiController::class, 'getRecords']);
+    Route::post('action/{action}', [LocalitatiController::class, 'doAction']);
 
 });
 
@@ -16,6 +17,7 @@ Route::prefix('countries')->group( function() {
     
     Route::post('items', [CountriesController::class, 'getItems']);
     Route::post('get-items', [CountriesController::class, 'getRecords']);
+    Route::post('action/{action}', [CountriesController::class, 'doAction']);
 
 });
 
@@ -23,5 +25,6 @@ Route::prefix('regions')->group( function() {
     
     Route::post('items', [RegionsController::class, 'getItems']);
     Route::post('get-items', [RegionsController::class, 'getRecords']);
+    Route::post('action/{action}', [RegionsController::class, 'doAction']);
 
 });
