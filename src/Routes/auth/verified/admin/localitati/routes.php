bd69009e0705b523c89a346d907d85a052af9044
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-05-26 08:30:53 +0300

    Centralizatoare

--- src/Routes/auth/verified/admin/localitati/routes.php
+++ src/Routes/auth/verified/admin/localitati/routes.php
@@ -8,17 +8,20 @@ Route::prefix('localitati')->group( function() {
         
     Route::get('/', [LocalitatiController::class, 'index']);        
     Route::post('items', [LocalitatiController::class, 'getItems']);
+    Route::post('get-items', [LocalitatiController::class, 'getRecords']);
 
 });
 
 Route::prefix('countries')->group( function() {
     
     Route::post('items', [CountriesController::class, 'getItems']);
+    Route::post('get-items', [CountriesController::class, 'getRecords']);
 
 });
 
 Route::prefix('regions')->group( function() {
     
     Route::post('items', [RegionsController::class, 'getItems']);
+    Route::post('get-items', [RegionsController::class, 'getRecords']);
 
 });
