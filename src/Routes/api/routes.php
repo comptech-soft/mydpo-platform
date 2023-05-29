9377f1abbbe2e8948e2272ac41976b353d0dd317
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-05-29 21:32:27 +0300

    Plan de conformare

--- src/Routes/api/routes.php
+++ src/Routes/api/routes.php
@@ -2,13 +2,12 @@
 
 Route::middleware('verified')->group(function () {
 
-    require __DIR__ . '/customers/routes.php';
-
     require __DIR__ . '/centralizatoare/routes.php';
-
-    require __DIR__ . '/customer-notifications/routes.php';
     require __DIR__ . '/customer-accounts/routes.php';
-    require __DIR__ . '/customer-departamente/routes.php';
     require __DIR__ . '/customer-centralizatoare/routes.php';
-
+    require __DIR__ . '/customer-departamente/routes.php';
+    require __DIR__ . '/customer-notifications/routes.php';
+    require __DIR__ . '/customers/routes.php';
+    require __DIR__ . '/plan-conformare/routes.php';
+    
 });
