edb24f047c2360746fe8e0cbfb948bce6a7afd33
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-02 02:04:29 +0300

    System

--- src/Routes/auth/verified/customers-centralizatoare/routes.php
+++ src/Routes/auth/verified/customers-centralizatoare/routes.php
@@ -3,21 +3,14 @@
 use MyDpo\Http\Controllers\Auth\CustomersCentralizatoareController;
 use MyDpo\Http\Controllers\Auth\CustomerCentralizatorController;
 
-Route::middleware('valid-customer')->prefix('customer-centralizatoare-dashboard')->group( function() {
+// Route::middleware('valid-customer')->prefix('customer-centralizatoare-dashboard')->group( function() {
 
-    Route::middleware('is-activated')->get('{customer_id}', [CustomersCentralizatoareController::class, 'index']);
+//     Route::middleware('is-activated')->get('{customer_id}', [CustomersCentralizatoareController::class, 'index']);
 
-});
+// });
 
-Route::middleware('valid-customer')->prefix('customer-centralizator')->group( function() {
+// Route::middleware('valid-customer')->prefix('customer-centralizator')->group( function() {
 
-    Route::middleware('is-activated')->get('{customer_id}/{centralizator_id}', [CustomerCentralizatorController::class, 'index']);
-
-});
-
-// Route::prefix('customers-centralizatoare')->group( function() {
-
-//     Route::post('items', [CustomersCentralizatoareController::class, 'getItems']);
-//     Route::post('get-summary', [CustomersCentralizatoareController::class, 'getSummary']);
+//     Route::middleware('is-activated')->get('{customer_id}/{centralizator_id}', [CustomerCentralizatorController::class, 'index']);
 
 // });
