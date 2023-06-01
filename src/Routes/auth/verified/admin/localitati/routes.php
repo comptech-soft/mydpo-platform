a8a8ef0e9cbdee786442d6585cf3d5e6b766b17c
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-02 00:39:24 +0300

    System

--- src/Routes/auth/verified/admin/localitati/routes.php
+++ src/Routes/auth/verified/admin/localitati/routes.php
@@ -7,24 +7,17 @@ use MyDpo\Http\Controllers\Admin\LocalitatiController;
 Route::prefix('localitati')->group( function() {
         
     // Route::get('/', [LocalitatiController::class, 'index']);        
-    Route::post('items', [LocalitatiController::class, 'getItems']);
-    Route::post('get-items', [LocalitatiController::class, 'getRecords']);
-    Route::post('action/{action}', [LocalitatiController::class, 'doAction']);
+    // Route::post('items', [LocalitatiController::class, 'getItems']);
+    // Route::post('get-items', [LocalitatiController::class, 'getRecords']);
+    // Route::post('action/{action}', [LocalitatiController::class, 'doAction']);
 
 });
 
-Route::prefix('countries')->group( function() {
-    
-    // Route::post('items', [CountriesController::class, 'getItems']);
-    // Route::post('get-items', [CountriesController::class, 'getRecords']);
-    // Route::post('action/{action}', [CountriesController::class, 'doAction']);
-
-});
 
 Route::prefix('regions')->group( function() {
     
-    Route::post('items', [RegionsController::class, 'getItems']);
-    Route::post('get-items', [RegionsController::class, 'getRecords']);
-    Route::post('action/{action}', [RegionsController::class, 'doAction']);
+    // Route::post('items', [RegionsController::class, 'getItems']);
+    // Route::post('get-items', [RegionsController::class, 'getRecords']);
+    // Route::post('action/{action}', [RegionsController::class, 'doAction']);
 
 });
