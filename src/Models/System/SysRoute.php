0a081c8b9278eabb8470fc3b422b011a1a0ab630
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-01 23:24:17 +0300

    System

---
+++ src/Models/System/SysRoute.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace MyDpo\Models\System;
+
+use Illuminate\Database\Eloquent\Model;
+// use MyDpo\Helpers\Performers\Datatable\GetItems;
+// use MyDpo\Helpers\Performers\Datatable\DoAction;
+// use MyDpo\Traits\Itemable;
+// use MyDpo\Models\Region;
+
+class SysRoute extends Model {
+
+    use Itemable;
+    
+    protected $table = 'system-routes';
+
+    protected $fillable = [
+        'id',
+        'slug',
+        'type',
+        'name',
+        'prefix',
+        'middleware',
+        'path',
+        'protocol',
+        'controller',
+        'method',
+        'description',
+        'props',
+        'created_by',
+        'updated_by'
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'middleware' => 'json',
+        'props' => 'json',
+        'created_by' => 'integer',
+        'updated_by' => 'integer',
+    ];
+
+    // public function region() {
+    //     return $this->belongsTo(Region::class, 'region_id');
+    // }
+
+    // public static function getItems($input) {
+    //     return (new GetItems($input, self::query(), __CLASS__))->Perform();
+    // }
+
+    // public static function doAction($action, $input) {
+    //     return (new DoAction($action, $input, __CLASS__))->Perform();
+    // }
+    
+}
