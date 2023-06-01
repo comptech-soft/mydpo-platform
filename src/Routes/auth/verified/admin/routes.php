96037afb21cc06846834934117f791f3e04a28dd
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-02 01:57:31 +0300

    System

--- src/Routes/auth/verified/admin/routes.php
+++ src/Routes/auth/verified/admin/routes.php
@@ -18,7 +18,6 @@ Route::middleware(['isadmin'])->prefix('admin')->group(function () {
     require __DIR__ . '/planificare/routes.php';
     require __DIR__ . '/timesheet/routes.php';
 
-    require __DIR__ . '/centralizatoare/routes.php';
     require __DIR__ . '/plan-conformare/routes.php';
     require __DIR__ . '/chestionare/routes.php';
     require __DIR__ . '/cursuri/routes.php';
