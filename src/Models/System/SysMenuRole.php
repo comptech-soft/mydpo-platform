a922d69de291b7f6c929c9bcda6c15c6178dffaf
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-02 13:35:59 +0300

    System

---
+++ src/Models/System/SysMenuRole.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MyDpo\Models\System;
+
+use Illuminate\Database\Eloquent\Model;
+// use Kalnoy\Nestedset\NodeTrait;
+// use MyDpo\Helpers\Performers\Datatable\DoAction;
+// use MyDpo\Traits\Itemable;
+// use MyDpo\Models\Region;
+
+class SysMenuRole extends Model {
+
+    // use NodeTrait, Itemable;
+    
+    protected $table = 'system-menus-roles';
+
+    protected $fillable = [
+        'id',
+        'menu_id',
+        'role_id',
+        'customer_id',
+        'visible',
+        'disabled',
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'menu_id' => 'integer',
+        'role_id' => 'integer',
+        'customer_id' => 'integer',
+        'visible' => 'integer',
+        'disabled' => 'integer',
+    ];
+
+
+}
