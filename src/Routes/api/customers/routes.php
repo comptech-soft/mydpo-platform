b2f378e1bb6897d8236d7d6b7161908c41207da1
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-02 20:25:15 +0300

    System

--- src/Routes/api/customers/routes.php
+++ src/Routes/api/customers/routes.php
@@ -4,6 +4,6 @@ use MyDpo\Http\Controllers\Admin\CustomersController;
 
 Route::prefix('customers')->group( function() {
             
-    Route::post('get-records', [CustomersController::class, 'getRecords']);
+    // Route::post('get-records', [CustomersController::class, 'getRecords']);
 
 });
