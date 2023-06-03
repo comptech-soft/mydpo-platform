00949609994e0fc3f0df61d116b1b814445bf768
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-03 08:04:19 +0300

    System

---
+++ src/Models/System/SysAction.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MyDpo\Models\System;
+
+use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
+use MyDpo\Traits\Itemable;
+
+class SysAction extends Model {
+
+    use NodeTrait, Itemable;
+    
+    protected $table = 'system-menus';
+
+    protected $fillable = [
+        'id',
+        'slug',
+        'type',
+        'platform',
+        'order_no',
+        'caption',
+        'description',
+        'props',
+        'created_by',
+        'updated_by'
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'platform' => 'json',
+        'props' => 'json',
+        'order_no' => 'integer',
+        'created_by' => 'integer',
+        'updated_by' => 'integer',
+    ];
+
+    protected $with = [
+        'children',
+    ];
+
+    
+
+}
