e2c815c495bbf4761cc0e58d30f62d53af304c8c
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-03 10:18:42 +0300

    System

---
+++ src/Models/System/SysActionRole.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MyDpo\Models\System;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SysActionRole extends Model {
+
+   
+    protected $table = 'system-actions-roles';
+
+    protected $fillable = [
+        'id',
+        'action_id',
+        'role_id',
+        'customer_id',
+        'platform',
+        'visible',
+        'disabled',
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'action_id' => 'integer',
+        'role_id' => 'integer',
+        'customer_id' => 'integer',
+        'visible' => 'integer',
+        'disabled' => 'integer',
+    ];
+
+
+}
--- src/Performers/SysMenu/GetVisibilities.php
+++ src/Performers/SysMenu/GetVisibilities.php
@@ -4,15 +4,17 @@ namespace MyDpo\Performers\SysMenu;
 
 use MyDpo\Helpers\Perform;
 use MyDpo\Models\System\SysMenuRole;
-
+use MyDpo\Models\System\SysActionRole;
 
 class GetVisibilities extends Perform {
 
     public function Action() {
-        
-        $records = SysMenuRole::where('role_id', $this->role_id)->wherePlatform($this->platform)->get();
 
-        $this->payload = $records;
+        $this->payload = [
+            'menus' => SysMenuRole::where('role_id', $this->role_id)->wherePlatform($this->platform)->get(),
+            'actions' => SysActionRole::where('role_id', $this->role_id)->get(),
+            
+        ];
     }
 
 }
