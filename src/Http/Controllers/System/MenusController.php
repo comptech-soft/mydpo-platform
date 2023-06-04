e4089add41eedcea9845a5425900d4ee6a211011
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-04 22:54:11 +0300

    System

--- src/Http/Controllers/System/MenusController.php
+++ src/Http/Controllers/System/MenusController.php
@@ -30,5 +30,9 @@ class MenusController extends Controller {
         return SysMenu::getVisibilities($r->all());
     }
 
+    public function reorder(Request $r) {
+        return SysMenu::reorder($r->all());
+    }
+
 
 }
