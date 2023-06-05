89fed358ece5ea36dab0763352a2689819cd9603
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-05 08:41:00 +0300

    System

--- src/Http/Controllers/System/MenusRolesController.php
+++ src/Http/Controllers/System/MenusRolesController.php
@@ -9,6 +9,10 @@ use MyDpo\Models\System\SysMenuRole;
 
 class MenusRolesController extends Controller {
 
+    public function getRecords(Request $r) {
+        return SysMenuRole::getRecords($r->all());
+    }
+
     public function doAction($action, Request $r) {
         return SysMenuRole::doAction($action, $r->all());
     }
