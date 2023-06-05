d9cd1f0b0e74cf54c1e74ced2dae6c7d20fc319d
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-05 06:57:10 +0300

    System

---
+++ src/Models/System/SysRole.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MyDpo\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Model;
+use MyDpo\Traits\Itemable;
+
+class SysRole extends Model {
+
+    use Itemable;
+
+    protected $table = 'roles';
+
+    protected $casts = [
+        'id' => 'integer',
+        'permissions' => 'json',
+        'editable' => 'integer',
+        'deletable' => 'integer',
+        'created_by' => 'integer',
+        'updated_by' => 'integer',
+    ];
+
+    protected $fillable = [
+        'id',
+        'name',
+        'slug',
+        'permissions',
+        'type',
+        'color',
+        'editable',
+        'deleteabe',
+        'created_by',
+        'updated_by'
+    ];
+
+}
