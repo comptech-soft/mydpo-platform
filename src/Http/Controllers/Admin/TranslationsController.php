2b0aea069b4081ef43ba60635f0416f4aa5c1ea7
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-15 11:10:05 +0300

    Menus

--- src/Http/Controllers/Admin/TranslationsController.php
+++ src/Http/Controllers/Admin/TranslationsController.php
@@ -4,35 +4,36 @@ namespace MyDpo\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use MyDpo\Helpers\Response;
+use MyDpo\Core\Http\Response\Index;
 use MyDpo\Models\Translation;
 
 class TranslationsController extends Controller {
     
     public function index(Request $r) {
 
-        return Response::View(
-            '~templates.index', 
-            asset('apps/translations/index.js'),
-            [], 
-            []
+
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/admin/translations/index.js']
         );
+
+        
     }
 
-    public function createFile(Request $r) {
-        return Translation::createFile($r->all());
-    }
+    // public function createFile(Request $r) {
+    //     return Translation::createFile($r->all());
+    // }
 
-    public function activate(Request $r) {
-        return Translation::activate($r->all());
-    }
+    // public function activate(Request $r) {
+    //     return Translation::activate($r->all());
+    // }
 
-    public function getItems(Request $r) {
-        return Translation::getItems($r->all());
-    }
+    // public function getItems(Request $r) {
+    //     return Translation::getItems($r->all());
+    // }
     
-    public function doAction($action, Request $r) {
-        return Translation::doAction($action, $r->all());
-    }
+    // public function doAction($action, Request $r) {
+    //     return Translation::doAction($action, $r->all());
+    // }
 
 }
