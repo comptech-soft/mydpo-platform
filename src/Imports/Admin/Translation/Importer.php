afd1a3bad84f268129a108a763bce72fe62d6945
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-17 22:27:43 +0300

    Menus

--- src/Imports/Admin/Translation/Importer.php
+++ src/Imports/Admin/Translation/Importer.php
@@ -4,8 +4,11 @@ namespace MyDpo\Imports\Admin\Translation;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use MyDpo\Models\System\Translation; 
 
 class Importer implements ToCollection {
+
+    protected $lines = NULL;
 
     public function __construct($input) {
         $this->input = $input;
@@ -13,66 +16,85 @@ class Importer implements ToCollection {
 
     public function collection(Collection $rows) {
 
-        dd($rows);
-        
-        foreach($rows as $i => $row)
-        {
-            if($i > 1)
-            {
-                $this->processRow($row);
-            }
-        }
+        $this->CreateLines($rows);
+
+        $this->Process();
     }
 
-    private function processRow($row) {
+    private function ValidRecord($record) {
+        
+        return !! $record['ro'];
+    }
 
-        $rowinput = [
-            'customer_register_id' => $this->registru->id,
-            'customer_id' => $this->registru->customer_id,
-            'register_id' => $this->registru->props['registru']['id'],
-            'status' => 'protected',
-            'customer' => config('app.platform') == 'b2b' ? Customer::find($this->registru->customer_id)->name : 'Decalex',
-            'createdby' => \Auth::user()->full_name,
+    private function RowToRecord($row) {
+        return [
+            'ro' => $row[1],
+            'en' => $row[2],
+            '__line' => $row['__line']
         ];
+    }
 
-        $rowrecord = CustomerRegistruRow::create($rowinput);
+    private function ProcessLine($line) {
 
-        $i = 0;
-        foreach($this->registru->real_columns as $column_id => $column)
+        $input = collect($line)->except(['__line'])->toArray();
+
+        $record = Translation::where('ro', $input['ro'])->first();
+
+        if( !! $record )
         {
-            $valueinput = [
-                'row_id' => $rowrecord->id,
-                'column_id' => $column_id,
-                'value' => $row[$i],
-                'type' => $column['type'],
-            ];
-
-            if($column['type'] == 'DEPARTAMENT')
-            {
-                $valueinput = [
-                    ...$valueinput,
-                    'value' => array_key_exists($valueinput['value'], $this->departamente) ? $this->departamente[$valueinput['value']] : NULL,
-                ];
-                
-            }
-            else
-            {
-                if($column['type'] == 'O')
-                {
-                    $options = collect($column['props']['options'])->pluck('value', 'text')->toArray();
-
-                    $valueinput = [
-                        ...$valueinput,
-                        'value' => array_key_exists($valueinput['value'], $options) ? $options[$valueinput['value']] : NULL,
-                    ];
-                }
-            }
-
-            CustomerRegistruRowValue::create($valueinput);
-
-            $i++;
+            $record->update($input);
+        }
+        else
+        {
+            $record = Translation::create($input);
         }
 
     }    
+
+    protected function Process() {
+        foreach($this->lines as $i => $line) 
+        {
+            $this->ProcessLine($line);
+        }
+    }
+
+    protected function CreateLines($rows) {
+        /**
+         * Din input se ia start_row = linia de inceput
+         */
+        $start_row = 1 * $this->input['start_row'];
+
+        $this->lines = $rows->map(function($row, $i){
+
+            /**
+             * Se ataseaza numarul de linie
+             */
+            return [
+                ...$row,
+                '__line' => $i,
+            ];
+
+        })->filter( function($row, $i) use ($start_row) {
+            /**
+             * Se incepe de la linia $start_row
+             */
+            return $i >= $start_row; 
+        
+        })->map( function($row, $i) {
+
+            /**
+             * Se convertesc randurile la tabela din BD
+             */
+            return $this->RowToRecord($row);
+        
+        })->filter( function($record, $i) {
+
+            /**
+             * Logica de validare a recordurilor
+             */
+            return $this->ValidRecord($record);
+
+        })->values()->toArray();
+    }
 
 }
