69ec99bfdabe84cf80f2468e76fb238fd3b17b14
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-17 10:29:54 +0300

    Menus

---
+++ src/Exports/Translation/Exporter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MyDpo\Exports\CustomerRegisterExport;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithStrictNullComparison;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\FromView;
+// use MyDpo\Models\CustomerRegister;
+// use MyDpo\Models\CustomerDepartment;
+
+use Illuminate\Contracts\View\View;
+
+class Exporter implements FromView, WithStrictNullComparison, ShouldAutoSize {
+
+    // public $registru = NULL;
+    // public $juststructure = 1;
+    // public $departamente_ids = NULL;
+
+    public function __construct() {
+
+       
+    }
+
+    public function view(): View {
+
+        dd(__METHOD__);
+        return view('exports.customer-register.export', [
+            'columns' => $this->registru->columns,
+            'records' => $records,
+            'children' => $this->registru->children_columns,
+        ]);
+    }
+
+}
--- src/Performers/Translation/Export.php
+++ src/Performers/Translation/Export.php
@@ -3,38 +3,35 @@
 namespace MyDpo\Performers\Translation;
 
 use MyDpo\Helpers\Perform;
-// use MyDpo\Exports\CustomerCentralizator\Exporter;
+use MyDpo\Exports\Translation\Exporter;
 
 class Export extends Perform {
 
     public function Action() {
-
-        dd($this->input);
+	
+		$this->createUserFolder();
 		
-		// $this->createUserFolder();
+        $exporter = new Exporter();
 		
-        // $exporter = new Exporter($this->department_ids, $this->id);
+		$file_name = 'public/exports/' . \Auth::user()->id. '/' . $this->file_name;
 		
-		// $file_name = 'public/exports/' . \Auth::user()->id. '/' . $this->file_name;
-		
-		// \Excel::store($exporter, $file_name, NULL, NULL, ['visibility' => 'public']);
+		\Excel::store($exporter, $file_name, NULL, NULL, ['visibility' => 'public']);
 			
-		// $file = \Str::replace('public', 'storage', $file_name);
+		$file = \Str::replace('public', 'storage', $file_name);
 		
-        // $this->payload = [
-        //     'record' => [
-        //         'url' => asset($file),
-        //     ],
-        // ];
+        $this->payload = [
+            'record' => [
+                'url' => asset($file),
+            ],
+        ];
 
     }
 	
-	public function createUserFolder() {
-            
-        // if(! \Storage::exists('public/exports/' . \Auth::user()->id) )
-        // {
-        //     \Storage::disk('public')->makeDirectory('exports/' . \Auth::user()->id, 0777);
-        // }
+	public function createUserFolder() {            
+        if(! \Storage::exists('public/exports/' . \Auth::user()->id) )
+        {
+            \Storage::disk('public')->makeDirectory('exports/' . \Auth::user()->id, 0777);
+        }
     }
 	
 }
