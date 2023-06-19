3fdf9988c079d5f4f7e1891c10623c75189730ed
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-19 09:02:58 +0300

    Menus

--- src/Routes/api/customer-planuri-conformare/routes.php
+++ src/Routes/api/customer-planuri-conformare/routes.php
@@ -4,9 +4,9 @@ use MyDpo\Http\Controllers\Auth\CustomersPlanConformareController;
 
 Route::prefix('customers-planuri-conformare')->group( function() {
             
-    Route::post('get-records', [CustomersPlanConformareController::class, 'getRecords']);
-    Route::post('get-next-number', [CustomersPlanConformareController::class, 'getNextNumber']);
-    Route::post('save-rows', [CustomersPlanConformareController::class, 'saveRows']);
-    Route::post('action/{action}', [CustomersPlanConformareController::class, 'doAction']);
+    // Route::post('get-records', [CustomersPlanConformareController::class, 'getRecords']);
+    // Route::post('get-next-number', [CustomersPlanConformareController::class, 'getNextNumber']);
+    // Route::post('save-rows', [CustomersPlanConformareController::class, 'saveRows']);
+    // Route::post('action/{action}', [CustomersPlanConformareController::class, 'doAction']);
 
 });
--- src/Routes/auth/verified/admin/plan-conformare/routes.php
+++ src/Routes/auth/verified/admin/plan-conformare/routes.php
@@ -4,7 +4,7 @@ use MyDpo\Http\Controllers\Admin\PlanconformareController;
 
 Route::prefix('plan-conformare')->group( function() {
         
-    Route::get('/', [PlanconformareController::class, 'index']);        
+    // Route::get('/', [PlanconformareController::class, 'index']);        
     
 });
 
