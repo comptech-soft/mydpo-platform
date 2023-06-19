373402b966fb1e27d87706e9a3f3e65e76977107
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-19 08:46:52 +0300

    Menus

--- src/Http/Controllers/Admin/CentralizatoareController.php
+++ src/Http/Controllers/Admin/CentralizatoareController.php
@@ -4,32 +4,33 @@ namespace MyDpo\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use MyDpo\Helpers\Response;
+use MyDpo\Core\Http\Response\Index;
 use MyDpo\Models\Centralizator;
 
-class CentralizatoareController extends Controller {
+class CentralizatoareController extends Controller 
+{
     
     public function index(Request $r) {
-        return Response::View(
-            '~templates.index', 
-            asset('apps/centralizatoare/index.js')
-        );
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/admin/centralizatoare/index.js']
+        );        
     }
 
-    public function getRecords(Request $r) {
-        return Centralizator::getRecords($r->all());
-    }
+    // public function getRecords(Request $r) {
+    //     return Centralizator::getRecords($r->all());
+    // }
 
-    public function getCustomerAsociere(Request $r) {
-        return Centralizator::getCustomerAsociere($r->all());
-    }
+    // public function getCustomerAsociere(Request $r) {
+    //     return Centralizator::getCustomerAsociere($r->all());
+    // }
 
-    public function saveCustomerAsociere(Request $r) {
-        return Centralizator::saveCustomerAsociere($r->all());
-    }
+    // public function saveCustomerAsociere(Request $r) {
+    //     return Centralizator::saveCustomerAsociere($r->all());
+    // }
     
-    public function doAction($action, Request $r) {
-        return Centralizator::doAction($action, $r->all());
-    }
+    // public function doAction($action, Request $r) {
+    //     return Centralizator::doAction($action, $r->all());
+    // }
 
 }
