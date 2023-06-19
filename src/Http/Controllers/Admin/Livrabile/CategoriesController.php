18b8f3f6f621f3d290e930f10856666678e0fcfc
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-06-19 10:27:13 +0300

    Menus

--- src/Http/Controllers/Admin/Livrabile/CategoriesController.php
+++ src/Http/Controllers/Admin/Livrabile/CategoriesController.php
@@ -4,21 +4,29 @@ namespace MyDpo\Http\Controllers\Admin\Livrabile;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use MyDpo\Helpers\Response;
+use MyDpo\Core\Http\Response\Index;
 use MyDpo\Models\Category;
 
-class CategoriesController extends Controller {
+class CategoriesController extends Controller 
+{
 
-    public function getItems($type = NULL, Request $r) {
-        return Category::getItems($r->all(), $type);
+    public function index(Request $r) {
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/admin/centralizatoare/index.js']
+        );        
     }
 
-    public function doAction($action, Request $r) {
-        return Category::doAction($action, $r->all());
-    }
+    // public function getItems($type = NULL, Request $r) {
+    //     return Category::getItems($r->all(), $type);
+    // }
 
-    public function isValidName(Request $r) {
-        return Category::isValidName($r->all());
-    }
+    // public function doAction($action, Request $r) {
+    //     return Category::doAction($action, $r->all());
+    // }
+
+    // public function isValidName(Request $r) {
+    //     return Category::isValidName($r->all());
+    // }
 
 }
