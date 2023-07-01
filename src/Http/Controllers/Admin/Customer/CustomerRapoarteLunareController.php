ff5676190cf14fc695331650d92f34413feb8a9c
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-07-01 20:43:44 +0300

    Customers

---
+++ src/Http/Controllers/Admin/Customer/CustomerRapoarteLunareController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Admin\Customer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Core\Http\Response\Index;
+use MyDpo\Models\Customer;
+
+
+class CustomerRapoarteLunareController extends Controller {
+    
+    public function index($customer_id, Request $r) {
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/customer/analiza-gap/index.js'],
+            payload: [
+                'customer_id' => $customer_id,
+                'customer' => Customer::find($customer_id),
+            ],
+        );        
+    }
+
+  
+}
