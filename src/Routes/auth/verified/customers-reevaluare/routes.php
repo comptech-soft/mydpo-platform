05548e5a62280948b73faab0d3da1df808a28393
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-07-01 14:29:23 +0300

    Customers

--- src/Routes/auth/verified/customers-reevaluare/routes.php
+++ src/Routes/auth/verified/customers-reevaluare/routes.php
@@ -1,10 +1,10 @@
 <?php
 
-use MyDpo\Http\Controllers\Auth\CustomersReevaluareController;
+// use MyDpo\Http\Controllers\Auth\CustomersReevaluareController;
 
 
-Route::middleware('valid-customer')->prefix('/customer-reevaluare')->group( function() {
+// Route::middleware('valid-customer')->prefix('/customer-reevaluare')->group( function() {
 
-    Route::middleware('is-activated')->get('/{customer_id}', [CustomersReevaluareController::class, 'index']);
+//     Route::middleware('is-activated')->get('/{customer_id}', [CustomersReevaluareController::class, 'index']);
 
-});
+// });
