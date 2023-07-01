f6ebf3ba87af678947c14bb617e8cef163cefee8
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-07-01 20:44:03 +0300

    Customers

--- src/Http/Controllers/Admin/Customer/CustomerRapoarteLunareController.php
+++ src/Http/Controllers/Admin/Customer/CustomerRapoarteLunareController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
 use MyDpo\Models\Customer;
 
-
 class CustomerRapoarteLunareController extends Controller {
     
     public function index($customer_id, Request $r) {
@@ -20,6 +19,5 @@ class CustomerRapoarteLunareController extends Controller {
             ],
         );        
     }
-
   
 }
--- src/Routes/auth/verified/customers-rapoarte-lunare/routes.php
+++ src/Routes/auth/verified/customers-rapoarte-lunare/routes.php
@@ -1,9 +1,9 @@
 <?php
 
-use MyDpo\Http\Controllers\Auth\CustomersRapoarteLunareController;
+// use MyDpo\Http\Controllers\Auth\CustomersRapoarteLunareController;
 
-Route::middleware('valid-customer')->prefix('customer-rapoarte-lunare')->group( function() {
+// Route::middleware('valid-customer')->prefix('customer-rapoarte-lunare')->group( function() {
 
-    Route::get('/{customer_id}', [CustomersRapoarteLunareController::class, 'index']);
+//     Route::get('/{customer_id}', [CustomersRapoarteLunareController::class, 'index']);
 
-});
+// });
