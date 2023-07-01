bad028367a6bd1e6df9c738010660070e794ec15
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-07-01 20:45:01 +0300

    Customers

--- src/Http/Controllers/Admin/Customer/CustomerRapoarteLunareController.php
+++ src/Http/Controllers/Admin/Customer/CustomerRapoarteLunareController.php
@@ -12,7 +12,7 @@ class CustomerRapoarteLunareController extends Controller {
     public function index($customer_id, Request $r) {
         return Index::View(
             styles: ['css/app.css'],
-            scripts: ['apps/customer/analiza-gap/index.js'],
+            scripts: ['apps/customer/rapoarte-lunare/index.js'],
             payload: [
                 'customer_id' => $customer_id,
                 'customer' => Customer::find($customer_id),
