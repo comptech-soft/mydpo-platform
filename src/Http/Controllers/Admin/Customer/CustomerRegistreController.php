5c3be2f6603d5693c1df10f8f1d4cac54b02cce6
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-07-01 14:51:38 +0300

    Customers

---
+++ src/Http/Controllers/Admin/Customer/CustomerRegistreController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Admin\Customer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Core\Http\Response\Index;
+use MyDpo\Models\Customer;
+
+class CustomerRegistreController extends Controller {
+    
+    public function index($customer_id, Request $r) {
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/customer/registre/index.js'],
+            payload: [
+                'customer_id' => $customer_id,
+                'customer' => Customer::find($customer_id),
+            ],
+        );        
+    }
+
+    // public function index($customer_id, Request $r) {
+
+    //     return Response::View(
+    //         '~templates.index', 
+    //         asset('apps/customer-registre/index.js'),
+    //         [], 
+    //         [
+    //             'customer_id' => $customer_id,
+    //         ]
+    //     );
+    // }
+
+    // public function getItems(Request $r) {
+    //     return CustomerRegister::getItems($r->all());
+    // }
+
+    // public function doAction($action, Request $r) {
+    //     return CustomerRegister::doAction($action, $r->all());
+    // }
+
+    // public function getNextNumber(Request $r) {
+    //     return CustomerRegister::getNextNumber($r->all());
+    // }
+
+    // public function registerCopy(Request $r) {
+    //     return CustomerRegister::registerCopy($r->all());
+    // }
+
+    // public function registerDownloadPreview($id) {
+
+    //     $registru = CustomerRegister::where('id', $id)->first();
+
+    //     return view('exports.customer-register.export', [
+    //         'columns' => $registru->columns,
+    //         'records' => $registru->records,
+    //         'children' => $registru->children_columns,
+    //     ]);
+    // }
+
+    // public function registerDownload(Request $r) {
+    //     return CustomerRegister::registerDownload($r->all());
+    // }
+
+    // public function registerUpload(Request $r) {
+    //     return CustomerRegister::registerUpload($r->all());
+    // }
+
+    // public function registerSaveAccess(Request $r) {
+    //     return CustomerRegister::registerSaveAccess($r->all());
+    // }
+
+    
+
+}
--- src/Routes/auth/verified/customers-registre/routes.php
+++ src/Routes/auth/verified/customers-registre/routes.php
@@ -1,72 +1,72 @@
 <?php
 
-use MyDpo\Http\Controllers\Admin\RegistreController;
-use MyDpo\Http\Controllers\Admin\RegistreColumnsController;
-use MyDpo\Http\Controllers\Auth\CustomersRegistreController;
-use MyDpo\Http\Controllers\Auth\CustomersRegistreAsociateController;
-use MyDpo\Http\Controllers\Auth\CustomerRegistruController;
-use MyDpo\Http\Controllers\Auth\CustomersRegistreRowsController;
-use MyDpo\Http\Controllers\Auth\CustomersRegistreUsersController;
+// use MyDpo\Http\Controllers\Admin\RegistreController;
+// use MyDpo\Http\Controllers\Admin\RegistreColumnsController;
+// use MyDpo\Http\Controllers\Auth\CustomersRegistreController;
+// use MyDpo\Http\Controllers\Auth\CustomersRegistreAsociateController;
+// use MyDpo\Http\Controllers\Auth\CustomerRegistruController;
+// use MyDpo\Http\Controllers\Auth\CustomersRegistreRowsController;
+// use MyDpo\Http\Controllers\Auth\CustomersRegistreUsersController;
 
-Route::prefix('registre')->group( function() {
+// Route::prefix('registre')->group( function() {
         
-    Route::get('/', [RegistreController::class, 'index']);        
-    Route::post('items', [RegistreController::class, 'getItems']);
-    Route::post('action/{action}', [RegistreController::class, 'doAction']);
+//     Route::get('/', [RegistreController::class, 'index']);        
+//     Route::post('items', [RegistreController::class, 'getItems']);
+//     Route::post('action/{action}', [RegistreController::class, 'doAction']);
 
-});
+// });
 
-Route::prefix('registre-columns')->group( function() {
+// Route::prefix('registre-columns')->group( function() {
         
-    Route::post('action/{action}', [RegistreColumnsController::class, 'doAction']);
-    Route::post('reorder-columns', [RegistreColumnsController::class, 'reorderColumns']);
+//     Route::post('action/{action}', [RegistreColumnsController::class, 'doAction']);
+//     Route::post('reorder-columns', [RegistreColumnsController::class, 'reorderColumns']);
 
-});
+// });
 
 
-Route::middleware('valid-customer')->prefix('/customer-registre')->group( function() {
+// Route::middleware('valid-customer')->prefix('/customer-registre')->group( function() {
 
-    Route::middleware('is-activated')->get('/{customer_id}', [CustomersRegistreController::class, 'index']);
+//     Route::middleware('is-activated')->get('/{customer_id}', [CustomersRegistreController::class, 'index']);
 
-});
+// });
 
-Route::prefix('/customers-registers-asociate')->group( function() {
+// Route::prefix('/customers-registers-asociate')->group( function() {
 
-    Route::post('items', [CustomersRegistreAsociateController::class, 'getItems']);
-    Route::post('save-asociere', [CustomersRegistreAsociateController::class, 'saveAsociere']);
+//     Route::post('items', [CustomersRegistreAsociateController::class, 'getItems']);
+//     Route::post('save-asociere', [CustomersRegistreAsociateController::class, 'saveAsociere']);
     
-});
+// });
 
-Route::middleware('valid-customer')->prefix('/customer-registru')->group( function() {
+// Route::middleware('valid-customer')->prefix('/customer-registru')->group( function() {
 
-    Route::middleware('is-activated')->get('/{customer_id}/{registru_id}', [CustomerRegistruController::class, 'index']);
+//     Route::middleware('is-activated')->get('/{customer_id}/{registru_id}', [CustomerRegistruController::class, 'index']);
 
-});
+// });
 
-Route::prefix('/customers-registers')->group( function() {
+// Route::prefix('/customers-registers')->group( function() {
 
-    Route::post('items', [CustomersRegistreController::class, 'getItems']);
-    Route::post('action/{action}', [CustomersRegistreController::class, 'doAction']);
-    Route::post('get-next-number', [CustomersRegistreController::class, 'getNextNumber']);
-    Route::post('register-download', [CustomersRegistreController::class, 'registerDownload']);
-    Route::get('register-download-preview/{id}', [CustomersRegistreController::class, 'registerDownloadPreview']);
-    Route::post('register-upload', [CustomersRegistreController::class, 'registerUpload']);
-    Route::post('register-save-access', [CustomersRegistreController::class, 'registerSaveAccess']);
-    Route::post('register-copy', [CustomersRegistreController::class, 'registerCopy']);
+//     Route::post('items', [CustomersRegistreController::class, 'getItems']);
+//     Route::post('action/{action}', [CustomersRegistreController::class, 'doAction']);
+//     Route::post('get-next-number', [CustomersRegistreController::class, 'getNextNumber']);
+//     Route::post('register-download', [CustomersRegistreController::class, 'registerDownload']);
+//     Route::get('register-download-preview/{id}', [CustomersRegistreController::class, 'registerDownloadPreview']);
+//     Route::post('register-upload', [CustomersRegistreController::class, 'registerUpload']);
+//     Route::post('register-save-access', [CustomersRegistreController::class, 'registerSaveAccess']);
+//     Route::post('register-copy', [CustomersRegistreController::class, 'registerCopy']);
     
-});
+// });
 
-Route::prefix('/customers-registers-rows')->group( function() {
-    Route::post('action/{action}', [CustomersRegistreRowsController::class, 'doAction']);
-    Route::post('change-status', [CustomersRegistreRowsController::class, 'changeStatus']);  
-    Route::post('change-stare', [CustomersRegistreRowsController::class, 'changeStare']);    
-    Route::post('delete-rows', [CustomersRegistreRowsController::class, 'deleteRows']);    
-    Route::post('upload-file', [CustomersRegistreRowsController::class, 'uploadFile']);    
-    Route::post('load-files', [CustomersRegistreRowsController::class, 'loadFiles']); 
-    Route::post('delete-file', [CustomersRegistreRowsController::class, 'deleteFile']);    
-    Route::get('download-file/{id}', [CustomersRegistreRowsController::class, 'downloadFile']);  
-});
+// Route::prefix('/customers-registers-rows')->group( function() {
+//     Route::post('action/{action}', [CustomersRegistreRowsController::class, 'doAction']);
+//     Route::post('change-status', [CustomersRegistreRowsController::class, 'changeStatus']);  
+//     Route::post('change-stare', [CustomersRegistreRowsController::class, 'changeStare']);    
+//     Route::post('delete-rows', [CustomersRegistreRowsController::class, 'deleteRows']);    
+//     Route::post('upload-file', [CustomersRegistreRowsController::class, 'uploadFile']);    
+//     Route::post('load-files', [CustomersRegistreRowsController::class, 'loadFiles']); 
+//     Route::post('delete-file', [CustomersRegistreRowsController::class, 'deleteFile']);    
+//     Route::get('download-file/{id}', [CustomersRegistreRowsController::class, 'downloadFile']);  
+// });
 
-Route::prefix('/customers-registers-users')->group( function() {
-    Route::post('items', [CustomersRegistreUsersController::class, 'getItems']);    
-});
+// Route::prefix('/customers-registers-users')->group( function() {
+//     Route::post('items', [CustomersRegistreUsersController::class, 'getItems']);    
+// });
