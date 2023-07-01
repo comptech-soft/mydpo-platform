235b0c1308c2895803823a075bf886542c8d11bf
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-07-01 13:59:32 +0300

    Customers

--- src/Routes/auth/verified/customers-analiza-gap/routes.php
+++ src/Routes/auth/verified/customers-analiza-gap/routes.php
@@ -1,11 +1,11 @@
 <?php
 
-use MyDpo\Http\Controllers\Auth\CustomersAnalizaGapController;
-use MyDpo\Http\Controllers\Auth\CustomersAnalizaGapCentralizatoareController;
+// use MyDpo\Http\Controllers\Auth\CustomersAnalizaGapController;
+// use MyDpo\Http\Controllers\Auth\CustomersAnalizaGapCentralizatoareController;
 
-Route::middleware('valid-customer')->prefix('/customer-analiza-gap')->group( function() {
+// Route::middleware('valid-customer')->prefix('/customer-analiza-gap')->group( function() {
 
-    Route::middleware('is-activated')->get('/{customer_id}/centralizatoare', [CustomersAnalizaGapCentralizatoareController::class, 'index']);
-    Route::middleware('is-activated')->get('/{customer_id}', [CustomersAnalizaGapController::class, 'index']);
+//     Route::middleware('is-activated')->get('/{customer_id}/centralizatoare', [CustomersAnalizaGapCentralizatoareController::class, 'index']);
+//     Route::middleware('is-activated')->get('/{customer_id}', [CustomersAnalizaGapController::class, 'index']);
     
-});
+// });
