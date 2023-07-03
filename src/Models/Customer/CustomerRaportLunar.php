c6e44ed208329a00e4416f97dfedcfbdabe4c36e
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-07-03 11:01:32 +0300

    Customers

---
+++ src/Models/Customer/CustomerRaportLunar.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MyDpo\Models\Customer;
+
+use Illuminate\Database\Eloquent\Model;
+// use MyDpo\Helpers\Performers\Datatable\GetItems;
+// use MyDpo\Helpers\Performers\Datatable\DoAction;
+
+use MyDpo\Traits\Itemable;
+
+class CustomerRaportLunar extends Model {
+
+    use Itemable;
+    
+    protected $table = 'customers-rapoarte-lunare';
+
+    // public static function getItems($input) {
+    //     return (new GetItems($input, self::query()->with(['orders.services.service.type', 'customer']), __CLASS__))->Perform();
+    // }
+
+}
