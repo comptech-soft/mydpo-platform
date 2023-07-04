2f75482b4d8458ef2e38dde487f2f3492590c76f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-07-04 09:50:56 +0300

    Customers

--- src/Models/System/SysMenu.php
+++ src/Models/System/SysMenu.php
@@ -5,13 +5,13 @@ namespace MyDpo\Models\System;
 use Illuminate\Database\Eloquent\Model;
 use Kalnoy\Nestedset\NodeTrait;
 // use MyDpo\Performers\SysMenu\GetVisibilities;
-// use MyDpo\Traits\Itemable;
+use MyDpo\Traits\Itemable;
 // use MyDpo\Traits\Actionable;
 // use MyDpo\Traits\Reorderable;
 
 class SysMenu extends Model {
 
-    use NodeTrait; //, Itemable, Actionable, Reorderable;
+    use NodeTrait, Itemable; //, Actionable, Reorderable;
     
     protected $table = 'system-menus';
 
