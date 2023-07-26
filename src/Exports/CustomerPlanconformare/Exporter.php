7c6100b37b913b2d9c47de570d0f71107ecfc8d4
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-07-26 09:10:36 +0300

    Planuri

--- src/Exports/CustomerPlanconformare/Exporter.php
+++ src/Exports/CustomerPlanconformare/Exporter.php
@@ -25,9 +25,55 @@ class Exporter implements FromView, WithStrictNullComparison, ShouldAutoSize
 
         $plan = CustomerPlanconformare::find($this->input['plan_id']);
 
+        $year = $plan->year;
+
+        $columns = collect($plan->columns)->map( function($column) use ($year) {
+
+            return [
+                ...$column,
+                'caption' => collect($column['caption'])->map( function($text) use ($year){
+
+                    $r = \Str::replace(':year', $year, $text);
+
+                    $parts = \Str::of($r)->explode("\r\n");
+
+                    return $parts->join('<br/>');
+
+                })->toArray(), 
+            ];
+
+        })->toArray();
+
+        $records = collect($plan->GetRowsAsTable())->map(function( $item) {
+
+            $html_style = NULL;
+
+            if($item['type'] == 'capitol')
+            {
+                $html_style = 'color: #3f51b5; font-size: 14px; font-weight:bold; word-wrap: break-word;';
+            }
+
+            if($item['type'] == 'actiune')
+            {
+                $html_style = 'color: #1976d2; font-size: 12px; font-weight:bold; word-wrap: break-word;';
+            }
+
+            if($item['type'] == 'subactiune')
+            {
+                $html_style = 'color: #2196f3; font-size: 10px; font-weight:bold; word-wrap: break-word;';
+            }
+            
+            return [
+                ...$item,
+                'html_style' => $html_style,
+            ];
+
+        })->toArray();
+
         return view('exports.customer.plan-conformare.export', [
             'plan' => $plan,
-            'records' => $plan->GetRowsAsTable(),
+            'records' => $records,
+            'columns' => $columns,
         ]);
     }
 
