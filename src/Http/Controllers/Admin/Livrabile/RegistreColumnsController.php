6b5f2dc3bc58ffdd281697b6055a8d0fafebac19
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-01 01:57:00 +0300

    Registre

---
+++ src/Http/Controllers/Admin/Livrabile/RegistreColumnsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Admin\Livrabile;
+
+use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+// use MyDpo\Models\RegistruColoana;
+
+class RegistreColumnsController extends Controller {
+    
+    // public function doAction($action, Request $r) {
+    //     return RegistruColoana::doAction($action, $r->all());
+    // }
+
+    // public function reorderColumns(Request $r) {
+    //     return RegistruColoana::reorderColumns($r->all());
+    // }
+
+}
