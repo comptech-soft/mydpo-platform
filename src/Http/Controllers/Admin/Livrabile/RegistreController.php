ec78539deb66e006021d4dc0b066f3b093b9ec4f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-07-31 09:42:06 +0300

    Registre

---
+++ src/Http/Controllers/Admin/Livrabile/RegistreController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Admin\Livrabile;
+
+use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+// use MyDpo\Helpers\Response;
+// use MyDpo\Models\Registru;
+
+class RegistreController extends Controller {
+    
+    // public function index(Request $r) {
+    //     return Response::View(
+    //         '~templates.index', 
+    //         asset('apps/registre/index.js')
+    //     );
+    // }
+
+    // public function getItems(Request $r) {
+    //     return Registru::getItems($r->all());
+    // }
+
+    // public function doAction($action, Request $r) {
+    //     return Registru::doAction($action, $r->all());
+    // }
+
+}
