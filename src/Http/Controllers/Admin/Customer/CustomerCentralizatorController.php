9e2f189b259bad6b864ffe8648029c73f034b5f4
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-01 18:22:43 +0300

    Registre

--- src/Http/Controllers/Admin/Customer/CustomerCentralizatorController.php
+++ src/Http/Controllers/Admin/Customer/CustomerCentralizatorController.php
@@ -1,24 +1,25 @@
 <?php
 
-namespace MyDpo\Http\Controllers\Auth;
+namespace MyDpo\Http\Controllers\Admin\Customer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use MyDpo\Helpers\Response;
+use MyDpo\Core\Http\Response\Index;
 
 class CustomerCentralizatorController extends Controller {
     
     public function index($customer_id, $centralizator_id, Request $r) {
 
-        return Response::View(
-            '~templates.index', 
-            asset('apps/customer-centralizator/index.js'),
-            [], 
-            [
+        dd(__METHOD__);
+        
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/customer/centralizatoare/index.js'],
+            payload: [
                 'customer_id' => $customer_id,
                 'centralizator_id' => $centralizator_id,
-            ]
-        );
+            ],
+        );        
     }
-    
+
 }
