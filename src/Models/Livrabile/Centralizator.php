de4e14701b2a0b3777790fca385dde5661a88e8c
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-01 10:35:07 +0300

    Registre

--- src/Models/Livrabile/Centralizator.php
+++ src/Models/Livrabile/Centralizator.php
@@ -5,12 +5,13 @@ namespace MyDpo\Models\Livrabile;
 use Illuminate\Database\Eloquent\Model;
 use MyDpo\Traits\Itemable;
 use MyDpo\Traits\Actionable;
+use MyDpo\Traits\Columnable;
 use MyDpo\Scopes\NotdeletedScope;
 use MyDpo\Performers\Centralizator\SaveCustomerAsociere;
 
 class Centralizator extends Model {
 
-    use Itemable, Actionable;
+    use Itemable, Actionable, Columnable;
 
     protected $table = 'centralizatoare';
     
@@ -129,111 +130,111 @@ class Centralizator extends Model {
         return $records->toArray();
     }
 
-    public static function doInsert($input, $record) {
+    // public static function doInsert($input, $record) {
 
-        $record = self::create($input);
+    //     $record = self::create($input);
 
-        if(array_key_exists('body', $input))
-        {
-            foreach($input['body'] as $key => $value)
-            {
-                if($value == 1)
-                {
-                    $record->{'AddColumn' . ucfirst($key)}();
-                }
-            }
-        }
+    //     if(array_key_exists('body', $input))
+    //     {
+    //         foreach($input['body'] as $key => $value)
+    //         {
+    //             if($value == 1)
+    //             {
+    //                 $record->{'AddColumn' . ucfirst($key)}();
+    //             }
+    //         }
+    //     }
 
-        return self::withCount('columns')->find($record->id);
-    }
+    //     return self::withCount('columns')->find($record->id);
+    // }
 
-    public static function doUpdate($input, $record) {
+    // public static function doUpdate($input, $record) {
 
-        $record->update($input);
+    //     $record->update($input);
         
-        if(array_key_exists('body', $input))
-        {
-            foreach($input['body'] as $key => $value)
-            {
-                if($value == 1)
-                {
-                    $record->{'AddColumn' . ucfirst($key)}();
-                }
-                else
-                {
-                    $record->{'DeleteColumn' . ucfirst($key)}();
-                }
-            }
-        }
+    //     if(array_key_exists('body', $input))
+    //     {
+    //         foreach($input['body'] as $key => $value)
+    //         {
+    //             if($value == 1)
+    //             {
+    //                 $record->{'AddColumn' . ucfirst($key)}();
+    //             }
+    //             else
+    //             {
+    //                 $record->{'DeleteColumn' . ucfirst($key)}();
+    //             }
+    //         }
+    //     }
 
-        return $record;
-    }
+    //     return $record;
+    // }
 
-    public function DeleteColumnStatus() {
-        $this->DeleteColumn('STATUS');
-    }
+    // public function DeleteColumnStatus() {
+    //     $this->DeleteColumn('STATUS');
+    // }
 
-    public function DeleteColumnDepartament() {
-        $this->DeleteColumn('DEPARTMENT');
-    }
+    // public function DeleteColumnDepartament() {
+    //     $this->DeleteColumn('DEPARTMENT');
+    // }
 
-    public function DeleteColumnVizibilitate() {
-        $this->DeleteColumn('VISIBILITY');
-    }
+    // public function DeleteColumnVizibilitate() {
+    //     $this->DeleteColumn('VISIBILITY');
+    // }
 
-    public function AddColumnVizibilitate() {
-        $this->AddColumn('Vizibilitate', 'VISIBILITY', -100, 150);
-    }
+    // public function AddColumnVizibilitate() {
+    //     $this->AddColumn('Vizibilitate', 'VISIBILITY', -100, 150);
+    // }
 
-    public function AddColumnStatus() {
-        $this->AddColumn('Status', 'STATUS', -90, 150);
-    }
+    // public function AddColumnStatus() {
+    //     $this->AddColumn('Status', 'STATUS', -90, 150);
+    // }
 
-    public function AddColumnDepartament() {
-        $this->AddColumn('Departament', 'DEPARTMENT', -80, 200);
-    }
+    // public function AddColumnDepartament() {
+    //     $this->AddColumn('Departament', 'DEPARTMENT', -80, 200);
+    // }
 
-    public function DeleteColumn($type) {
-        $record = CentralizatorColoana::where('centralizator_id', $this->id)->where('type', $type)->delete();
-    }
+    // public function DeleteColumn($type) {
+    //     $record = CentralizatorColoana::where('centralizator_id', $this->id)->where('type', $type)->delete();
+    // }
 
-    public function AddColumn($caption, $type, $order_no, $width) {
+    // public function AddColumn($caption, $type, $order_no, $width) {
         
-        $input = [
-            'centralizator_id' => $this->id,
-            'caption' => $caption,
-            'slug' => md5($caption . time()),
-            'is_group' => 0,
-            'group_id' => NULL,
-            'type' =>  $type,
-            'order_no' => $order_no,
-            'width' => $width,
-        ];
+    //     $input = [
+    //         'centralizator_id' => $this->id,
+    //         'caption' => $caption,
+    //         'slug' => md5($caption . time()),
+    //         'is_group' => 0,
+    //         'group_id' => NULL,
+    //         'type' =>  $type,
+    //         'order_no' => $order_no,
+    //         'width' => $width,
+    //     ];
 
-        $record = CentralizatorColoana::where('centralizator_id', $this->id)->where('type', $type)->first();
+    //     $record = CentralizatorColoana::where('centralizator_id', $this->id)->where('type', $type)->first();
 
-        if(!! $record)
-        {
-            $record->update($input);
-        }
-        else
-        {
-            $record = CentralizatorColoana::create($input);
-        }
+    //     if(!! $record)
+    //     {
+    //         $record->update($input);
+    //     }
+    //     else
+    //     {
+    //         $record = CentralizatorColoana::create($input);
+    //     }
 
-    }
+    // }
 
-    public static function doDelete($input, $record) {
+    // public static function doDelete($input, $record) {
 
-        CentralizatorColoana::where('centralizator_id', $record->id)->delete();
+    //     CentralizatorColoana::where('centralizator_id', $record->id)->delete();
 
-        $record->deleted = 1;
-        $record->deleted_by = \Auth::user()->id;
-        $record->save();
+    //     $record->deleted = 1;
+    //     $record->deleted_by = \Auth::user()->id;
+    //     $record->save();
 
-        return $record;
+    //     return $record;
 
-    }
+    // }
 
     public static function GetRules($action, $input) {
         
