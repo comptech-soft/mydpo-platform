9d027f51faaf750f742fcae8fbdd9c80f20a6f8f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-01 18:24:38 +0300

    Registre

--- src/Http/Controllers/Admin/Customer/CustomerCentralizatoareController.php
+++ src/Http/Controllers/Admin/Customer/CustomerCentralizatoareController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
 use MyDpo\Models\Customer;
 
-
 class CustomerCentralizatoareController extends Controller {
     
     public function index($customer_id, Request $r) {
--- src/Http/Controllers/Admin/Customer/CustomerCentralizatorController.php
+++ src/Http/Controllers/Admin/Customer/CustomerCentralizatorController.php
@@ -5,19 +5,21 @@ namespace MyDpo\Http\Controllers\Admin\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
+use MyDpo\Models\Customer;
 
 class CustomerCentralizatorController extends Controller {
     
     public function index($customer_id, $centralizator_id, Request $r) {
 
-        dd(__METHOD__);
-        
+        dd(__METHOD__, $centralizator_id);
+
         return Index::View(
             styles: ['css/app.css'],
             scripts: ['apps/customer/centralizatoare/index.js'],
             payload: [
                 'customer_id' => $customer_id,
                 'centralizator_id' => $centralizator_id,
+                'customer' => Customer::find($customer_id),
             ],
         );        
     }
