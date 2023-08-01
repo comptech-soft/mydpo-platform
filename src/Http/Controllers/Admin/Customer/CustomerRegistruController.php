e936b20ae94f13a94f337f458aea462f3248706f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-01 23:30:53 +0300

    Registre

--- src/Http/Controllers/Admin/Customer/CustomerRegistruController.php
+++ src/Http/Controllers/Admin/Customer/CustomerRegistruController.php
@@ -5,24 +5,22 @@ namespace MyDpo\Http\Controllers\Admin\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
-// use MyDpo\Models\Customer;
-// use MyDpo\Models\Livrabile\Centralizator;
+use MyDpo\Models\Customer;
+use MyDpo\Models\Livrabile\Registru;
 // use MyDpo\Models\Customer\CustomerCentralizator;
 
 class CustomerRegistruController extends Controller {
     
     public function index($customer_id, $register_id, Request $r) {
 
-        dd($customer_id, $register_id);
-        
         return Index::View(
             styles: ['css/app.css'],
-            scripts: ['apps/customer/centralizator/index.js'],
+            scripts: ['apps/customer/registru/index.js'],
             payload: [
                 'customer_id' => $customer_id,
                 'centralizator_id' => $centralizator_id,
                 'customer' => Customer::find($customer_id),
-                'centralizator' => Centralizator::find($centralizator_id),
+                'registru' => Registru::find($register_id),
             ],
         );        
     }
