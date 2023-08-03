15a39b9451c9e51baa39c8c3a5463da3e644c24f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-03 19:38:38 +0300

    Registre

--- src/Http/Controllers/Admin/Customer/CustomerCentralizatorRowsFilesController.php
+++ src/Http/Controllers/Admin/Customer/CustomerCentralizatorRowsFilesController.php
@@ -4,7 +4,7 @@ namespace MyDpo\Http\Controllers\Admin\Customer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use MyDpo\Models\CustomerCentralizatorRowFile;
+use MyDpo\Models\Customer\CustomerCentralizatorRowFile;
 
 class CustomerCentralizatorRowsFilesController extends Controller {
     
