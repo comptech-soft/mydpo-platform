4e0ca090bd96af4487a9541ded66e26b02fe4620
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-06 23:05:01 +0300

    Registre

--- src/Http/Controllers/Admin/Customer/Centralizatoare/CustomerTipCentralizatorController.php
+++ src/Http/Controllers/Admin/Customer/Centralizatoare/CustomerTipCentralizatorController.php
@@ -12,8 +12,7 @@ use MyDpo\Models\Customer\Centralizatoare\CustomerCentralizator;
 class CustomerTipCentralizatorController extends Controller {
     
     public function index($customer_id, $centralizator_id, Request $r) {
-
-
+        
         if( ! ($customer = Customer::find($customer_id)) )
         {
             return redirect('admin/customers');
@@ -23,10 +22,7 @@ class CustomerTipCentralizatorController extends Controller {
         {
             return redirect('customer-centralizatoare-dashboard/' . $customer_id);
         }
-
-        	
-        dd($tip_centralizator->columns_items);
-
+        
         return Index::View(
             styles: ['css/app.css'],
             scripts: ['apps/customer/centralizator/index.js'],
