46910cbc96dd579c67377fcaf59e517e3f6c3bc7
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-10 02:38:11 +0300

    Centralizatorable rows

--- src/Http/Controllers/Admin/Customer/Centralizatorable/CentralizatorableRowsController.php
+++ src/Http/Controllers/Admin/Customer/Centralizatorable/CentralizatorableRowsController.php
@@ -5,7 +5,7 @@ namespace MyDpo\Http\Controllers\Admin\Customer\Centralizatorable;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 // use MyDpo\Core\Http\Response\Index;
-// use MyDpo\Models\Customer;
+use MyDpo\Models\Customer;
 // use MyDpo\Models\Livrabile\TipCentralizator;
 // use MyDpo\Models\Customer\Centralizatoare\CentralizatorAsociat;
 
@@ -14,29 +14,36 @@ class CentralizatorableRowsController extends Controller {
 
     public function index($model, $customer_id, $tip_id, $document_id, Request $r) {
         
-        dd($model, $customer_id, $tip_id, $document_id);
+        if(! in_array($model, ['centralizatoare', 'registre']))
+        {
+            return redirect('/');
+        }
 
-        // if( ! ($customer = Customer::find($customer_id)) )
-        // {
-        //     return redirect('/');
-        // }
+        if( ! ($customer = Customer::find($customer_id)) )
+        {
+            return redirect('/');
+        }
 
         // if( ! ($tip = TipCentralizator::find($centralizator_id)) )
         // {
         //     return redirect('/');
         // }
 
-        // return Index::View(
-        //     styles: ['css/app.css'],
-        //     scripts: ['apps/customer/customers-centralizatoare-list/index.js'],
-        //     payload: [
-        //         'customer_id' => $customer_id,
-        //         'tip_id' => $centralizator_id,
-        //         'customer' => $customer,
-        //         'tip' => $tip,
-        //         'model' => 'Customercentralizatoare',
-        //     ],
-        // );    
+        // dd($model, $customer_id, $tip_id, $document_id);
+
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/customer/customers-centralizatoare-list/index.js'],
+            payload: [
+                'model' => $model,
+                'customer_id' => $customer_id,
+                'customer' => $customer,
+                // 'tip_id' => $centralizator_id,
+                
+                // 'tip' => $tip,
+                
+            ],
+        );    
 
       
     }
