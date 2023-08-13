78cf7441b13d023d7259c2b4b53d30d482c359e2
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-13 18:31:36 +0300

    Centralizatorable rows

--- src/Models/Customer/Centralizatoare/RowFile.php
+++ src/Models/Customer/Centralizatoare/RowFile.php
@@ -5,7 +5,7 @@ namespace MyDpo\Models\Customer\Centralizatoare;
 use Illuminate\Database\Eloquent\Model;
 use MyDpo\Traits\Itemable;
 use MyDpo\Traits\Actionable;
-use MyDpo\Performers\Customer\Centralizator\RowFile\UploadFiles;
+use MyDpo\Performers\Customer\Centralizatoare\RowFile\UploadFiles;
 
 class RowFile extends Model {
 
