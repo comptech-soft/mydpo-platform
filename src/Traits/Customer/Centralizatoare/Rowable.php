6d15d072a423b5723ec87db17cf58b469d2622bb
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-15 09:59:36 +0300

    Autentificare cu Keycloak

--- src/Traits/Customer/Centralizatoare/Rowable.php
+++ src/Traits/Customer/Centralizatoare/Rowable.php
@@ -5,6 +5,7 @@ namespace MyDpo\Traits\Customer\Centralizatoare;
 use MyDpo\Models\Customer\CustomerDepartment;
 use MyDpo\Models\Livrabile\TipCentralizator;
 use MyDpo\Models\Livrabile\TipCentralizatorColoana;
+use MyDpo\Traits\Exportable;
 
 trait Rowable {
 
@@ -220,10 +221,6 @@ trait Rowable {
                 ]);
             }
         }
-    }
-
-    public static function doExport($input, $record) {
-        dd($input, $record);
     }
 
 }
