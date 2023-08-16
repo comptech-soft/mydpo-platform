4786c93db9988443ad557d75bbcd5d1e2690c236
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-16 23:05:31 +0300

    Centralizatoare export

--- src/Models/Customer/Statuses/Status.php
+++ src/Models/Customer/Statuses/Status.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MyDpo\Models\Customer\Customer;
+namespace MyDpo\Models\Customer\Statuses;
 
 use Illuminate\Database\Eloquent\Model;
 use MyDpo\Helpers\Performers\Datatable\GetItems;
