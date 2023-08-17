1ce0402c656fc9f2e687f6f2c0938fe3aa2212d1
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-17 15:00:42 +0300

    Centralizatoare export

---
+++ src/Models/Livrabile/Services/Service.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace MyDpo\Models\Livrabile\Tasks;
+
+use Illuminate\Database\Eloquent\Model;
+use MyDpo\Traits\Itemable;
+use MyDpo\Traits\Actionable;
+use MyDpo\Rules\Livrabile\Service\UniqueName;
+
+class Service extends Model {
+    
+    use Itemable, Actionable;
+
+    protected $table = 'services';
+
+    protected $casts = [
+        'id' => 'integer',
+        'created_by' => 'integer',
+        'updated_by' => 'integer',
+        'deleted_by' => 'integer',
+        'deleted' => 'integer',
+        'tarif' => 'decimal:2',
+        'tarif_ore_suplimentare' => 'decimal:2',
+        'ore_incluse_abonament' => 'integer',
+        'abonamente' => 'json',
+    ];
+
+    protected $fillable = [
+        'id',
+        'name',
+        'type',
+        'order_no',
+        'tarif',
+        'tarif_ore_suplimentare',
+        'ore_incluse_abonament',
+        'is_abonament',
+        'abonamente',
+        'others',
+        'deleted',
+        'created_by',
+        'updated_by',
+        'deleted_by'
+    ];
+
+    public static function GetRules($action, $input) {
+
+        if(! in_array($action, ['insert', 'update']) )
+        {
+            return NULL;
+        }
+
+        $result = [
+            'name' => [
+                'required',
+                'max:191',
+                new UniqueName($action, $input),
+            ],
+        ];
+
+        return $result;
+    }
+
+}
---
+++ src/Rules/Livrabile/Service/UniqueName.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MyDpo\Rules\Livrabile\Service;
+
+use Illuminate\Contracts\Validation\Rule;
+use MyDpo\Models\Livrabile\Services\Service;
+
+class UniqueName implements Rule {
+
+    public $input = NULL;
+    public $action = NULL;
+    public $record = NULL;
+
+    public function __construct($action, $input) {
+        $this->input = $input;
+        $this->action = $action;
+    }
+
+    public function passes($attribute, $value) {   
+
+        $q = Service::where('name', $this->input['name'])->where('type', $this->input['type']);
+
+        if($this->action == 'update')
+        {
+            $q->where('id', '<>', $this->input['id']);
+        }
+
+        $this->record = $q->first();
+        
+        return ! $this->record;
+    }
+
+    public function message()
+    {
+        return 'Serviciul (' . $this->input['name'] . ') este deja definit.';
+    }
+}
