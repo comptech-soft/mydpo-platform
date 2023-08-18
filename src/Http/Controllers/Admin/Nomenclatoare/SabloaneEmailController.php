0e7b547da98844a2304e4bef4f976ee94077ab21
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-18 10:40:51 +0300

    Centralizatoare export

---
+++ src/Http/Controllers/Admin/Nomenclatoare/SabloaneEmailController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Admin\Nomenclatoare;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Core\Http\Response\Index;
+// use MyDpo\Models\Livrabile\Services\Service;
+
+class SabloaneEmailController extends Controller {
+    
+    public function index(Request $r) {
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/admin/sabloane-email/index.js']
+        );        
+    }
+
+    // public function getRecords(Request $r) {
+    //     return Service::getRecords($r->all());
+    // }
+
+    // public function doAction($action, Request $r) {
+    //     return Service::doAction($action, $r->all());
+    // }
+}
