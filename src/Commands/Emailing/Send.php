2ec05dbd2e16d96894b3f414e273ef1dc9c9a791
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-18 21:56:13 +0300

    Nomenclatoare

--- src/Commands/Emailing/Send.php
+++ src/Commands/Emailing/Send.php
@@ -3,6 +3,7 @@
 namespace MyDpo\Commands\Emailing;
 
 use Illuminate\Console\Command;
+use MyDpo\Models\Customer\Emails\EmailUser;
 
 class Send extends Command {
 
@@ -11,6 +12,13 @@ class Send extends Command {
     protected $description = 'Trimiterea de emailuri';
 
     public function handle() {
+
+        $pending_emails = EmailUser::whereNull('sended_at')
+            ->take(10)
+            ->get();
+
+        dd($pending_emails);
+
 
         $this->info('Pam pam. The command was successful!');
     
---
+++ src/Mail/System/SystemMail.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace MyDpo\Mail\System;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SystemMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Sample Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
