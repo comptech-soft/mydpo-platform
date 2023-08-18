0ccd35aa56c3bb1c7370c6e8faebedbb39c05c54
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-18 18:35:18 +0300

    Nomenclatoare

--- src/Models/Customer/Accounts/Account.php
+++ src/Models/Customer/Accounts/Account.php
@@ -5,22 +5,22 @@ namespace MyDpo\Models\Customer\Accounts;
 use Illuminate\Database\Eloquent\Model;
 // use MyDpo\Helpers\Performers\Datatable\GetItems;
 use MyDpo\Traits\Itemable;
-use MyDpo\Helpers\Performers\Datatable\DoAction;
-use MyDpo\Models\Authentication\User;
-use MyDpo\Models\Customer\Customer;
-use MyDpo\Models\Customer\CustomerDepartment;
-use MyDpo\Models\RoleUser;
-use MyDpo\Models\Activation;
-use MyDpo\Rules\CustomerAccount\ValidAccountEmail;
-use MyDpo\Events\CustomerPersons\CustomerPersonCreateAccount;
-use MyDpo\Performers\CustomerFolder\SaveFoldersAccess;
-use MyDpo\Performers\CustomerAccount\UpdateRole;
-use MyDpo\Performers\CustomerAccount\UpdateStatus;
-use MyDpo\Performers\CustomerAccount\SaveDashboardPermissions;
-use MyDpo\Performers\CustomerAccount\SaveFolderPermissions;
-use MyDpo\Performers\CustomerAccount\SavePermissions;
-use MyDpo\Performers\CustomerAccount\SaveFolderAccess;
-use MyDpo\Performers\CustomerAccount\AssignUser;
+// use MyDpo\Helpers\Performers\Datatable\DoAction;
+// use MyDpo\Models\Authentication\User;
+// use MyDpo\Models\Customer\Customer;
+// use MyDpo\Models\Customer\CustomerDepartment;
+// use MyDpo\Models\RoleUser;
+// use MyDpo\Models\Activation;
+// use MyDpo\Rules\CustomerAccount\ValidAccountEmail;
+// use MyDpo\Events\CustomerPersons\CustomerPersonCreateAccount;
+// use MyDpo\Performers\CustomerFolder\SaveFoldersAccess;
+// use MyDpo\Performers\CustomerAccount\UpdateRole;
+// use MyDpo\Performers\CustomerAccount\UpdateStatus;
+// use MyDpo\Performers\CustomerAccount\SaveDashboardPermissions;
+// use MyDpo\Performers\CustomerAccount\SaveFolderPermissions;
+// use MyDpo\Performers\CustomerAccount\SavePermissions;
+// use MyDpo\Performers\CustomerAccount\SaveFolderAccess;
+// use MyDpo\Performers\CustomerAccount\AssignUser;
 
 use MyDpo\Scopes\NotdeletedScope;
 
@@ -70,202 +70,202 @@ class Account extends Model {
         static::addGlobalScope( new NotdeletedScope() );
     }
 
-    protected $with = [
-        'user', 
-        'department'
-    ];
+    // protected $with = [
+    //     'user', 
+    //     'department'
+    // ];
 
-    protected $appends = [
-        'role'
-    ];
+    // protected $appends = [
+    //     'role'
+    // ];
 
-    public function getRoleAttribute() {
-        $r = NULL;
-        foreach($this->user->roles as $i => $role)
-        {
-            if( ($role->type == 'b2b') && ($role->pivot->customer_id == $this->customer_id) )
-            {
-                $r = $role;
-            }
-        }
-        return $r;
-    }
-
-    public function user() {
-        return $this->belongsTo(User::class, 'user_id');
-    }
-
-    public function customer() {
-        return $this->belongsTo(Customer::class, 'customer_id');
-    }
-
-    public function department() {
-        return $this->belongsTo(CustomerDepartment::class, 'department_id');
-    }
-
-    // public static function getItems($input) {
-
-    //     $q = self::query()->leftJoin(
-    //         'users',
-    //         function($j) {
-    //             $j->on('users.id', '=', 'customers-persons.user_id');
+    // public function getRoleAttribute() {
+    //     $r = NULL;
+    //     foreach($this->user->roles as $i => $role)
+    //     {
+    //         if( ($role->type == 'b2b') && ($role->pivot->customer_id == $this->customer_id) )
+    //         {
+    //             $r = $role;
     //         }
-    //     )
-    //     ->leftJoin(
-    //         'customers',
-    //         function($j) {
-    //             $j->on('customers.id', '=', 'customers-persons.customer_id');
-    //         }
-    //     )
-    //     ->select('customers-persons.*');
-
-    //     return (new GetItems($input, $q->with(['customer', 'department', 'user']), __CLASS__))->Perform();
+    //     }
+    //     return $r;
     // }
 
-    public static function doAction($action, $input) {
-        return (new DoAction($action, $input, __CLASS__))->Perform();
-    }
+    // public function user() {
+    //     return $this->belongsTo(User::class, 'user_id');
+    // }
 
-    public static function doUpdate($input, $account) {
+    // public function customer() {
+    //     return $this->belongsTo(Customer::class, 'customer_id');
+    // }
 
-        $account->update($input);
+    // public function department() {
+    //     return $this->belongsTo(CustomerDepartment::class, 'department_id');
+    // }
 
-        if(array_key_exists('user', $input) && array_key_exists('role_id', $input['user']) )
-        {
-            $roleUser = RoleUser::CreateAccountRole(
-                $input['customer_id'], 
-                $input['user_id'], 
-                $input['user']['role_id']
-            );
-        }
+    // // public static function getItems($input) {
 
-        return $account;
-    }
+    // //     $q = self::query()->leftJoin(
+    // //         'users',
+    // //         function($j) {
+    // //             $j->on('users.id', '=', 'customers-persons.user_id');
+    // //         }
+    // //     )
+    // //     ->leftJoin(
+    // //         'customers',
+    // //         function($j) {
+    // //             $j->on('customers.id', '=', 'customers-persons.customer_id');
+    // //         }
+    // //     )
+    // //     ->select('customers-persons.*');
 
-    public static function doInsert($input) {
+    // //     return (new GetItems($input, $q->with(['customer', 'department', 'user']), __CLASS__))->Perform();
+    // // }
 
-        $accountInput = [
-            'customer_id' => $input['customer_id'],
-            'user_id' => $input['user_id'],
-            'department_id' => $input['department_id'],
-            'newsletter' => $input['newsletter'],
-            'locale' => $input['locale'],
-            'role_id' => $input['role_id'],
-        ];
+    // public static function doAction($action, $input) {
+    //     return (new DoAction($action, $input, __CLASS__))->Perform();
+    // }
 
-        $input['user']['password'] = $password = (\Str::random(10) . 'aA1!');
-        $input['user']['password_confirmation'] = $password;
-        $input['user']['type'] = 'b2b';
+    // public static function doUpdate($input, $account) {
 
-        $user = User::doAction('insert', $input['user']);
+    //     $account->update($input);
 
-        $accountInput['user_id'] = $user['payload']['record']['id'];
+    //     if(array_key_exists('user', $input) && array_key_exists('role_id', $input['user']) )
+    //     {
+    //         $roleUser = RoleUser::CreateAccountRole(
+    //             $input['customer_id'], 
+    //             $input['user_id'], 
+    //             $input['user']['role_id']
+    //         );
+    //     }
 
-        $account = self::create($accountInput);
+    //     return $account;
+    // }
 
-        $roleUser = RoleUser::CreateAccountRole(
-            $input['customer_id'], 
-            $accountInput['user_id'], 
-            $account->role_id,
-        );
+    // public static function doInsert($input) {
 
-        event(new CustomerPersonCreateAccount([
-            ...$input,
-            'account' => $account,
-            'roleUser' => $roleUser,
-        ]));
+    //     $accountInput = [
+    //         'customer_id' => $input['customer_id'],
+    //         'user_id' => $input['user_id'],
+    //         'department_id' => $input['department_id'],
+    //         'newsletter' => $input['newsletter'],
+    //         'locale' => $input['locale'],
+    //         'role_id' => $input['role_id'],
+    //     ];
 
-        return $account;
-    }
+    //     $input['user']['password'] = $password = (\Str::random(10) . 'aA1!');
+    //     $input['user']['password_confirmation'] = $password;
+    //     $input['user']['type'] = 'b2b';
 
-    public static function updateRole($action, $input) {
-        return (new UpdateRole($input))->Perform();
-    }
+    //     $user = User::doAction('insert', $input['user']);
 
-    public static function updateStatus($action, $input) {
-        return (new UpdateStatus($input))->Perform();
-    }
+    //     $accountInput['user_id'] = $user['payload']['record']['id'];
 
-    public static function saveDashboardPermissions($input) {
-        return (new SaveDashboardPermissions($input))->Perform();
-    }
+    //     $account = self::create($accountInput);
 
-    public static function saveFolderPermissions($input) {
-        return (new SaveFolderPermissions($input))->Perform();
-    }
+    //     $roleUser = RoleUser::CreateAccountRole(
+    //         $input['customer_id'], 
+    //         $accountInput['user_id'], 
+    //         $account->role_id,
+    //     );
 
-    public static function savePermissions($input) {
-        return (new SavePermissions($input))->Perform();
-    }
+    //     event(new CustomerPersonCreateAccount([
+    //         ...$input,
+    //         'account' => $account,
+    //         'roleUser' => $roleUser,
+    //     ]));
 
-    public static function saveFolderAccess($input) {
-        return (new SaveFolderAccess($input))->Perform();
-    }
+    //     return $account;
+    // }
 
-    public static function assignUser($input) {
-        return (new AssignUser($input))->Perform();
-    }
+    // public static function updateRole($action, $input) {
+    //     return (new UpdateRole($input))->Perform();
+    // }
 
-    public static function GetRules($action, $input) {
+    // public static function updateStatus($action, $input) {
+    //     return (new UpdateStatus($input))->Perform();
+    // }
+
+    // public static function saveDashboardPermissions($input) {
+    //     return (new SaveDashboardPermissions($input))->Perform();
+    // }
+
+    // public static function saveFolderPermissions($input) {
+    //     return (new SaveFolderPermissions($input))->Perform();
+    // }
+
+    // public static function savePermissions($input) {
+    //     return (new SavePermissions($input))->Perform();
+    // }
+
+    // public static function saveFolderAccess($input) {
+    //     return (new SaveFolderAccess($input))->Perform();
+    // }
+
+    // public static function assignUser($input) {
+    //     return (new AssignUser($input))->Perform();
+    // }
+
+    // public static function GetRules($action, $input) {
        
-        if($action == 'delete')
-        {
-            return NULL;
-        }
+    //     if($action == 'delete')
+    //     {
+    //         return NULL;
+    //     }
 
-        $result = [
-            'customer_id' => 'required|exists:customers,id',
-            'department_id' => 'required|exists:customers-departamente,id', 
-            'role_id' => 'required|in:4,5', 
-        ];
+    //     $result = [
+    //         'customer_id' => 'required|exists:customers,id',
+    //         'department_id' => 'required|exists:customers-departamente,id', 
+    //         'role_id' => 'required|in:4,5', 
+    //     ];
 
-        if($action == 'insert')
-        {
-            $result['user.first_name'] = 'required';
-            $result['user.last_name'] = 'required';
+    //     if($action == 'insert')
+    //     {
+    //         $result['user.first_name'] = 'required';
+    //         $result['user.last_name'] = 'required';
             
-            $result['user.email'] = [
-                'required',
-                'email',
-                new ValidAccountEmail($input)
-            ];
-        }
+    //         $result['user.email'] = [
+    //             'required',
+    //             'email',
+    //             new ValidAccountEmail($input)
+    //         ];
+    //     }
     
-        return $result;
-    }
+    //     return $result;
+    // }
 
-    public static function SyncRecords($customer_id = NULL) {
+    // public static function SyncRecords($customer_id = NULL) {
 
-        if($customer_id)
-        {
-            $accounts = self::where('customer_id', $customer_id)->get();
-        }
-        else
-        {
-            $accounts = self::all();
-        }
+    //     if($customer_id)
+    //     {
+    //         $accounts = self::where('customer_id', $customer_id)->get();
+    //     }
+    //     else
+    //     {
+    //         $accounts = self::all();
+    //     }
 
 
-        foreach($accounts as $i => $account)
-        {
-            $activation = Activation::byUserAndCustomer($account->user_id, $account->customer_id);
+    //     foreach($accounts as $i => $account)
+    //     {
+    //         $activation = Activation::byUserAndCustomer($account->user_id, $account->customer_id);
             
-            if(!! $activation)
-            {
-                $account->activated = $activation->activated;
-                $account->activated_at = $activation->activated_at;
-            }
+    //         if(!! $activation)
+    //         {
+    //             $account->activated = $activation->activated;
+    //             $account->activated_at = $activation->activated_at;
+    //         }
 
-            $roleUser = RoleUser::byUserAndCustomer($account->user_id, $account->customer_id);
+    //         $roleUser = RoleUser::byUserAndCustomer($account->user_id, $account->customer_id);
 
-            if(!! $roleUser)
-            {
-                $account->role_id = $roleUser->role_id;
-            }
+    //         if(!! $roleUser)
+    //         {
+    //             $account->role_id = $roleUser->role_id;
+    //         }
 
-            $account->save();
-        }
-    }
+    //         $account->save();
+    //     }
+    // }
 
     public static function GetQuery() {
         return 
