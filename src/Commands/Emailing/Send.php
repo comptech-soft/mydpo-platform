dc676070124b5fe73b11c8eda50fed48c2c98bd3
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-18 21:59:53 +0300

    Nomenclatoare

--- src/Commands/Emailing/Send.php
+++ src/Commands/Emailing/Send.php
@@ -3,6 +3,8 @@
 namespace MyDpo\Commands\Emailing;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\RateLimiter; 
+
 use MyDpo\Models\Customer\Emails\EmailUser;
 
 class Send extends Command {
@@ -17,10 +19,19 @@ class Send extends Command {
             ->take(10)
             ->get();
 
-        dd($pending_emails);
-
-
-        $this->info('Pam pam. The command was successful!');
+        foreach($pending_emails as $email) 
+        {
+            // Introduceți o pauză de 6 secunde (60 secunde / 10 emailuri)
+            RateLimiter::throttle('email-sending')->allow(1)->every(6);
+    
+            // // Trimiteți emailul folosind clasa de email corespunzătoare
+            // \Mail::to($email->email_address)->send(new YourMailClass($email));
+            
+            // // Actualizați câmpul 'sended_at' pentru email
+            // $email->update(['sended_at' => now()]);
+        }
+        
+        $this->info('Sent ' . count($pending_emails) . ' emails.');
     
     }
 }
