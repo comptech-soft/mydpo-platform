293e1c6f69dc279a41887c657c23ec949811eb45
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-19 19:12:05 +0300

    Nomenclatoare

---
+++ src/Http/Controllers/Authentication/TeamController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Authentication;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Core\Http\Response\Index;
+use MyDpo\Models\Authentication\User;;
+
+class TeamController extends Controller {
+    
+    public function index(Request $r) {
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/authentication/team/index.js'],
+        );        
+    }
+
+}
