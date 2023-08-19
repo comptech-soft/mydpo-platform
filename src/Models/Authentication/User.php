d397936424645fb46053531fe7a1a359429235de
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-19 15:32:53 +0300

    Nomenclatoare

--- src/Models/Authentication/User.php
+++ src/Models/Authentication/User.php
@@ -78,7 +78,7 @@ class User extends Authenticatable implements CanResetPassword, MustVerifyEmail 
 
     protected $with = [
         'roles',
-        'customers',
+        // 'customers',
     ];
 
     protected $appends  = [
