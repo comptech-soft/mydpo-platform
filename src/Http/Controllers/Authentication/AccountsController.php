b9fe2803cf26b5e2b9d8826e7cc97e42c398f79f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-19 19:59:06 +0300

    Nomenclatoare

---
+++ src/Http/Controllers/Authentication/AccountsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Authentication;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Core\Http\Response\Index;
+use MyDpo\Models\Customer\Accounts\Account;
+
+// use MyDpo\Models\Authentication\User;
+
+class TeamController extends Controller {
+    
+    public function index(Request $r) {
+
+        Account::SyncRecords();
+
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/authentication/accounts/index.js'],
+        );        
+    }
+
+}
+
+// use App\Http\Controllers\Controller;
+// use MyDpo\Helpers\Response;
+// use MyDpo\Models\Customer\CustomerAccount;
+
+
+
+// class PersonsController extends Controller {
+    
+//     public function index() {
+//         
+
+//         return Response::View(
+//             '~templates.index', 
+//             asset('apps/persons/index.js')
+//         );
+//     }
+
+// }
--- src/Http/Controllers/Authentication/TeamController.php
+++ src/Http/Controllers/Authentication/TeamController.php
@@ -5,7 +5,7 @@ namespace MyDpo\Http\Controllers\Authentication;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
-use MyDpo\Models\Authentication\User;;
+// use MyDpo\Models\Authentication\User;
 
 class TeamController extends Controller {
     
