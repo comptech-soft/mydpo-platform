d43174ce75060b235bbb4b14cbd3b192c2eb03d2
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-19 23:58:14 +0300

    Nomenclatoare

---
+++ src/Http/Controllers/Authentication/CustomersAccountsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Authentication;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Core\Http\Response\Index;
+// use MyDpo\Models\Customer\Accounts\Account;
+
+// use MyDpo\Models\Authentication\User;
+
+class CustomersAccountsController extends Controller {
+    
+    public function index(Request $r) {
+
+        Account::SyncRecords();
+
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/authentication/customer-accounts/index.js'],
+        );        
+    }
+
+    // public function getUsers(Request $r) {
+
+    //     return Account::GetUsers($r->all());
+    // }
+
+}
