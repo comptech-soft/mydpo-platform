772c936407e96a2f4bb4e6b701921145e6fda093
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-20 02:03:46 +0300

    Nomenclatoare

--- src/Http/Controllers/Admin/Customer/ComenziController.php
+++ src/Http/Controllers/Admin/Customer/ComenziController.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
 
-class ContracteController extends Controller {
+class ComenziController extends Controller {
     
     public function index(Request $r) {
         return Index::View(
             styles: ['css/app.css'],
-            scripts: ['apps/admin/contracte/index.js'],
+            scripts: ['apps/admin/comenzi/index.js'],
         );        
     }
 
