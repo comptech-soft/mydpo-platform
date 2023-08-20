f214a604be6cd49418f3f8efc46146ff5bde8aa2
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-20 22:38:56 +0300

    Nomenclatoare

--- src/Http/Controllers/Nomenclatoare/Livrabile/Registre/TipRegistruColoaneController.php
+++ src/Http/Controllers/Nomenclatoare/Livrabile/Registre/TipRegistruColoaneController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MyDpo\Http\Controllers\Admin\Livrabile;
+namespace MyDpo\Http\Controllers\Nomenclatoare\Livrabile\Registre;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
