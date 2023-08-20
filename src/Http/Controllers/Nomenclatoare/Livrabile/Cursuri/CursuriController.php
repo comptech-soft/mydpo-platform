3f53e2c18e63274f8ccb40683d506ef258d1dbde
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-20 22:10:27 +0300

    Nomenclatoare

--- src/Http/Controllers/Nomenclatoare/Livrabile/Cursuri/CursuriController.php
+++ src/Http/Controllers/Nomenclatoare/Livrabile/Cursuri/CursuriController.php
@@ -1,23 +1,31 @@
 <?php
 
-namespace MyDpo\Http\Controllers\Admin;
+namespace MyDpo\Http\Controllers\Nomenclatoare\Livrabile\Cursuri;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use MyDpo\Helpers\Response;
-use MyDpo\Models\Curs;
+use MyDpo\Core\Http\Response\Index;
+// use MyDpo\Models\Curs;
 
 class CursuriController extends Controller {
     
     public function index(Request $r) {
 
-        Curs::calculateInfos();
+        // Curs::calculateInfos();
 
-        return Response::View(
-            '~templates.index', 
-            asset('apps/elearning/index.js')
+        // return Response::View(
+        //     '~templates.index', 
+        //     asset('apps/elearning/index.js')
+        // );
+
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/admin/cursuri/index.js'],
+            
         );
     }
+
+    
 
     public function getKnolyxCourses(Request $r) {
         return Curs::getKnolyxCourses($r->all());
