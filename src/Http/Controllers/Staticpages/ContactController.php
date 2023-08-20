e8e97dc0578839047ac77fef4c61fa48c203df42
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-20 23:55:25 +0300

    Nomenclatoare

--- src/Http/Controllers/Staticpages/ContactController.php
+++ src/Http/Controllers/Staticpages/ContactController.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace MyDpo\Http\Controllers\System;
+namespace MyDpo\Http\Controllers\Staticpages;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
 
-class ContactController extends Controller
-{
+class ContactController extends Controller {
 
     public function index(Request $r) 
     {
