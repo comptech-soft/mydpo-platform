5c117a3585e0b2bcba8f283e1909d71ac3bd6f47
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-20 10:38:23 +0300

    Nomenclatoare

--- src/Models/Customer/Contracts/OrderService.php
+++ src/Models/Customer/Contracts/OrderService.php
@@ -3,12 +3,12 @@
 namespace MyDpo\Models\Customer\Contracts;
 
 use Illuminate\Database\Eloquent\Model;
-use MyDpo\Helpers\Performers\Datatable\GetItems;
-use MyDpo\Helpers\Performers\Datatable\DoAction;
-use MyDpo\Models\Service;
-use MyDpo\Models\CustomerOrder;
-use MyDpo\Models\Customer\Customer_base as Customer;
-use MyDpo\Rules\CustomerService\UniqueOrderService;
+// use MyDpo\Helpers\Performers\Datatable\GetItems;
+// use MyDpo\Helpers\Performers\Datatable\DoAction;
+// use MyDpo\Models\Service;
+// use MyDpo\Models\CustomerOrder;
+// use MyDpo\Models\Customer\Customer_base as Customer;
+// use MyDpo\Rules\CustomerService\UniqueOrderService;
 
 class OrderService extends Model {
 
@@ -50,50 +50,50 @@ class OrderService extends Model {
         'deleted_by'
     ];
 
-    public function service() {
-        return $this->belongsTo(Service::class, 'service_id');
-    }
+    // public function service() {
+    //     return $this->belongsTo(Service::class, 'service_id');
+    // }
 
-    public function order() {
-        return $this->belongsTo(CustomerOrder::class, 'order_id');
-    }
+    // public function order() {
+    //     return $this->belongsTo(CustomerOrder::class, 'order_id');
+    // }
 
-    public function customer() {
-        return $this->belongsTo(Customer::class, 'customer_id');
-    }
+    // public function customer() {
+    //     return $this->belongsTo(Customer::class, 'customer_id');
+    // }
 
-    public static function getItems($input) {
-        return (new GetItems($input, self::query()->with(['customer', 'order.contract', 'service.type']), __CLASS__))->Perform();
-    }
+    // public static function getItems($input) {
+    //     return (new GetItems($input, self::query()->with(['customer', 'order.contract', 'service.type']), __CLASS__))->Perform();
+    // }
 
-    public static function doAction($action, $input) {
-        return (new DoAction($action, $input, __CLASS__))->Perform();
-    }
+    // public static function doAction($action, $input) {
+    //     return (new DoAction($action, $input, __CLASS__))->Perform();
+    // }
 
-    public static function doInsert($input, $service) {
-        $service = self::create($input);
-        return $service;
-    }
+    // public static function doInsert($input, $service) {
+    //     $service = self::create($input);
+    //     return $service;
+    // }
 
-    public static function GetRules($action, $input) {
+    // public static function GetRules($action, $input) {
 
-        if($action == 'delete')
-        {
-            return NULL;
-        }
+    //     if($action == 'delete')
+    //     {
+    //         return NULL;
+    //     }
 
-        $result = [
-            'customer_id' => 'required|exists:customers,id',
-            'contract_id' => 'required|exists:customers-contracts,id',
-            'order_id' => 'required|exists:customers-orders,id',
-            'service_id' => [
-                'required',
-                'exists:services,id',
-                new UniqueOrderService($input),
-            ],
-        ];
-        return $result;
-    }
+    //     $result = [
+    //         'customer_id' => 'required|exists:customers,id',
+    //         'contract_id' => 'required|exists:customers-contracts,id',
+    //         'order_id' => 'required|exists:customers-orders,id',
+    //         'service_id' => [
+    //             'required',
+    //             'exists:services,id',
+    //             new UniqueOrderService($input),
+    //         ],
+    //     ];
+    //     return $result;
+    // }
 
     public static function syncWithOrders() {
         $records = self::with(['order'])->get();
