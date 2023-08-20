5a7eb0cd194e43ef26be8278f8b5d738de361c09
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-20 10:51:17 +0300

    Nomenclatoare

--- src/Routes/guest/routes.php
+++ src/Routes/guest/routes.php
@@ -1,25 +1,25 @@
 <?php
 
-use MyDpo\Http\Controllers\Usersession\ForgotPasswordController;
-use MyDpo\Http\Controllers\Usersession\NewPasswordController;
+// use MyDpo\Http\Controllers\Usersession\ForgotPasswordController;
+// use MyDpo\Http\Controllers\Usersession\NewPasswordController;
 
-use MyDpo\Http\Controllers\Auth\AuthenticatedSessionController;
+// use MyDpo\Http\Controllers\Auth\AuthenticatedSessionController;
 
-Route::middleware('guest')->group(function () {
+// Route::middleware('guest')->group(function () {
 
-    /**
-     * Resetarea parolei
-     */
-    Route::get('forgot-password',  [ForgotPasswordController::class, 'index']);
-    Route::post('system/forgot-password',  [ForgotPasswordController::class, 'sendResetPasswordLink']);
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'index'])->name('password.reset');
-    Route::post('system/reset-password', [NewPasswordController::class, 'updateNewPassword']);
+//     /**
+//      * Resetarea parolei
+//      */
+//     Route::get('forgot-password',  [ForgotPasswordController::class, 'index']);
+//     Route::post('system/forgot-password',  [ForgotPasswordController::class, 'sendResetPasswordLink']);
+//     Route::get('reset-password/{token}', [NewPasswordController::class, 'index'])->name('password.reset');
+//     Route::post('system/reset-password', [NewPasswordController::class, 'updateNewPassword']);
 
 
-    /**
-     * Atentie la asta! Trebuie pentru SSO
-     */
-    Route::get('connect', [AuthenticatedSessionController::class, 'create'])->name('login');
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+//     /**
+//      * Atentie la asta! Trebuie pentru SSO
+//      */
+//     Route::get('connect', [AuthenticatedSessionController::class, 'create'])->name('login');
+//     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-});
+// });
