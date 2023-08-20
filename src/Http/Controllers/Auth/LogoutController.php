23e329eefdc77ce59b656bea07caa610f52f2e2f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-20 23:35:14 +0300

    Nomenclatoare

--- src/Http/Controllers/Auth/LogoutController.php
+++ src/Http/Controllers/Auth/LogoutController.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace MyDpo\Http\Controllers\Usersession;
+namespace MyDpo\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Helpers\UserSession;
 
-class LogoutController extends Controller
-{
+class LogoutController extends Controller {
 
     public function logout() {
         return UserSession::logout();
