04ee3948f98e86febe033ca06561240be21a2df4
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-20 11:31:24 +0300

    Nomenclatoare

--- src/Http/Controllers/Auth/ConfigController.php
+++ src/Http/Controllers/Auth/ConfigController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MyDpo\Http\Controllers\System;
+namespace MyDpo\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -13,25 +13,25 @@ class ConfigController extends Controller {
         return Config::getConfig();
     }
 
-    public function getUserRole(Request $r) {
-        return Config::getUserRole($r->all());
-    }
+    // public function getUserRole(Request $r) {
+    //     return Config::getUserRole($r->all());
+    // }
     
-    public function saveActiveCustomer(Request $r) {
-        return UserSetting::saveActiveCustomer($r->all());
-    }
+    // public function saveActiveCustomer(Request $r) {
+    //     return UserSetting::saveActiveCustomer($r->all());
+    // }
     
-    public function setLocale($locale) {
+    // public function setLocale($locale) {
         
-        if(! in_array($locale, ['en', 'ro']))
-        {
-            $locale = 'ro';
-        }
+    //     if(! in_array($locale, ['en', 'ro']))
+    //     {
+    //         $locale = 'ro';
+    //     }
 
-        \App::setlocale($locale);
-        session()->put('locale', $locale);
+    //     \App::setlocale($locale);
+    //     session()->put('locale', $locale);
 
-        return redirect()->back();
-    } 
+    //     return redirect()->back();
+    // } 
 
 }
