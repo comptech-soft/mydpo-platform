82e53f9b64a8fec7412d4eeeaf6d5a8268f6fdf8
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-21 13:55:10 +0300

    Nomenclatoare

--- src/Http/Controllers/Nomenclatoare/Livrabile/Cursuri/CursuriTypesController.php
+++ src/Http/Controllers/Nomenclatoare/Livrabile/Cursuri/CursuriTypesController.php
@@ -5,12 +5,12 @@ namespace MyDpo\Http\Controllers\Nomenclatoare\Livrabile\Cursuri;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 // use MyDpo\Helpers\Response;
-// use MyDpo\Models\CursType;
+use MyDpo\Models\Nomenclatoare\Livrabile\ELearning\Type;
 
 class CursuriTypesController extends Controller {
 
-    // public function getItems(Request $r) {
-    //     return CursType::getItems($r->all());
-    // }
+    public function getRecords(Request $r) {
+        return Type::getRecords($r->all());
+    }
 
 }
