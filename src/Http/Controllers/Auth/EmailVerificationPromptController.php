a323dd5869667b83bd5a8495f1377fccd091eb94
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-21 10:19:37 +0300

    Nomenclatoare

--- src/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ src/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -1,17 +1,15 @@
 <?php
 
-namespace MyDpo\Http\Controllers\Usersession;
+namespace MyDpo\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use MyDpo\Helpers\Response;
 
-class EmailVerificationPromptController extends Controller
-{
+class EmailVerificationPromptController extends Controller {
     
-    public function index(Request $request)
-    {
+    public function index(Request $request) {
         if($request->user()->hasVerifiedEmail())
         {
             return redirect()->intended('dashboard');
