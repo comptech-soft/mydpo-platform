fc0c4720eb37cdd57b9015454f90a984db252138
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-21 13:53:26 +0300

    Nomenclatoare

--- src/Http/Controllers/Nomenclatoare/Livrabile/Cursuri/CursuriTypesController.php
+++ src/Http/Controllers/Nomenclatoare/Livrabile/Cursuri/CursuriTypesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MyDpo\Http\Controllers\Admin;
+namespace MyDpo\Http\Controllers\Nomenclatoare\Livrabile\Cursuri;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
