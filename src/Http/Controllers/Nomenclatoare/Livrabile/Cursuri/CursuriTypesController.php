00483755626302c8edaf1ffa0e728caa16107995
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-21 13:52:25 +0300

    Nomenclatoare

---
+++ src/Http/Controllers/Nomenclatoare/Livrabile/Cursuri/CursuriTypesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+// use MyDpo\Helpers\Response;
+// use MyDpo\Models\CursType;
+
+class CursuriTypesController extends Controller {
+
+    // public function getItems(Request $r) {
+    //     return CursType::getItems($r->all());
+    // }
+
+}
