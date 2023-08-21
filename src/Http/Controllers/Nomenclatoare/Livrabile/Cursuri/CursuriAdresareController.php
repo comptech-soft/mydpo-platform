ac692b005636bab936c2dda5d3a79efb140cd456
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-21 14:25:23 +0300

    Nomenclatoare

---
+++ src/Http/Controllers/Nomenclatoare/Livrabile/Cursuri/CursuriAdresareController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Nomenclatoare\Livrabile\Cursuri;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+// use MyDpo\Helpers\Response;
+// use MyDpo\Models\Cursadresare;
+
+class CursuriAdresareController extends Controller {
+
+    // public function getItems(Request $r) {
+    //     return Cursadresare::getItems($r->all());
+    // }
+
+    // public function doAction($action, Request $r) {
+    //     return Cursadresare::doAction($action, $r->all());
+    // }
+
+}
