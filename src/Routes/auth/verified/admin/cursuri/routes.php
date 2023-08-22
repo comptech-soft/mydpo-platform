6a0f5b132ecf81a882fd63d9545f7fbd0896c066
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-08-22 04:18:17 +0300

    Nomenclatoare

--- src/Routes/auth/verified/admin/cursuri/routes.php
+++ src/Routes/auth/verified/admin/cursuri/routes.php
@@ -1,21 +1,21 @@
 <?php
 
-use MyDpo\Http\Controllers\Admin\CursuriController;
-use MyDpo\Http\Controllers\Admin\AccesCursController;
+// use MyDpo\Http\Controllers\Admin\CursuriController;
+// use MyDpo\Http\Controllers\Admin\AccesCursController;
 
-Route::prefix('cursuri')->group( function() {
+// Route::prefix('cursuri')->group( function() {
         
-    Route::get('/', [CursuriController::class, 'index']);        
-    Route::post('items', [CursuriController::class, 'getItems']);
-    Route::post('action/{action}', [CursuriController::class, 'doAction']);    
+//     Route::get('/', [CursuriController::class, 'index']);        
+//     Route::post('items', [CursuriController::class, 'getItems']);
+//     Route::post('action/{action}', [CursuriController::class, 'doAction']);    
 
-    Route::post('get-knolyx-courses', [CursuriController::class, 'getKnolyxCourses']);
+//     Route::post('get-knolyx-courses', [CursuriController::class, 'getKnolyxCourses']);
 
-});
+// });
 
-Route::prefix('acces-curs')->group( function() {
+// Route::prefix('acces-curs')->group( function() {
         
-    Route::get('/{curs_id}', [AccesCursController::class, 'index']);        
+//     Route::get('/{curs_id}', [AccesCursController::class, 'index']);        
 
 
-});
+// });
