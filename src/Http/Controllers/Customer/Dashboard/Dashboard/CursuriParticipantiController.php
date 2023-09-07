fb895d4a7df2f284157eba5fa1dfe01482620c64
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-09-07 13:09:24 +0300

    Cursuri

---
+++ src/Http/Controllers/Customer/Dashboard/Dashboard/CursuriParticipantiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Customer\Dashboard\Dashboard;
+
+use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+// use MyDpo\Helpers\Response;
+// use MyDpo\Models\Customer\CustomerCursParticipant;
+
+class CursuriParticipantiController extends Controller {
+    
+    // public function getItems(Request $r) {
+    //     return CustomerCursParticipant::getItems($r->all());
+    // }
+
+    // public function importParticipants(Request $r) {
+    //     return CustomerCursParticipant::importParticipants($r->all());
+    // }
+    
+
+}
