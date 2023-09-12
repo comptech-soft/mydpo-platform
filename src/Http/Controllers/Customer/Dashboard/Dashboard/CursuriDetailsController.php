83dce4b4778cc6ee17d57edd9f4219140f15d0fc
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-09-12 09:20:53 +0300

    Cursuri

--- src/Http/Controllers/Customer/Dashboard/Dashboard/CursuriDetailsController.php
+++ src/Http/Controllers/Customer/Dashboard/Dashboard/CursuriDetailsController.php
@@ -4,37 +4,35 @@ namespace MyDpo\Http\Controllers\Customer\Dashboard\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-// use MyDpo\Helpers\Response;
-// use MyDpo\Models\Customer\CustomerCurs;
-// use MyDpo\Models\Customer\Customer;
+use MyDpo\Core\Http\Response\Index;
+use MyDpo\Models\Customer\CustomerCurs;
+use MyDpo\Models\Customer\Customer;
 
 class CursuriDetailsController extends Controller {
     
     public function index($customer_id, $customer_curs_id, Request $r) {
 
-        dd(__METHOD__);
-        // $record = CustomerCurs::find($customer_curs_id);
-        // if(! $record)
-        // {
-        //     $customer = Customer::find($customer_id);
+        if(! ($customer = Customer::find($customer_id)) )
+        {
+            return redirect('clienti');
+        }
 
-        //     if(  $customer )
-        //     {
-        //         return redirect(config('app.url') . '/customer-cursuri/' . $customer_id);
-        //     }
+        if(! ($customer_curs = CustomerCurs::find($customer_curs_id)) )
+        {
+            return redirect(config('app.url') . '/customer-elearning/' . $customer_id);
+        }
 
-        //     return redirect('/');
-        // }
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/customer/elearning-details/index.js'],
+            payload: [
+                'customer_id' => $customer_id,
+                'customer' => Customer::find($customer_id),
+                'customer_curs_id' => $customer_curs_id,
+                'customer_curs' => $customer_curs,
+            ],
+        );     
 
-        // return Response::View(
-        //     '~templates.index', 
-        //     asset('apps/customer-cursuri-acces-curs/index.js'),
-        //     [], 
-        //     [
-        //         'customer_id' => $customer_id,
-        //         'customer_curs_id' => $customer_curs_id,
-        //     ]
-        // );
     }
 
 }
