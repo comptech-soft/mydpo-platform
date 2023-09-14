f56f7eca289a6a3081b61469e335594b6d297d17
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-09-14 11:41:49 +0300

    b2b

--- src/Http/Controllers/Auth/MyCustomersController.php
+++ src/Http/Controllers/Auth/MyCustomersController.php
@@ -3,25 +3,15 @@
 namespace MyDpo\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 // use MyDpo\Helpers\Response;
 // use MyDpo\Helpers\UserSession;
 
 class MyCustomersController extends Controller {
     
-    // public function index(Request $r) {
-    //     return Response::View(
-    //         '~templates.index', 
-    //         asset('apps/login/index.js')
-    //     );
-    // }
+    public function index(Request $r) {
 
-    // public function login(Request $r) {
-    //     return UserSession::login($r->only([
-    //         'email', 
-    //         'password', 
-    //         'remember_me'
-    //     ]));
-    // }
+        dd(__METHOD__);
+    }
 
 }
