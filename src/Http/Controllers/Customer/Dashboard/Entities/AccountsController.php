075a7d4b04015b6e162d29d238641ed297ff5a05
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-09-14 19:00:29 +0300

    b2b

--- src/Http/Controllers/Customer/Dashboard/Entities/AccountsController.php
+++ src/Http/Controllers/Customer/Dashboard/Entities/AccountsController.php
@@ -17,6 +17,7 @@ class AccountsController extends Controller {
             payload: [
                 'customer_id' => $customer_id,
                 'customer' => Customer::find($customer_id),
+                'customer_user' => \Auth::user(),
             ],
         );        
     }
