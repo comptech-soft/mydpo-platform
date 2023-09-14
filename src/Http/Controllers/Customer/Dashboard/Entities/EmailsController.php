2e02dffc3b82c257d3efc80da1675c4ded9b1017
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-09-14 19:15:25 +0300

    b2b

--- src/Http/Controllers/Customer/Dashboard/Entities/EmailsController.php
+++ src/Http/Controllers/Customer/Dashboard/Entities/EmailsController.php
@@ -17,6 +17,7 @@ class EmailsController extends Controller {
             payload: [
                 'customer_id' => $customer_id,
                 'customer' => Customer::find($customer_id),
+                'customer_user' => \Auth::user(),
             ],
         );        
     }
--- src/Http/Controllers/Customer/Dashboard/Entities/NotificariController.php
+++ src/Http/Controllers/Customer/Dashboard/Entities/NotificariController.php
@@ -17,6 +17,7 @@ class NotificariController extends Controller {
             payload: [
                 'customer_id' => $customer_id,
                 'customer' => Customer::find($customer_id),
+                'customer_user' => \Auth::user(),
             ],
         );        
     }
