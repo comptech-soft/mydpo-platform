7266f484642049bf0cd59f5226f272925f4e08c6
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-09-14 11:40:36 +0300

    b2b

---
+++ src/Http/Controllers/Auth/MyCustomersController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+// use MyDpo\Helpers\Response;
+// use MyDpo\Helpers\UserSession;
+
+class MyCustomersController extends Controller {
+    
+    // public function index(Request $r) {
+    //     return Response::View(
+    //         '~templates.index', 
+    //         asset('apps/login/index.js')
+    //     );
+    // }
+
+    // public function login(Request $r) {
+    //     return UserSession::login($r->only([
+    //         'email', 
+    //         'password', 
+    //         'remember_me'
+    //     ]));
+    // }
+
+}
