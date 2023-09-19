43bd471e524bf2f0317c5afe0855e4e38712f2e9
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-09-19 12:03:34 +0300

    b2b

---
+++ src/Http/Controllers/Customer/Dashboard/Dashboard/FilesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Customer\Dashboard\Dashboard;
+
+use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+// use MyDpo\Models\Customer\CustomerFile;
+
+class FilesController extends Controller {
+
+    // public function getItems(Request $r) {
+    //     return CustomerFile::getItems($r->all());
+    // }
+
+    // public function doAction($action, Request $r) {
+    //     return CustomerFile::doAction($action, $r->all());
+    // }
+
+    // public function changeFilesStatus(Request $r) {
+    //     return CustomerFile::changeFilesStatus($r->all());
+    // }
+
+    // public function moveFiles(Request $r) {
+    //     return CustomerFile::moveFiles($r->all());
+    // }
+    
+    // public function deleteFiles(Request $r) {
+    //     return CustomerFile::deleteFiles($r->all());
+    // }
+    
+
+}
