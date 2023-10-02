fb143ca0a458270d356b9add8ffaa4c0903c7039
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-10-02 10:01:29 +0300

    Documentsable

--- src/Models/Customer/Planuriconformare/Planconformare.php
+++ src/Models/Customer/Planuriconformare/Planconformare.php
@@ -5,7 +5,7 @@ namespace MyDpo\Models\Customer\Planuriconformare;
 use Illuminate\Database\Eloquent\Model;
 use MyDpo\Traits\Itemable;
 use MyDpo\Traits\Actionable;
-use MyDpo\Models\Livrabile\Planconformare as Structura;
+use MyDpo\Models\Livrabile\Planuriconformare\Planconformare as Structura;
 use MyDpo\Models\Customer\Departments\Department;
 use MyDpo\Traits\Exportable;
 use MyDpo\Exports\CustomerPlanconformare\Exporter;
