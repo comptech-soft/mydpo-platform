b69c3318c50cfbf51e168c0269feb1fc4ec2bee1
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-11-05 20:53:47 +0200

    Issues

--- src/Exports/Customer/Registru/Exporter.php
+++ src/Exports/Customer/Registru/Exporter.php
@@ -1,25 +1,47 @@
 <?php
 
-namespace MyDpo\Exports\Customer\Registru;
+namespace MyDpo\Exports\Customer\Centralizator;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithStrictNullComparison;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\FromView;
-use MyDpo\Models\CustomerRegister;
+
 use MyDpo\Models\Customer\Departments\Department;
+use MyDpo\Models\Customer\Registre\Registru;
+use MyDpo\Models\Customer\Registre\Row;
 
 use Illuminate\Contracts\View\View;
 
 class Exporter implements FromView, WithStrictNullComparison, ShouldAutoSize {
 
-    public $registru = NULL;
-    public $juststructure = 1;
-    public $departamente_ids = NULL;
+    public $input = NULL;
+
+    /**
+     * Departamentele ce se exporta
+     */
+    public $department_ids = NULL;
+
+    /**
+     * Departamentele
+     */
+    public $departamente = [];
+    public $just_structure = NULL;
+
+    /**
+     * Id-ul registrului
+     */
+    public $id = NULL;
+    public $document = NULL;
+
+    protected $myclasses = [
+        'centralizatoare' => [
+            'document' => Centralizator::class,
+            'row' => Row::class,
+        ],
+    ];
 
     public function __construct($input) {
-
-        dd($input);
         $this->input = $input;   
         
         $this->department_ids = array_key_exists('department_ids', $input) ? $input['department_ids'] : [];
@@ -33,50 +55,101 @@ class Exporter implements FromView, WithStrictNullComparison, ShouldAutoSize {
         
         $this->document = $this->GetDocument();
     }
-
+    
     public function view(): View {
-
-        if($this->juststructure == 1)
-        {
-            $records = [];
-        }
-        else
-        {
-            $index = NULL;
-            foreach($this->registru->columns as $i => $column)
-            {
-                if($column['type'] == 'DEPARTAMENT')
-                {
-                    $index = $i;
-                }
-            }
-
-            if(! $index )
-            {
-                $records = $this->registru->records;
-            }
-            else
-            {
-
-                $departamente = $this->departamente;
-                $records = collect($this->registru->records)->filter( function($item) use ($departamente, $index){
-
-                    if(! array_key_exists($index, $item) )
-                    {
-                        return TRUE;
-                    }
-                    
-                    return in_array($item[$index], $departamente);
-
-                })->toArray();
-            }
-        }
-
-        return view('exports.customer-register.export', [
-            'columns' => $this->registru->columns,
-            'records' => $records,
-            'children' => $this->registru->children_columns,
+        return view('exports.customer.centralizatorable.export', [
+            'document' => $this->document->toArray(),
+            'header' => $this->GetHeader(),
+            'rows' => $this->just_structure == 1 ? [] : $this->GetRows(),
+            'columns' => $this->just_structure == 1 ? [] : $this->GetColumns(),
         ]);
+    }
+
+    protected function GetRows(){
+        $rows = $this->myclasses['centralizatoare']['row']::where('customer_centralizator_id', $this->input['document_id'])
+            ->orderBy('order_no')
+            ->get()
+            ->map(function($row) {
+                return [
+                    'visibility' => $row->visibility,
+                    'status' => $row->status,
+                    'department' => $row->department->departament,
+                    'values' => $this->GetValues($row),
+                ];
+            })
+            ->toArray();
+
+        return $rows;
+    }
+
+    protected function GetValue($record) {
+
+        $value = $record['value'];
+
+        if($record['type'] == 'O')
+        {
+            $options = collect(collect($this->document->columns_with_values)->first(function($column) use ($record){
+                return $column['id'] == $record['column_id'];
+            })['props'])->pluck('text', 'value')->toArray();
+
+            return $options[$value];
+
+        }
+
+        if($record['type'] == 'D')
+        {
+            if(!! $value )
+            {
+                return \Carbon\Carbon::createFromFormat('Y-m-d', $value)->format('d.m.Y');
+            }
+        }
+
+        if($record['type'] == 'T')
+        {
+            if(!! $value )
+            {
+                return \Carbon\Carbon::createFromFormat('Y-m-d, H:i', $value)->format('d.m.Y, H:i');
+            }
+        }
+
+        return $value;
+    }
+
+    protected function GetValues($row) {
+
+        $records = $row->props['rowvalues'];
+
+        foreach($records as $i => $record)
+        {
+            $records[$i]['value'] = $this->GetValue($record);
+        }
+        
+        return $records;
+    }
+
+    protected function GetColumns() {
+
+        return collect($this->document->columns_with_values)
+            ->filter( function($column) {
+                return ! in_array($column['type'], ['NRCRT', 'VISIBILITY', 'STATUS', 'DEPARTMENT', 'CHECK', 'FILES', 'EMPTY']);
+            })
+            ->toArray();
+    }
+
+    protected function GetHeader() {
+        /**
+         * Nu vom transmite coloanele CHECK, FILES
+         */
+
+        return collect($this->document->table_headers)
+            ->filter( function($column) {
+                return ! in_array($column['type'], ['CHECK', 'FILES', 'EMPTY']);
+            })
+            ->toArray();
+    }
+
+    protected function GetDocument() {
+        return $this->myclasses['centralizatoare']['document']::find($this->input['document_id']);
     }
 
 }
