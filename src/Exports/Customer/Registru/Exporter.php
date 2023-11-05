3559b0f713e3226da1986b1dae0efc5222ecfb4e
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-11-05 20:55:18 +0200

    Issues

--- src/Exports/Customer/Registru/Exporter.php
+++ src/Exports/Customer/Registru/Exporter.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MyDpo\Exports\Customer\Centralizator;
+namespace MyDpo\Exports\Registru\Centralizator;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithStrictNullComparison;
