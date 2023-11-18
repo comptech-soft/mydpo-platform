ffc7007ab9f84dfa6cdd2fd987144e39aee4a3e9
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-11-18 23:44:25 +0200

    Issues

--- src/Http/Controllers/Customer/Dashboard/DashboardController.php
+++ src/Http/Controllers/Customer/Dashboard/DashboardController.php
@@ -17,9 +17,6 @@ class DashboardController extends Controller {
      * Se afiseaza linkuri catre livrabile si catre entitati
      */
     public function index($customer_id, Request $r) {
-
-        dd(Item::getByColumns(), \Auth::user()->toArray());
-
         return Index::View(
             styles: ['css/app.css'],
             scripts: ['apps/customer/dashboard/index.js'],
