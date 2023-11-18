6c70c3535c78089da440ca2e875b4510c91e9b72
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-11-18 23:51:48 +0200

    Issues

--- src/Models/Customer/Dashboard/Summaries/Documents.php
+++ src/Models/Customer/Dashboard/Summaries/Documents.php
@@ -6,22 +6,26 @@ class Documents {
 
     public static function CountLivrabile($customer_id) {
 
-        $sql = "
-            SELECT
-                COUNT(*) AS count_records
-            FROM `customers-files`
-            LEFT JOIN `customers-folders`
-            ON `customers-folders`.`id` = `customers-files`.`folder_id`
-            WHERE 
-                (`customers-folders`.`type` = 'documente') AND
-                (`customers-files`.`customer_id` = " . $customer_id . ")
-            ";
+        dd(\Auth::user()->role->toArray());
+        if( (config('app.platform') == 'admin') || ((config('app.platform') == 'b2b') && (1 > 0)) )
+        {
+            $sql = "
+                SELECT
+                    COUNT(*) AS count_records
+                FROM `customers-files`
+                LEFT JOIN `customers-folders`
+                ON `customers-folders`.`id` = `customers-files`.`folder_id`
+                WHERE 
+                    (`customers-folders`.`type` = 'documente') AND
+                    (`customers-files`.`customer_id` = " . $customer_id . ")
+                ";
 
-        $records = \DB::select($sql);
-
-        return 1154;
+            $records = \DB::select($sql);
         
-        return $records[0]->count_records;
+            return $records[0]->count_records;
+        }
+
+        return -198;
     } 
 
 }
