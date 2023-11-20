2f00a99b221fdb4044e849b9355fb4507b830210
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-11-20 13:16:17 +0200

    Issues

--- src/Http/Controllers/Customer/Dashboard/Entities/AccountDashboardController.php
+++ src/Http/Controllers/Customer/Dashboard/Entities/AccountDashboardController.php
@@ -5,20 +5,26 @@ namespace MyDpo\Http\Controllers\Customer\Dashboard\Entities;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
-// use MyDpo\Models\Customer\Accounts\Account;
+use MyDpo\Models\Customer\Accounts\Account;
 
 class AccountDashboardController extends Controller {
     
-    public function index($account_id, Request $r) {
+    public function index($customer_id, $account_id, Request $r) {
 
-        dd($account_id);
-        
+        $account = Account::find($account_id);
+
+        if(! $account || $account->customer_id != $customer_id)
+        {
+            return redirect('/');
+        }
+
         return Index::View(
             styles: ['css/app.css'],
-            scripts: ['apps/customer/conturi/index.js'],
+            scripts: ['apps/customer/cont-dashboard/index.js'],
             payload: [
-                'customer_id' => $customer_id,
-                'customer' => Customer::find($customer_id),
+                'customer_id' => $account->customer_id,
+                'customer' => $account->customer->toArray(),
+                'account' => $account->toArray(),
                 'customer_user' => \Auth::user(),
             ],
         );        
