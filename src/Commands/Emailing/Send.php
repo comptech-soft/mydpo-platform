afbfe215eef5902170b73226ccc216e23122e38f
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-11-24 13:20:11 +0200

    =Accounts

--- src/Commands/Emailing/Send.php
+++ src/Commands/Emailing/Send.php
@@ -32,25 +32,32 @@ class Send extends Command {
                 break;
             }
 
-            /**
-             * Trimiteți emailul folosind clasa de email corespunzătoare
-             **/ 
-            \Mail::to($email->user->email)->send(
-                new SystemMail(
-                    user: $email->user,
-                    sender: $email->sender,
-                    template: $email->props['template'],
-                    payload: $email->props['payload']
-                ));
-            
-            /**
-             * Se actualizeaza câmpul 'sended_at' pentru email
-             */
-            $email->update([
-                'sended_at' => Carbon::now()
-            ]);
+            try
+            {
+                /**
+                 * Trimiteți emailul folosind clasa de email corespunzătoare
+                 **/ 
+                \Mail::to($email->user->email)->send(
+                    new SystemMail(
+                        user: $email->user,
+                        sender: $email->sender,
+                        template: $email->props['template'],
+                        payload: $email->props['payload']
+                    ));
+                
+                /**
+                 * Se actualizeaza câmpul 'sended_at' pentru email
+                 */
+                $email->update([
+                    'sended_at' => Carbon::now()
+                ]);
 
-            sleep(1);
+                sleep(1);
+            }
+            catch(\Exception $e)
+            {
+                \Log::info($e->getMessage());
+            }
         }
         
         $this->info('Sent ' . count($pending_emails) . ' emails.');
