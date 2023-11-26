bf3328cc9c4bb2acdd7f1b2b40aedf3739393d0e
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2023-11-26 12:37:18 +0200

    =Accounts

--- src/Events/BaseBroadcastEvent.php
+++ src/Events/BaseBroadcastEvent.php
@@ -5,7 +5,6 @@ namespace MyDpo\Events;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
