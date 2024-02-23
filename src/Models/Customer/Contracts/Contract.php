8b0454a5f2fc004717307acfe1e22030abc0e492
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-02-23 23:38:18 +0200

    Notifications

--- src/Models/Customer/Contracts/Contract.php
+++ src/Models/Customer/Contracts/Contract.php
@@ -124,4 +124,6 @@ class Contract extends Model {
 
     }
 
+    
+
 }
--- src/Models/Customer/Contracts/CustomerContract.php
+++ src/Models/Customer/Contracts/CustomerContract.php
@@ -25,12 +25,12 @@ class CustomerContract extends Contract {
     // }
 
     function orders() {
-        return $this->hasMany(CustomerOrder::class, 'contract_id')->orderBy('date_to', 'desc');
+        return $this->hasMany(Order::class, 'contract_id')->orderBy('date_to', 'desc');
     }
 
-    public function customer() {
-        return $this->belongsTo(Customer::class, 'customer_id')->select(['id', 'name', 'status'])->with(['mystatus']);
-    }
+    // public function customer() {
+    //     return $this->belongsTo(Customer::class, 'customer_id')->select(['id', 'name', 'status'])->with(['mystatus']);
+    // }
     
     // public static function getItems($input) {
     //     return (new GetItems($input, self::query()->with(['orders.services.service.type', 'customer']), __CLASS__))->Perform();
