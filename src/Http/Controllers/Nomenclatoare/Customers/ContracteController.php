b4af8f53bc486ee5497a0096a7b1bd06a0164180
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-02-23 22:09:46 +0200

    Notifications

--- src/Http/Controllers/Nomenclatoare/Customers/ContracteController.php
+++ src/Http/Controllers/Nomenclatoare/Customers/ContracteController.php
@@ -5,10 +5,14 @@ namespace MyDpo\Http\Controllers\Nomenclatoare\Customers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
+use MyDpo\Models\Customer\Contracts\Contract;
 
 class ContracteController extends Controller {
     
     public function index(Request $r) {
+        
+        Contract::CalculateContractExpirat();
+
         return Index::View(
             styles: ['css/app.css'],
             scripts: ['apps/nomenclatoare/customers/contracte/index.js'],
