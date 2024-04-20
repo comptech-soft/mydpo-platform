94f6e323fa558050af5d18cdbb41715a144f72b3
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-04-21 00:03:21 +0300

    Bam bam

--- src/Events/Authentication/Team/CreateTeamActivation.php
+++ src/Events/Authentication/Team/CreateTeamActivation.php
@@ -4,7 +4,7 @@ namespace MyDpo\Events\Authentication\Team;
 
 use MyDpo\Events\BaseBroadcastEvent;
 
-class CreateAccountActivation extends BaseBroadcastEvent {
+class CreateTeamActivation extends BaseBroadcastEvent {
 
     public function __construct($template, $input) {
         parent::__construct($template, $input);
