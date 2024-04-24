f8cea90ae09e7611239c26803ae2d944d50b6e4b
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-04-24 15:13:51 +0300

    Error emails

--- src/Http/Controllers/Nomenclatoare/Auth/TeamDashboardController.php
+++ src/Http/Controllers/Nomenclatoare/Auth/TeamDashboardController.php
@@ -5,16 +5,25 @@ namespace MyDpo\Http\Controllers\Nomenclatoare\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
+use MyDpo\Models\Authentication\User;
 
 class TeamDashboardController extends Controller {
     
     public function index($user_id, Request $r) {
 
-        dd($user_id);
-        
+        $user = User::find($user_id);
+
+        if(! $user )
+        {
+            return redirect('/');
+        }
+
         return Index::View(
             styles: ['css/app.css'],
             scripts: ['apps/nomenclatoare/auth/team-dashboard/index.js'],
+            payload: [
+                'user' => $user,
+            ],
         );        
     }
 
