f30cef120598ec6b61ee4ab6fb8913c5cf8882e3
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-07-08 10:00:55 +0300

    Questions

--- src/Http/Controllers/Nomenclatoare/Livrabile/Chestionare/ColectieIntrebariIntrebareDetailsController.php
+++ src/Http/Controllers/Nomenclatoare/Livrabile/Chestionare/ColectieIntrebariIntrebareDetailsController.php
@@ -5,14 +5,16 @@ namespace MyDpo\Http\Controllers\Nomenclatoare\Livrabile\Chestionare;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
-// use MyDpo\Models\Nomenclatoare\Livrabile\Chestionare\Question;
+use MyDpo\Models\Nomenclatoare\Livrabile\Chestionare\Question;
 
 class ColectieIntrebariIntrebareDetailsController extends Controller {
     
     public function index($id, Request $r) {
 
-        dd($id, $r->all());
-        
+        $question = Question::find($id);
+
+        dd($question);
+
         return Index::View(
             styles: ['css/app.css'],
             scripts: ['apps/nomenclatoare/livrabile/colectie-intrebari/index.js'],
