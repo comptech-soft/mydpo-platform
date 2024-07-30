105e349ccf58cdfefda27b43559363ade16bda55
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-07-31 00:40:15 +0300

    Questions

---
+++ src/Http/Controllers/Nomenclatoare/Livrabile/Chestionare/ColectieIntrebariAnswersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Nomenclatoare\Livrabile\Chestionare;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+// use MyDpo\Core\Http\Response\Index;
+// use MyDpo\Models\Nomenclatoare\Livrabile\Chestionare\Question;
+
+class ColectieIntrebariAnswersController extends Controller {
+    
+    // public function getRecords(Request $r) {
+    //     return Question::getRecords($r->all());
+    // }
+
+    // public function doAction($action, Request $r) {
+    //     return Question::doAction($action, $r->all());
+    // }
+ 
+}
