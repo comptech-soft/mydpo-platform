54ea717f213d17690b567dc27660400e46626b05
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-08-01 08:06:58 +0300

    Questions

---
+++ src/Models/Livrabile/Chestionare/ChestionarQuestionOption.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MyDpo\Models\Livrabile\Chestionare;
+
+use Illuminate\Database\Eloquent\Model;
+use MyDpo\Traits\Itemable;
+use MyDpo\Traits\Actionable;
+
+class QuestionAnswer extends Model {
+
+    use Actionable, Itemable;
+    
+    protected $table = 'chestionare-questions-options';
+    
+    protected $fillable = [
+        'id',
+        'chestionar_question_id',
+        'answer_text',
+        'triggered_subquestion_id',
+        'is_correct',
+        'order_no',
+        'props',
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'order_no' => 'integer',
+        'chestionar_question_id' => 'integer',
+        'triggered_subquestion_id' => 'integer',
+        'is_correct' => 'integer',
+        'props' => 'json',
+    ];
+
+
+}
