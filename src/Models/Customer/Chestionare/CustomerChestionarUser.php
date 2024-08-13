56d5b64f8d1d94420969d697bb7be5687c175703
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-08-13 11:03:57 +0300

    Customer chestionare

--- src/Models/Customer/Chestionare/CustomerChestionarUser.php
+++ src/Models/Customer/Chestionare/CustomerChestionarUser.php
@@ -57,7 +57,7 @@ class CustomerChestionarUser extends Model {
     ];
 
     protected $with = [
-        // 'user',
+        'user',
         // 'trimitere',
         // 'createdby',
     ];
@@ -168,9 +168,9 @@ class CustomerChestionarUser extends Model {
     //     ];
     // }
 
-    // public function user() {
-    //     return $this->belongsTo(User::class, 'user_id')->select(['id', 'first_name', 'last_name']);
-    // }
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id')->select(['id', 'first_name', 'last_name']);
+    }
 
     // public function createdby() {
     //     return $this->belongsTo(User::class, 'created_by');
