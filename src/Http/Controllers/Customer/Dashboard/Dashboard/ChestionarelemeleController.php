ec0f756279ef5ab31b0bd98f916f21bc54774167
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-09-01 21:46:19 +0300

    Chestionarele mele

---
+++ src/Http/Controllers/Customer/Dashboard/Dashboard/ChestionarelemeleController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MyDpo\Http\Controllers\Customer\Dashboard\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MyDpo\Core\Http\Response\Index;
+use MyDpo\Models\Customer\Customer;
+// use MyDpo\Models\Customer\ELearning\CustomerCurs;
+
+class ChestionarelemeleController extends Controller {
+
+    public function index($customer_id, Request $r) {
+
+        // CustomerCurs::Sync($customer_id);
+
+        return Index::View(
+            styles: ['css/app.css'],
+            scripts: ['apps/customer/chestionarele-mele/index.js'],
+            payload: [
+                'customer_id' => $customer_id,
+                'customer' => Customer::find($customer_id),
+                'customer_user' => \Auth::user(),
+            ],
+        );        
+    }
+
+}
