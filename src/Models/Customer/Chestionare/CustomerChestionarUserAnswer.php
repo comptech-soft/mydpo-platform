a9281208445ec0182bec37ce3e00b28855be51c7
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-09-03 01:20:48 +0300

    Chestionare

---
+++ src/Models/Customer/Chestionare/CustomerChestionarUserAnswer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MyDpo\Models\Customer\Chestionare;
+
+use Illuminate\Database\Eloquent\Model;
+// use MyDpo\Models\Authentication\User;
+// use MyDpo\Models\Livrabile\Chestionare\Chestionar;
+// use MyDpo\Models\Nomenclatoare\Livrabile\ELearning\Sharematerial;
+// use MyDpo\Traits\Itemable;
+// use MyDpo\Traits\Actionable;
+// use Carbon\Carbon;
+
+class CustomerChestionarUserAnswer extends Model {
+
+
+    protected $table = 'customers-chestionare-users-answers';
+
+    protected $casts = [
+        'subanswers' => 'json',
+        'customer_chestionar_user_id' => 'integer',
+        'question_id' => 'integer',
+        'id' => 'integer',
+    ];
+
+    protected $fillable = [
+        'id',
+        'customer_chestionar_user_id',
+        'question_id',
+        'value',
+        'subanswers'
+    ];
+
+}
