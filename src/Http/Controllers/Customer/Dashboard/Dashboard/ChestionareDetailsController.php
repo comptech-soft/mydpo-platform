80d6c477a2a39a0593be418a8bdbc943e3144b4a
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-09-04 02:21:08 +0300

    Chestonare

--- src/Http/Controllers/Customer/Dashboard/Dashboard/ChestionareDetailsController.php
+++ src/Http/Controllers/Customer/Dashboard/Dashboard/ChestionareDetailsController.php
@@ -5,31 +5,31 @@ namespace MyDpo\Http\Controllers\Customer\Dashboard\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use MyDpo\Core\Http\Response\Index;
-use MyDpo\Models\Customer\ELearning\CustomerCurs;
+use MyDpo\Models\Customer\Chestionare\CustomerChestionar;
 use MyDpo\Models\Customer\Customer;
 
-class CursuriDetailsController extends Controller {
+class ChestionareDetailsController extends Controller {
     
-    public function index($customer_id, $customer_curs_id, Request $r) {
+    public function index($customer_id, $customer_chestionar_id, Request $r) {
 
         if(! ($customer = Customer::find($customer_id)) )
         {
             return redirect('clienti');
         }
 
-        if(! ($customer_curs = CustomerCurs::find($customer_curs_id)) )
+        if(! ($customer_chestionar = CustomerChestionar::find($customer_chestionar_id)) )
         {
-            return redirect(config('app.url') . '/customer-elearning/' . $customer_id);
+            return redirect(config('app.url') . '/customer-chestionare/' . $customer_id);
         }
 
         return Index::View(
             styles: ['css/app.css'],
-            scripts: ['apps/customer/elearning-details/index.js'],
+            scripts: ['apps/customer/chestionare-details/index.js'],
             payload: [
                 'customer_id' => $customer_id,
                 'customer' => $customer,
-                'customer_curs_id' => $customer_curs_id,
-                'customer_curs' => $customer_curs,
+                'customer_chestionar_id' => $customer_chestionar_id,
+                'customer_chestionar' => $customer_chestionar,
                 'customer_user' => \Auth::user(),
             ],
         );     
