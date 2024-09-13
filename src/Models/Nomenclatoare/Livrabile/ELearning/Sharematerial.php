8874f014bbd890c86bbd39a59a607a9d237bf33d
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-09-13 13:37:46 +0300

    Chestionare

--- src/Models/Nomenclatoare/Livrabile/ELearning/Sharematerial.php
+++ src/Models/Nomenclatoare/Livrabile/ELearning/Sharematerial.php
@@ -288,6 +288,9 @@ class Sharematerial extends Model {
     // }
 
     public static function doInsert($input, $record) {
+
+        dd($input);
+        
         $record = self::create([
             ...$input,
             'created_by' => \Auth::user()->id,
