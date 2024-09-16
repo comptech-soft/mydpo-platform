dc5acc26c5e68339bab5a557462a10ed9375094d
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2024-09-16 10:23:55 +0300

    Chestionare

--- src/Exports/Livrabile/Chestionare/Exporter.php
+++ src/Exports/Livrabile/Chestionare/Exporter.php
@@ -6,7 +6,7 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithStrictNullComparison;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\FromView;
-use MyDpo\Models\System\Translation;
+use MyDpo\Models\Livrabile\Chestionare\ChestionarQuestion;
 
 use Illuminate\Contracts\View\View;
 
@@ -15,12 +15,18 @@ class Exporter implements FromView, WithStrictNullComparison, ShouldAutoSize {
     public $input = NULL;
     public $record = NULL;
 
-    public function __construct($input, $record = NULL) {
+    public function __construct($input, $record = NULL) 
+    {
         $this->input = $input;
+    
         $this->record = $record;    
     }
 
-    public function view(): View {
+    public function view(): View 
+    {
+
+        $questions = ChestionarQuestion::where('chestionar_id', $this->record->id)->whereNull('parent_id')->get()->toArray();
+        dd($questions);
 
         return view('exports.livrabile.chestionar.export', [
             'chestionar' => $this->record->toArray(),
